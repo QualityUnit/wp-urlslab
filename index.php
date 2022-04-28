b2e1ad65cfb8c6291838e9efedda78d4b0aa0809
yasha-dev1 <yasha1boroumand@gmail.com>
2022-04-28 18:33:51 +0200

    Edited the plugin specifications regarding licensing

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 * Requires PHP:      7.2
 * Author:            Yasha
 * Author URI:        https://author.example.com/
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 <?php // Silence is golden
