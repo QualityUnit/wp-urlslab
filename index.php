a2d2b0497fd6d387add0656f852856c775dcced4
yasha-dev1 <yasha1boroumand@gmail.com>
2022-04-28 17:57:58 +0200

    Installed the Wordpress plugin. Added the propper metadata regarding the wordpress plugin.

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+/**
+* Plugin Name:       Urlslab Screenshot
+* Plugin URI:        https://urlslab.com
+* Description:       Serving screenshots of URL
+* Version:           0.0.0
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            Yasha
+* Author URI:        https://author.example.com/
+*/
+
 <?php // Silence is golden
