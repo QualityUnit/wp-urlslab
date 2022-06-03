038d9d9549532bc4b7960de362cd9f0943a93604
yasha-dev1 <yasha1boroumand@gmail.com>
2022-06-03 10:22:12 +0200

    fix: composer fix

--- includes/helpers/urlslab-helpers.php
+++ includes/helpers/urlslab-helpers.php
@@ -12,7 +12,7 @@ function urlslab_admin_menu_page_url( $menu_slug ): string {
 	return admin_url() . 'admin.php?page=' . urlencode( $menu_slug );
 }
 
-function urlslab_get_url_description($summary, $meta_description, $title, $url ) {
+function urlslab_get_url_description( $summary, $meta_description, $title, $url ) {
 	if ( strlen( trim( $summary ) ) ) {
 		return $summary;
 	} elseif ( strlen( trim( $meta_description ) ) ) {
@@ -21,7 +21,7 @@ function urlslab_get_url_description($summary, $meta_description, $title, $url )
 		return $title;
 	}
 
-	return trim(str_replace('/', ' ', parse_url( $url, PHP_URL_PATH )));
+	return trim( str_replace( '/', ' ', parse_url( $url, PHP_URL_PATH ) ) );
 }
 
 function urlslab_get_language() {
@@ -31,9 +31,9 @@ function urlslab_get_language() {
 		return apply_filters( 'wpml_current_language', null );
 	}
 
-	if ( ! empty( $polylang ) && function_exists( 'pll_current_language' ) && strlen(pll_current_language())) {
+	if ( ! empty( $polylang ) && function_exists( 'pll_current_language' ) && strlen( pll_current_language() ) ) {
 		return pll_current_language();
 	}
 
-	return substr(get_locale(),0,2);
+	return substr( get_locale(), 0, 2 );
 }
