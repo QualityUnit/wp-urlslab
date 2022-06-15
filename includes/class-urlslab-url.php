c9fe0649d2d3f8798e0f5530636591fce7a60016
Viktor Zeman <vzeman@qualityunit.com>
2022-06-15 10:46:20 +0200

    fix: code formatting

--- includes/class-urlslab-url.php
+++ includes/class-urlslab-url.php
@@ -1,26 +1,30 @@
 <?php
 
-class Urlslab_Url {
+class Urlslab_Url
+{
 	private string $urlslab_parsed_url;
 	private array $url_components;
 
 	/**
 	 * @param string $url
 	 */
-	public function __construct( string $url ) {
-		$this->urlslab_url_init( $url );
+	public function __construct(string $url)
+	{
+		$this->urlslab_url_init($url);
 	}
 
 
 	/**
 	 * @return string
 	 */
-	public function get_url(): string {
+	public function get_url(): string
+	{
 		return $this->urlslab_parsed_url;
 	}
 
-	private function urlslab_url_init( string $input_url ): void {
-		if ( empty( $input_url ) ) {
+	private function urlslab_url_init(string $input_url): void
+	{
+		if (empty($input_url)) {
 			$this->url_components = array();
 			$this->urlslab_parsed_url = '';
 			return;
@@ -32,25 +36,27 @@ class Urlslab_Url {
 		}
 
 		if (!isset($this->url_components['scheme'])) {
-			$this->url_components['scheme'] = parse_url( get_site_url(), PHP_URL_SCHEME ) ?? 'http';
+			$this->url_components['scheme'] = parse_url(get_site_url(), PHP_URL_SCHEME) ?? 'http';
 		}
 
 		if (!isset($this->url_components['host'])) {
-			$this->url_components['host'] = parse_url( get_site_url(), PHP_URL_HOST );
+			$this->url_components['host'] = parse_url(get_site_url(), PHP_URL_HOST);
 		}
 
-		$url = $this->url_components['host'] . ( $this->url_components['path'] ?? '' );
-		if ( isset( $this->url_components['query'] ) ) {
+		$url = $this->url_components['host'] . ($this->url_components['path'] ?? '');
+		if (isset($this->url_components['query'])) {
 			$url .= '?' . $this->url_components['query'];
 		}
 		$this->urlslab_parsed_url = $url;
 	}
 
-	public function get_url_id(): string {
-		return md5( $this->urlslab_parsed_url );
+	public function get_url_id(): string
+	{
+		return md5($this->urlslab_parsed_url);
 	}
 
-	public function get_url_path(): string {
+	public function get_url_path(): string
+	{
 		return $this->url_components['path'] ?? '';
 	}
 
