0d669baf2dc5554458dae5034ae44ddee374d3d1
yasha-dev1 <yasha1boroumand@gmail.com>
2022-07-19 17:28:33 +0200

    feat: added api key notification in the admin ui

--- admin/partials/urlslab-admin-header.php
+++ admin/partials/urlslab-admin-header.php
@@ -13,10 +13,24 @@ if ( isset( $_GET['page'] ) ) {
 	<div class="urlslab-header-content-container">
 		<figure>
 			<?php $logo_url = plugin_dir_url( URLSLAB_PLUGIN ) . 'admin/assets/logo.svg'; ?>
-			<img src="<?php echo esc_url( $logo_url ); ?>" alt="URLsLab Logo">
+			<img src="<?php echo esc_url( $logo_url ); ?>" width="100%" alt="URLsLab Logo">
 		</figure>
 		<div>
 			<h1>Dashboard</h1>
 		</div>
 	</div>
 </header>
+<section>
+	<?php
+	$urlslab_user = Urlslab_User_Widget::get_instance();
+	if ( ! $urlslab_user->has_api_key() ) {
+		?>
+		<div>
+			<h2>No API Key Added</h2>
+			<p>
+				Without API key you are only able to retrieve data only for main page urls of any domain.
+				You will see urls that are not main page urls will get blocked. to unlock them you need to input API Key.
+			</p>
+		</div>
+	<?php } ?>
+</section>
