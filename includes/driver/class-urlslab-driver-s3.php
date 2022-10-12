6c9eaa9f070064dfebd4a66afdcac744a180aeaa
vzeman <vzeman@qualityunit.com>
2022-10-12 13:35:59 +0200

    fix: load settings from environemnt variables

--- includes/driver/class-urlslab-driver-s3.php
+++ includes/driver/class-urlslab-driver-s3.php
@@ -30,6 +30,7 @@ class Urlslab_Driver_S3 extends Urlslab_Driver {
 		while ( $data = $result['Body']->read( 8 * 1024 ) ) {
 			$content .= $data;
 		}
+
 		return $content;
 	}
 
@@ -55,7 +56,7 @@ class Urlslab_Driver_S3 extends Urlslab_Driver {
 		}
 	}
 
-	function save_file_to_storage( Urlslab_File_Data $file_obj, string $local_file_name ):bool {
+	function save_file_to_storage( Urlslab_File_Data $file_obj, string $local_file_name ): bool {
 		if ( ! $this->is_configured() ) {
 			return false;
 		}
@@ -71,6 +72,7 @@ class Urlslab_Driver_S3 extends Urlslab_Driver {
 
 		try {
 			$result = $uploader->upload();
+
 			return isset( $result['ObjectURL'] );
 		} catch ( MultipartUploadException $e ) {
 			return false;
@@ -92,6 +94,7 @@ class Urlslab_Driver_S3 extends Urlslab_Driver {
 			//we will proxy content of file
 			return site_url( self::DOWNLOAD_URL_PATH . urlencode( $file->get_fileid() ) . '/' . urlencode( $file->get_filename() ) );
 		}
+
 		return false;
 	}
 
@@ -99,27 +102,52 @@ class Urlslab_Driver_S3 extends Urlslab_Driver {
 		return $this->is_configured() && $this->getClient()->doesBucketExist( get_option( self::SETTING_NAME_S3_BUCKET, '' ) );
 	}
 
+	private function get_access_key() {
+		if ( strlen( get_option( self::SETTING_NAME_S3_ACCESS_KEY, '' ) ) ) {
+			return get_option( self::SETTING_NAME_S3_ACCESS_KEY, '' );
+		}
+
+		if ( strlen( env( 'AWS_KEY' ) ) ) {
+			return env( 'AWS_KEY' );
+		}
+
+		return false;
+	}
+
+	private function get_secret_key() {
+		if ( strlen( get_option( self::SETTING_NAME_S3_SECRET, '' ) ) ) {
+			return get_option( self::SETTING_NAME_S3_SECRET, '' );
+		}
+
+		if ( strlen( env( 'AWS_SECRET' ) ) ) {
+			return env( 'AWS_SECRET' );
+		}
+
+		return false;
+	}
+
 	private function getClient(): S3Client {
 		if ( $this->client ) {
 			return $this->client;
 		}
 
-		$credentials = new Aws\Credentials\Credentials( get_option( self::SETTING_NAME_S3_ACCESS_KEY, '' ), get_option( self::SETTING_NAME_S3_SECRET, '' ) );
+		$credentials  = new Aws\Credentials\Credentials( $this->get_access_key(), $this->get_secret_key() );
 		$this->client = new S3Client(
 			array(
-				'version' => 'latest',
-				'region' => get_option( self::SETTING_NAME_S3_REGION, '' ),
+				'version'     => 'latest',
+				'region'      => get_option( self::SETTING_NAME_S3_REGION, '' ),
 				'credentials' => $credentials,
 			)
 		);
+
 		return $this->client;
 	}
 
 	private function is_configured() {
 		return ! empty( get_option( self::SETTING_NAME_S3_REGION, '' ) ) &&
-			   ! empty( get_option( self::SETTING_NAME_S3_ACCESS_KEY, '' ) ) &&
-			   ! empty( get_option( self::SETTING_NAME_S3_BUCKET, '' ) ) &&
-			   ! empty( get_option( self::SETTING_NAME_S3_SECRET, '' ) );
+			   strlen( $this->get_access_key() ) &&
+			   strlen( $this->get_secret_key() ) &&
+			   ! empty( get_option( self::SETTING_NAME_S3_BUCKET, '' ) );
 	}
 
 	public function save_to_file( Urlslab_File_Data $file, $file_name ): bool {
@@ -139,6 +167,7 @@ class Urlslab_Driver_S3 extends Urlslab_Driver {
 			fwrite( $fhandle, $data );
 		}
 		fclose( $fhandle );
+
 		return true;
 	}
 
@@ -206,12 +235,13 @@ class Urlslab_Driver_S3 extends Urlslab_Driver {
 			$result = $this->getClient()->deleteObject(
 				array(
 					'Bucket' => get_option( self::SETTING_NAME_S3_BUCKET, '' ),
-					'Key' => $this->get_file_dir( $file ) . $file->get_filename(),
+					'Key'    => $this->get_file_dir( $file ) . $file->get_filename(),
 				)
 			);
 		} catch ( \Aws\S3\Exception\S3Exception $e ) {
 			return false;
 		}
+
 		return true;
 	}
 }
