b03c740440da0bfc660e2db9dffe157320613f92
Jozef Remeň <jozef.remen@gmail.com>
2023-01-17 11:03:58 +0100

    feat: first modules screen implementation

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-// import './index.css'
+import Modules from './Modules'
+import './assets/styles/common/global.scss'
 
 ReactDOM.createRoot(document.getElementById('urlslab-settings')).render(
   <React.StrictMode>
-    <App />
+    <Modules />
   </React.StrictMode>,
 )
