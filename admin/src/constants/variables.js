1fd5ed74cfd06a3f33116457e65507b8e7eb4b7f
Jozef Remeň <jozef.remen@gmail.com>
2023-01-18 09:34:12 +0100

    fix: small tweaks

---
+++ admin/src/constants/variables.js
@@ -0,0 +1,1 @@
+export const publicDir = import.meta.env.VITE_PUBLIC_URL;
--- urlslab.php
+++ urlslab.php
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-const URLSLAB_VERSION = '1.43.7';
+const URLSLAB_VERSION = '1.43.8';
 const URLSLAB_VERSION_SETTING = 'urlslab_ver';
 
 /**
