dbe516f3abf3c5f245737670f065cc4029031caa
Jozef Remeň <jozef.remen@gmail.com>
2023-01-18 14:58:12 +0100

    fix: modules

--- admin/src/Modules.jsx
+++ admin/src/Modules.jsx
@@ -27,8 +27,8 @@ export default function Modules() {
       <SearchField onChange={(value) => handleSearch(value)} />
       {/*<FilterMenu filterItems={filterActive}>All items</FilterMenu> */}
       <div className="urlslab-modules flex-tablet-landscape flex-wrap">
-        {
-          modules.map((module) => {
+        {modules
+          ? modules.map((module) => {
             const title = module.title.toLowerCase();
             const excerpt = module.description.toLowerCase();
             return (
@@ -47,6 +47,7 @@ export default function Modules() {
                 : null
             )
           })
+          : null
         }
       </div>
     </>
