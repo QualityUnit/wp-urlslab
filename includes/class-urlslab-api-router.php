c47016ddf16d3f1595c1fecff3d506d75795f40f
vzeman <vzeman@qualityunit.com>
2023-01-31 15:29:48 +0100

    feat: css cache api

--- includes/class-urlslab-api-router.php
+++ includes/class-urlslab-api-router.php
@@ -5,6 +5,7 @@ class Urlslab_Api_Router {
 		require_once URLSLAB_PLUGIN_DIR . 'admin/includes/api/class-urlslab-api-modules.php';
 		require_once URLSLAB_PLUGIN_DIR . 'admin/includes/api/class-urlslab-api-settings.php';
 		require_once URLSLAB_PLUGIN_DIR . 'admin/includes/api/class-urlslab-api-youtube-cache.php';
+		require_once URLSLAB_PLUGIN_DIR . 'admin/includes/api/class-urlslab-api-css-cache.php';
 
 		( new Urlslab_Api_Modules() )->register_routes();
 		( new Urlslab_Api_Settings() )->register_routes();
