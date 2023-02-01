7c22dd2e1b5949f4184429926e3b5e356fd53b7a
Jozef Remeň <jozef.remen@gmail.com>
2023-02-01 11:22:10 +0100

    feat: support for listbox (list with one selected value)

--- admin/src/elements/SortMenu.jsx
+++ admin/src/elements/SortMenu.jsx
@@ -1,81 +1,66 @@
 import { useEffect, useState, useRef } from 'react';
-import encoding from 'encoding';
 import Checkbox from './Checkbox';
 
 import '../assets/styles/elements/_FilterMenu.scss';
 
-export default function SortMenu({
-	className, name, style, children, filterItems, filteredItems,
-}) {
-	const [isActive, setActive] = useState(false);
-	const [isVisible, setVisible] = useState(false);
-	const [checked, setChecked] = useState([]);
-	const ref = useRef(null);
-	let items = {};
+export default function SortMenu( {
+	className, name, style, children, items, checkedId, filteredItems,
+} ) {
+	const [ isActive, setActive ] = useState( false );
+	const [ isVisible, setVisible ] = useState( false );
+	const [ checked, setChecked ] = useState( checkedId );
+	const ref = useRef( null );
 
-	if (filterItems) {
-		items = Object.values(filterItems);
-	}
-
-	useEffect(() => {
-		const handleClickOutside = (event) => {
-			if (!ref.current?.contains(event.target)) {
-				setActive(false);
-				setVisible(false);
+	useEffect( () => {
+		const handleClickOutside = ( event ) => {
+			if ( ! ref.current?.contains( event.target ) ) {
+				setActive( false );
+				setVisible( false );
 			}
 		};
-		document.addEventListener('click', handleClickOutside, true);
-	});
+		document.addEventListener( 'click', handleClickOutside, true );
+	} );
 
-	const checkedCheckbox = (target, isChecked) => {
-		if (isChecked) {
-			setChecked([...checked, target]);
-		}
-		if (!isChecked) {
-			setChecked(checked.filter((item) => item !== target));
-		}
+	const checkedCheckbox = ( target ) => {
+		setChecked( target );
 	};
 
-	// filteredItems(checked);
-
 	const handleMenu = () => {
-		setActive(!isActive);
+		setActive( ! isActive );
 
-		setTimeout(() => {
-			setVisible(!isVisible);
-		}, 100);
+		setTimeout( () => {
+			setVisible( ! isVisible );
+		}, 100 );
 	};
 
 	return (
-		<div className={`urlslab-FilterMenu ${className || ''}`} style={style} ref={ref}>
+		<div className={ `urlslab-FilterMenu ${ className || '' }` } style={ style } ref={ ref }>
 			<div
-				className={`urlslab-FilterMenu__title ${isActive ? 'active' : ''}`}
-				onClick={handleMenu}
-				onKeyUp={(event) => handleMenu(event)}
+				className={ `urlslab-FilterMenu__title ${ isActive ? 'active' : '' }` }
+				onClick={ handleMenu }
+				onKeyUp={ ( event ) => handleMenu( event ) }
 				role="button"
-				tabIndex={0}
+				tabIndex={ 0 }
 			>
-				{children}
+				{ children }
 			</div>
-			<div className={`urlslab-FilterMenu__items ${isActive ? 'active' : ''} ${isVisible ? 'visible' : ''}`}>
-				<div className={`urlslab-FilterMenu__items--inn ${items.length > 8 ? 'has-scrollbar' : ''}`}>
-					{items.map((val) => {
-						const id = val.replace(/(\s+|[0-9])/g, '').toLowerCase();
-						encoding.convert(val, 'Latin_1');
-
+			<div className={ `urlslab-FilterMenu__items ${ isActive ? 'active' : '' } ${ isVisible ? 'visible' : '' }` }>
+				<div className={ `urlslab-FilterMenu__items--inn ${ Object.values( items ).length > 8 ? 'has-scrollbar' : '' }` }>
+					{ Object.entries( items ).map( ( [ id, value ] ) => {
 						return (
 							<Checkbox
 								className="urlslab-FilterMenu__item"
-								key={id}
-								id={id}
-								onClick={checkedCheckbox}
-								name={name}
+								key={ id }
+								id={ id }
+								onChange={ () => checkedCheckbox( id ) }
+								name={ name }
+								checked={ id === checked }
 								radial
 							>
-								{val}
+								{ value }
 							</Checkbox>
 						);
-					})}
+					} ) }
 				</div>
 			</div>
 		</div>
