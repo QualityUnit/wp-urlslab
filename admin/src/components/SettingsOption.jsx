e576752768fcd37fa977e71bf600ea94b1c5140d
Jozef Remeň <jozef.remen@gmail.com>
2023-02-01 11:22:10 +0100

    feat: support for listbox (list with one selected value)

--- admin/src/components/SettingsOption.jsx
+++ admin/src/components/SettingsOption.jsx
@@ -1,9 +1,16 @@
+
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
 import InputField from '../elements/InputField';
 import Switch from '../elements/Switch';
 
+import '../assets/styles/components/datepicker/datepicker.scss';
+import SortMenu from '../elements/SortMenu';
+
 export default function SettingsOption( { option } ) {
+	const { id, type, title, placeholder, value, possible_values } = option;
+	const [ date, setDate ] = useState( type !== 'datetime' || new Date( value ) );
 	const renderOption = () => {
-		const { id, type, title, placeholder, value } = option;
 		switch ( type ) {
 			case 'text':
 			case 'password':
@@ -21,8 +28,30 @@ export default function SettingsOption( { option } ) {
 					<Switch
 						className="option flex"
 						label={ title }
+						onChange={ () => console.log( `${ id }:${ ! value }` ) }
 						checked={ value === '1' || value === true }
 					/>
+				);
+			case 'datetime':
+				const handleDate = ( newDate ) => {
+					setDate( newDate );
+				};
+				return (
+					<DatePicker
+						className="urlslab-input"
+						selected={ date }
+						dateFormat="dd. MMMM yyyy"
+						timeFormat="HH:mm"
+						locale={ window.navigator.language }
+						onChange={ ( newDate ) => handleDate( newDate ) }
+						showTimeSelect
+					/>
+				);
+			case 'listbox':
+				return (
+					<SortMenu name={ id } items={ possible_values } checkedId={ value }>
+						{ title }
+					</SortMenu>
 				);
 			default:
 				break;
