a562b7e2c0a78d29b7a83babbf5388a12f4cd304
Jozef Remeň <jozef.remen@gmail.com>
2023-02-04 13:53:02 +0100

    feat: keywords module - preliminary support for upload and parsing csv

--- admin/src/modules/KeywordsLinks.jsx
+++ admin/src/modules/KeywordsLinks.jsx
@@ -1,7 +1,43 @@
+import { useState, useRef } from 'react';
+import { useCSVReader } from 'react-papaparse';
+import { useQuery } from '@tanstack/react-query';
+import { createColumnHelper } from '@tanstack/react-table';
+import { useI18n } from '@wordpress/react-i18n';
+import { fetchData } from '../api/fetchData';
+import Loader from '../components/Loader';
+import Table from '../components/TableComponent';
+
 export default function KeywordLinks( { settings } ) {
+	const { CSVReader } = useCSVReader();
 	return (
-		<>
-			KeywordLinks
-		</>
+		<CSVReader
+			onUploadAccepted={ ( results ) => {
+				console.log( '---------------------------' );
+				console.log( results );
+				console.log( '---------------------------' );
+			} }
+		>
+			{ ( {
+				getRootProps,
+				acceptedFile,
+				ProgressBar,
+				getRemoveFileProps,
+			} ) => (
+				<>
+					<div>
+						<button type="button" { ...getRootProps() }>
+							Browse file
+						</button>
+						<div>
+							{ acceptedFile && acceptedFile.name }
+						</div>
+						<button { ...getRemoveFileProps() }>
+							Remove
+						</button>
+					</div>
+					<ProgressBar />
+				</>
+			) }
+		</CSVReader>
 	);
 }
