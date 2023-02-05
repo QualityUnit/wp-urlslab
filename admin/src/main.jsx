47ea6c73b2662d633c39956ea89aba14f640abeb
Jozef Remeň <jozef.remen@gmail.com>
2023-02-05 09:04:01 +0100

    feat: implementing react query preliminary persistent cache

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,19 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { createRoot } from 'react-dom';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import {
 	QueryClient,
 	QueryClientProvider,
 } from '@tanstack/react-query';
+import {
+	PersistQueryClientProvider,
+} from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 import App from './App';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient( {
+	defaultOptions: {
+		queries: {
+			cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+		},
+	},
+} );
+
+const persister = createSyncStoragePersister( {
+	storage: window.localStorage,
+} );
 
 createRoot( document.getElementById( 'urlslab-root' ) ).render(
 	<React.StrictMode>
-		<QueryClientProvider client={ queryClient }>
+		<PersistQueryClientProvider client={ queryClient } persistOptions={ { persister } }>
 			<App />
-		</QueryClientProvider>
+		</PersistQueryClientProvider>
 	</React.StrictMode>
 );
