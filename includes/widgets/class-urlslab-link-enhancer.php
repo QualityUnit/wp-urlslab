feb525eb59f76bed1484fb3ebbfb59ebff2cab86
vzeman <vzeman@qualityunit.com>
2023-02-09 13:11:41 +0100

    feat: custom class to skip processing of fragments in links

--- includes/widgets/class-urlslab-link-enhancer.php
+++ includes/widgets/class-urlslab-link-enhancer.php
@@ -88,95 +88,8 @@ class Urlslab_Link_Enhancer extends Urlslab_Widget {
 	}
 
 	public function theContentHook( DOMDocument $document ) {
-		try {
-			$xpath    = new DOMXPath( $document );
-			$elements = $xpath->query( "//a[not(ancestor-or-self::*[contains(@class, 'urlslab-skip-all') or contains(@class, 'urlslab-skip-title')])]" );
-
-			$link_elements = array();
-			if ( $elements instanceof DOMNodeList ) {
-				foreach ( $elements as $dom_element ) {
-					//skip processing if A tag contains attribute "urlslab-skip-all" or urlslab-skip-title
-					if ( $this->is_skip_elemenet( $dom_element, 'title' ) ) {
-						continue;
-					}
-
-					if ( ! empty( trim( $dom_element->getAttribute( 'href' ) ) ) ) {
-						try {
-							$url             = new Urlslab_Url( $dom_element->getAttribute( 'href' ) );
-							$link_elements[] = array( $dom_element, $url );
-						} catch ( Exception $e ) {
-						}
-					}
-				}
-			}
-
-			if ( ! empty( $link_elements ) ) {
-
-				$result = $this->urlslab_url_data_fetcher->fetch_schedule_urls_batch(
-					array_merge(
-						array( new Urlslab_Url( urlslab_add_current_page_protocol( $this->get_current_page_url()->get_url() ) ) ),
-						array_map( fn( $elem ): Urlslab_Url => $elem[1], $link_elements )
-					)
-				);
-
-				if ( ! empty( $result ) ) {
-					$strategy = $this->get_option( self::SETTING_NAME_DESC_REPLACEMENT_STRATEGY );
-
-					$this->update_urls_map( array_keys( $result ) );
-
-					foreach ( $link_elements as $arr_element ) {
-						list( $dom_elem, $url_obj ) = $arr_element;
-						if (
-							isset( $result[ $url_obj->get_url_id() ] ) &&
-							! empty( $result[ $url_obj->get_url_id() ] )
-						) {
-
-							if (
-								$this->get_option( self::SETTING_NAME_REMOVE_LINKS ) &&
-								! $result[ $url_obj->get_url_id() ]->is_visible()
-							) {
-								//link should not be visible, remove it from content
-								if ( $dom_elem->childNodes->length > 0 ) {
-									$fragment = $document->createDocumentFragment();
-									if ( $dom_elem->childNodes->length > 0 ) {
-										$fragment->appendChild( $dom_elem->childNodes->item( 0 ) );
-									}
-									$dom_elem->parentNode->replaceChild( $fragment, $dom_elem );
-								} else {
-									if ( property_exists( $dom_element, 'domValue' ) ) {
-										$txt_value = $dom_elem->domValue;
-									} else {
-										$txt_value = '';
-									}
-									$txt_element = $document->createTextNode( $txt_value );
-									$dom_elem->parentNode->replaceChild( $txt_element, $dom_elem );
-								}
-							} else {
-								//enhance title if url has no title
-								if ( empty( $dom_elem->getAttribute( 'title' ) ) ) {
-									$dom_elem->setAttribute(
-										'title',
-										$result[ $url_obj->get_url_id() ]->get_url_summary_text( $strategy ),
-									);
-								}
-								if ( $this->get_option( self::SETTING_NAME_ADD_LINK_FRAGMENT ) && false === strpos( $dom_elem->getAttribute( 'href' ), '#' ) ) {
-									if ( $dom_elem->childNodes->length > 0 && property_exists( $dom_elem->childNodes->item( 0 ), 'wholeText' ) ) {
-										$fragment_text = $dom_elem->childNodes->item( 0 )->wholeText;
-									} else if ( property_exists( $dom_elem, 'domValue' ) ) {
-										$fragment_text = $dom_elem->domValue;
-									} else {
-										$fragment_text = $result[ $url_obj->get_url_id() ]->get_url_summary_text( Urlslab_Link_Enhancer::DESC_TEXT_TITLE );
-									}
-
-									$dom_elem->setAttribute( 'href', $dom_elem->getAttribute( 'href' ) . '#:~:text=' . urlencode( $fragment_text ) );
-								}
-							}
-						}
-					}
-				}
-			}
-		} catch ( Exception $e ) {
-		}
+		$this->processTitleAttribute( $document );
+		$this->processLinkFragments( $document );
 	}
 
 	public function get_shortcode_content( $atts = array(), $content = null, $tag = '' ): string {
@@ -280,13 +193,99 @@ class Urlslab_Link_Enhancer extends Urlslab_Widget {
 		return true;
 	}
 
+	/**
+	 * @param DOMDocument $document
+	 *
+	 * @return void
+	 */
+	private function processTitleAttribute( DOMDocument $document ): void {
+		try {
+			$xpath    = new DOMXPath( $document );
+			$elements = $xpath->query( "//a[not(ancestor-or-self::*[contains(@class, 'urlslab-skip-all') or contains(@class, 'urlslab-skip-title')])]" );
+
+			$link_elements = array();
+			if ( $elements instanceof DOMNodeList ) {
+				foreach ( $elements as $dom_element ) {
+					//skip processing if A tag contains attribute "urlslab-skip-all" or urlslab-skip-title
+					if ( $this->is_skip_elemenet( $dom_element, 'title' ) ) {
+						continue;
+					}
+
+					if ( ! empty( trim( $dom_element->getAttribute( 'href' ) ) ) ) {
+						try {
+							$url             = new Urlslab_Url( $dom_element->getAttribute( 'href' ) );
+							$link_elements[] = array( $dom_element, $url );
+						} catch ( Exception $e ) {
+						}
+					}
+				}
+			}
+
+			if ( ! empty( $link_elements ) ) {
+
+				$result = $this->urlslab_url_data_fetcher->fetch_schedule_urls_batch(
+					array_merge(
+						array( new Urlslab_Url( urlslab_add_current_page_protocol( $this->get_current_page_url()->get_url() ) ) ),
+						array_map( fn( $elem ): Urlslab_Url => $elem[1], $link_elements )
+					)
+				);
+
+				if ( ! empty( $result ) ) {
+					$strategy = $this->get_option( self::SETTING_NAME_DESC_REPLACEMENT_STRATEGY );
+
+					$this->update_urls_map( array_keys( $result ) );
+
+					foreach ( $link_elements as $arr_element ) {
+						list( $dom_elem, $url_obj ) = $arr_element;
+						if (
+							isset( $result[ $url_obj->get_url_id() ] ) &&
+							! empty( $result[ $url_obj->get_url_id() ] )
+						) {
+
+							if (
+								$this->get_option( self::SETTING_NAME_REMOVE_LINKS ) &&
+								! $result[ $url_obj->get_url_id() ]->is_visible()
+							) {
+								//link should not be visible, remove it from content
+								if ( $dom_elem->childNodes->length > 0 ) {
+									$fragment = $document->createDocumentFragment();
+									if ( $dom_elem->childNodes->length > 0 ) {
+										$fragment->appendChild( $dom_elem->childNodes->item( 0 ) );
+									}
+									$dom_elem->parentNode->replaceChild( $fragment, $dom_elem );
+								} else {
+									if ( property_exists( $dom_element, 'domValue' ) ) {
+										$txt_value = $dom_elem->domValue;
+									} else {
+										$txt_value = '';
+									}
+									$txt_element = $document->createTextNode( $txt_value );
+									$dom_elem->parentNode->replaceChild( $txt_element, $dom_elem );
+								}
+							} else {
+								//enhance title if url has no title
+								if ( empty( $dom_elem->getAttribute( 'title' ) ) ) {
+									$dom_elem->setAttribute(
+										'title',
+										$result[ $url_obj->get_url_id() ]->get_url_summary_text( $strategy ),
+									);
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch ( Exception $e ) {
+		}
+	}
+
 	protected function add_options() {
 		$this->add_option_definition(
 			self::SETTING_NAME_DESC_REPLACEMENT_STRATEGY,
 			self::DESC_TEXT_SUMMARY,
 			true,
 			__( 'Description value' ),
-			__( 'Specify which data should be used to enhance your links automatically' ),
+			__( 'Specify which data should be used to enhance your links automatically. If you want to disable enhancement for specific links, add class "urlslab-skip-title" to link or any parent tag in HTML.' ),
 			self::OPTION_TYPE_LISTBOX,
 			array(
 				Urlslab_Link_Enhancer::DESC_TEXT_SUMMARY          => __( 'Generate descriptions with summaries' ),
@@ -325,7 +324,7 @@ class Urlslab_Link_Enhancer extends Urlslab_Widget {
 			false,
 			true,
 			__( 'Add Text Fragments to every link' ),
-			__( 'Enhance every link in the page with text fragement. Example: www.yourdomain.com/page1#:~:text=link%20text' )
+			__( 'Enhance every link in the page with text fragement. Example: "www.yourdomain.com/page1#:~:text=link%20text". To disable processing on some links, add class "urlslab-skip-fragment" to link or any parent html object.' )
 		);
 
 		$this->add_option_definition(
@@ -336,5 +335,33 @@ class Urlslab_Link_Enhancer extends Urlslab_Widget {
 			__( 'Background process validates all found URLs in page created after selected date.' ),
 			self::OPTION_TYPE_DATETIME
 		);
+	}
+
+	private function processLinkFragments( DOMDocument $document ) {
+		if ( ! $this->get_option( self::SETTING_NAME_ADD_LINK_FRAGMENT ) ) {
+			return;
+		}
+		$xpath    = new DOMXPath( $document );
+		$elements = $xpath->query( "//a[@href and not(ancestor-or-self::*[contains(@class, 'urlslab-skip-all') or contains(@class, 'urlslab-skip-fragment')])]" );
+		foreach ( $elements as $dom_elem ) {
+			if ( strlen( $dom_elem->getAttribute( 'href' ) ) && false === strpos( $dom_elem->getAttribute( 'href' ), '#' ) ) {
+				$fragment_text = '';
+				if ( $dom_elem->childNodes->length > 0 && property_exists( $dom_elem->childNodes->item( 0 ), 'wholeText' ) ) {
+					$fragment_text = trim( $dom_elem->childNodes->item( 0 )->wholeText );
+				} else if ( property_exists( $dom_elem, 'domValue' ) ) {
+					$fragment_text = trim( $dom_elem->domValue );
+				}
+				if ( strlen( $fragment_text ) ) {
+					try {
+						$url = new Urlslab_Url( $dom_elem->getAttribute( 'href' ) );
+						if ( $url->is_url_valid() && $url->is_same_domain_url() ) {
+							$dom_elem->setAttribute( 'href', $dom_elem->getAttribute( 'href' ) . '#:~:text=' . urlencode( $fragment_text ) );
+						}
+					} catch ( Exception $e ) {
+						//noop, just skip link
+					}
+				}
+			}
+		}
 	}
 }
