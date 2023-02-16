cc6cf7a6f808c498f41ef53944240e874699b642
vzeman <vzeman@qualityunit.com>
2023-02-16 14:25:28 +0100

    fix: if zero rows returned in table, don't return error, skip local cache in client

--- admin/includes/api/class-urlslab-api-youtube-cache.php
+++ admin/includes/api/class-urlslab-api-youtube-cache.php
@@ -115,7 +115,7 @@ class Urlslab_Api_Youtube_Cache extends Urlslab_Api_Table {
 
 		$rows = $sql->get_results();
 
-		if ( null == $rows || false === $rows ) {
+		if ( null === $rows || false === $rows ) {
 			return new WP_Error( 'error', __( 'Failed to get items', 'urlslab' ), array( 'status' => 500 ) );
 		}
 
