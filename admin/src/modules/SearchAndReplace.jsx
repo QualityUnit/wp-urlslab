c2d6c9b5b059b8d666fb6bc1fa36652bdab4f1bf
Jozef Remeň <jozef.remen@gmail.com>
2023-03-06 08:04:51 +0100

    feat: seach replace module

---
+++ admin/src/modules/SearchAndReplace.jsx
@@ -0,0 +1,29 @@
+import { useState, Suspense, lazy } from 'react';
+
+import Overview from '../components/OverviewTemplate';
+import SearchAndReplaceOverview from '../overview/SearchAndReplace';
+import ModuleViewHeader from '../components/ModuleViewHeader';
+
+export default function SearchAndReplace( { moduleId } ) {
+	const [ activeSection, setActiveSection ] = useState( 'overview' );
+
+	const SettingsModule = lazy( () => import( `../modules/Settings.jsx` ) );
+
+	return (
+		<div className="urlslab-tableView">
+			<ModuleViewHeader activeMenu={ ( activemenu ) => setActiveSection( activemenu ) } />
+			{
+				activeSection === 'overview' &&
+				<Overview moduleId={ moduleId }>
+					<SearchAndReplaceOverview />
+				</Overview>
+			}
+			{
+				activeSection === 'settings' &&
+				<Suspense>
+					<SettingsModule className="fadeInto" settingId={ moduleId } />
+				</Suspense>
+			}
+		</div>
+	);
+}
