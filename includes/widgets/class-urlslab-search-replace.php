bfa2104a8b40fc2ac3428e485888ecb177ef8db6
vzeman <vzeman@qualityunit.com>
2023-03-07 16:18:25 +0100

    fix: current url for 404 is invalid, catch error

--- includes/widgets/class-urlslab-search-replace.php
+++ includes/widgets/class-urlslab-search-replace.php
@@ -56,14 +56,17 @@ class Urlslab_Search_Replace extends Urlslab_Widget {
 	private function get_rules(): array {
 		if ( ! $this->loaded ) {
 			global $wpdb;
-			$results = $wpdb->get_results( $wpdb->prepare( 'SELECT * FROM ' . URLSLAB_SEARCH_AND_REPLACE_TABLE ), 'ARRAY_A' ); // phpcs:ignore
-			$current_url = $this->get_current_page_url()->get_url();
-			foreach ( $results as $row ) {
-				if ( '.*' !== $row->get( 'url_filter' ) && ! preg_match( '/' . str_replace( '/', '\\/', $row->get( 'url_filter' ) ) . '/uim', $current_url ) ) {
-					continue;
+			try {
+				$results     = $wpdb->get_results( $wpdb->prepare( 'SELECT * FROM ' . URLSLAB_SEARCH_AND_REPLACE_TABLE ), 'ARRAY_A' ); // phpcs:ignore
+				$current_url = $this->get_current_page_url()->get_url();
+				foreach ( $results as $row ) {
+					if ( '.*' !== $row->get( 'url_filter' ) && ! preg_match( '/' . str_replace( '/', '\\/', $row->get( 'url_filter' ) ) . '/uim', $current_url ) ) {
+						continue;
+					}
+					$obj_search                              = new Urlslab_Search_Replace_Row( $row );
+					$this->rules[ $obj_search->get( 'id' ) ] = $obj_search;
 				}
-				$obj_search                              = new Urlslab_Search_Replace_Row( $row );
-				$this->rules[ $obj_search->get( 'id' ) ] = $obj_search;
+			} catch ( Exception $e ) {
 			}
 			$this->loaded = true;
 		}
