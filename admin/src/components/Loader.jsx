46dde5e9141560adc8e9c8cd043d8381831822b2
Jozef Remeň <jozef.remen@gmail.com>
2023-03-13 09:47:03 +0100

    feat: row counter in table header

--- admin/src/components/Loader.jsx
+++ admin/src/components/Loader.jsx
@@ -2,13 +2,15 @@ import { useI18n } from '@wordpress/react-i18n';
 import { ReactComponent as LoaderAnim } from '../assets/images/ajax-loader.svg';
 import '../assets/styles/components/_Loader.scss';
 
-export default function Loader( { children } ) {
+export default function Loader( { children, className } ) {
 	const { __ } = useI18n();
 
 	return (
-		<div className="urlslab-loader">
+		<div className={ `urlslab-loader ${ className || '' }` }>
 			<LoaderAnim className="urlslab-loader-anim" />
-			{ children ? children : __( 'Loading…' ) }
+			<span className="urlslab-loader-text">
+				{ children ? children : __( 'Loading…' ) }
+			</span>
 		</div>
 	);
 }
