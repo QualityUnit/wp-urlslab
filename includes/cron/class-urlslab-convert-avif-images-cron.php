a7deb1c7d73bec0e06859708f387b709abd49e9b
vzeman <vzeman@qualityunit.com>
2023-03-14 07:17:07 +0100

    fix: fatal error - return value must be of type array, string returned

--- includes/cron/class-urlslab-convert-avif-images-cron.php
+++ includes/cron/class-urlslab-convert-avif-images-cron.php
@@ -12,23 +12,10 @@ class Urlslab_Convert_Avif_Images_Cron extends Urlslab_Convert_Images_Cron {
 			   );
 	}
 
-	protected function get_file_types(): array {
-		$values = get_option( Urlslab_Media_Offloader_Widget::SETTING_NAME_AVIF_TYPES_TO_CONVERT, Urlslab_Media_Offloader_Widget::SETTING_DEFAULT_AVIF_TYPES_TO_CONVERT );
-		if ( ! is_array( $values ) ) {
-			if ( empty( $values ) ) {
-				return array();
-			} else {
-				return array( $values );
-			}
-		}
-
-		return $values;
-	}
-
 	protected function convert_next_file() {
 		global $wpdb;
 
-		$values = $this->get_file_types();
+		$values = $this->get_file_types( Urlslab_Media_Offloader_Widget::SETTING_NAME_AVIF_TYPES_TO_CONVERT, Urlslab_Media_Offloader_Widget::SETTING_DEFAULT_AVIF_TYPES_TO_CONVERT );
 
 		if ( empty( $values ) ) {
 			return false;
--- includes/cron/class-urlslab-convert-webp-images-cron.php
+++ includes/cron/class-urlslab-convert-webp-images-cron.php
@@ -12,23 +12,11 @@ class Urlslab_Convert_Webp_Images_Cron extends Urlslab_Convert_Images_Cron {
 			   );
 	}
 
-	protected function get_file_types(): array {
-		$values = get_option( Urlslab_Media_Offloader_Widget::SETTING_NAME_WEBP_TYPES_TO_CONVERT, Urlslab_Media_Offloader_Widget::SETTING_DEFAULT_WEBP_TYPES_TO_CONVERT );
-		if ( ! is_array( $values ) ) {
-			if ( empty( $values ) ) {
-				return array();
-			} else {
-				return array( $values );
-			}
-		}
-
-		return $values;
-	}
 
 	protected function convert_next_file() {
 		global $wpdb;
 
-		$values = $this->get_file_types();
+		$values = $this->get_file_types( Urlslab_Media_Offloader_Widget::SETTING_NAME_WEBP_TYPES_TO_CONVERT, Urlslab_Media_Offloader_Widget::SETTING_DEFAULT_WEBP_TYPES_TO_CONVERT );
 
 		if ( empty( $values ) ) {
 			return false;
