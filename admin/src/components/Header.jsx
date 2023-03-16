005b705d09f3a970f6646b33ae79f81eb1027aef
Jozef Remeň <jozef.remen@gmail.com>
2023-03-16 15:46:53 +0100

    feat: cron button and panel with abort

--- admin/src/components/Header.jsx
+++ admin/src/components/Header.jsx
@@ -1,37 +1,68 @@
-import { Suspense, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { Suspense, useState, useEffect, useRef } from 'react';
+import { useI18n } from '@wordpress/react-i18n';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { ReactComponent as Logo } from '../assets/images/urlslab-logo.svg';
-import { fetchData } from '../api/fetching';
-import { fetchSettings } from '../api/settings';
+import { fetchCronAll } from '../api/cron';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import NoAPIkey from './NoAPIkey';
 import Notifications from './Notifications';
 import Button from '../elements/Button';
+import NotificationsPanel from './NotificationsPanel';
 
 export default function Header( { pageTitle } ) {
-	const [ runCron, setCronRunner ] = useState( false );
-	// let apikey = '********';
-	// const generalSettings = useQuery( {
-	// 	queryKey: [ 'general' ],
-	// 	queryFn: () => fetchSettings( 'general' ).then( ( data ) => {
-	// 		return data;
-	// 	} ),
-	// } );
+	const { __ } = useI18n();
+	const runCron = useRef( false );
+	const [ cronRunning, setCronRun ] = useState( false );
+	const [ cronTasksResult, setCronTasks ] = useState( [] );
+	const [ panelActive, setPanelActive ] = useState( false );
 
-	// if ( generalSettings.isSuccess ) {
-	// 	const settings = generalSettings.data[ 0 ].options[ 'urlslab-api-key' ];
-	// 	apikey = settings.value;
-	// }
+	const handleCronRunner = () => {
+		setCronRun( ! cronRunning );
 
-	const { data: cronResults } = useQuery( {
-		queryKey: [ 'cron', 'all' ],
-		queryFn: () => fetchData( 'cron/all' ).then( async ( response ) => {
-			return await response.json();
-		} ).then( ( tasks ) => tasks ),
-		enabled: runCron,
-		refetchOnWindowFocus: false,
-		onSuccess: ( tasks ),
-	} );
+		runCron.current = ! runCron.current;
+
+		if ( runCron.current ) {
+			cronAll( ( cronTasks ) => setCronTasks( cronTasks ) );
+		}
+	};
+
+	async function cronAll( cronTasks ) {
+		const controller = new AbortController();
+		if ( ! runCron.current ) {
+			controller.abort();
+		}
+		const result = await fetch( '/wp-json/urlslab/v1/cron/all', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				accept: 'application/json',
+				'X-WP-Nonce': window.wpApiSettings.nonce,
+			},
+			credentials: 'include',
+			signal: controller.signal,
+		} ).then( ( response ) => response.json() ).catch( ( e ) => console.log( e ) );
+
+		const okResult = result?.filter( ( task ) => task.exec_time >= 5 );
+
+		if ( okResult?.length ) {
+			cronTasks( result );
+			cronAll( cronTasks );
+		}
+		if ( ! okResult?.length ) {
+			setInterval( () => cronAll( cronTasks ), 5000 );
+		}
+		return false;
+	}
+
+	useEffect( () => {
+		if ( cronTasksResult?.length ) {
+			setPanelActive( true );
+			setTimeout( () => {
+				setPanelActive( false );
+				setCronTasks( [] );
+			}, 3000 );
+		}
+	}, [ panelActive, cronTasksResult?.length ] );
 
 	return (
 		<Suspense>
@@ -40,7 +71,16 @@ export default function Header( { pageTitle } ) {
 					<Logo className="urlslab-header-logo" />
 					<span className="urlslab-header-slash">/</span>
 					<h1 className="urlslab-header-title">{ pageTitle }</h1>
-					<Button active className="small ma-left" onClick={ () => setCronRunner( true ) }>Accelerate Cron Execution</Button>
+					<Button active className="pos-relative small ma-left" onClick={ handleCronRunner }>
+						{ ! cronRunning
+							? __( 'Accelerate Cron Execution' )
+							: __( 'Stop Cron Execution' )
+						}
+						{
+							cronTasksResult.length > 0 &&
+							<NotificationsPanel className="wide" active={ panelActive }>{ cronTasksResult.map( ( task ) => <div className="message" key={ task.task }>{ task.description }</div> ) }</NotificationsPanel>
+						}
+					</Button>
 					{ /* <Notifications /> */ }
 				</div>
 				{ /* { apikey && apikey.length
--- admin/src/elements/ProgressBar.jsx
+++ admin/src/elements/ProgressBar.jsx
@@ -1,21 +1,12 @@
 import { useRef } from 'react';
 import '../assets/styles/elements/_ProgressBar.scss';
 
-let time = 0;
+const time = 0;
 export default function ProgressBar( { notification, value } ) {
-	const timer = setInterval( () => {
-		time = time + 1;
-
-		// time.current = time.current + 1;
-		// setTime( time + 1 );
-	}, 1000 );
-	if ( value === 100 ) {
-		clearInterval( timer );
-	}
 	return (
 		<div className="urlslab-progressBar-wrapper">
 			<progress className="urlslab-progressBar" value={ value } max="100"></progress>
-			<span className="urlslab-progressBar-timer">{ time }s </span>
+			<span className="urlslab-progressBar-timer">{ notification } </span>
 		</div>
 	);
 }
