581c8e280bf9d14cf8fa66f784d0a05ba88cf4b2
vzeman <vzeman@qualityunit.com>
2023-03-18 10:43:16 +0100

    feat: updating plugin

--- urlslab.php
+++ urlslab.php
@@ -9,15 +9,13 @@
  * that starts the plugin.
  *
  * @link              https://www.urlslab.com
- * @since             1.0.0
  * @package           Urlslab
  *
  * @wordpress-plugin
  *
  * Plugin Name:       URLsLab
- * Plugin URI:        https://www.urlslab.com
- * Description:       URLsLab WordPress Plugin
-
+ * Plugin URI:        https://github.com/QualityUnit/wp-urlslab
+ * Description:       URLsLab WordPress Plugin to optimize your website for search engines and enhance automatically content
  * Version:           2.2.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
@@ -32,11 +30,6 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
 
 const URLSLAB_VERSION = '2.2.0';
 const URLSLAB_VERSION_SETTING = 'urlslab_ver';
@@ -73,6 +66,8 @@ define( 'URLSLAB_SEARCH_AND_REPLACE_TABLE', $wpdb->prefix . 'urlslab_search_repl
 define( 'URLSLAB_SCREENSHOT_URLS_TABLE', $wpdb->prefix . 'urlslab_screenshot_urls' );
 define( 'URLSLAB_PLUGIN_LOG', plugin_dir_path( __FILE__ ) . 'debug.log' );
 
+define( 'URLSLAB_INFO_URL', 'https://github.com/QualityUnit/wp-urlslab/blob/main/info.json' );
+
 
 add_filter( 'cron_schedules', 'urlslab_add_cron_interval' );
 function urlslab_add_cron_interval( $schedules ): array {
@@ -102,24 +97,120 @@ function deactivate_urlslab() {
 	Urlslab_Deactivator::deactivate();
 }
 
+function urlslab_plugin_action_links( array $links ) {
+	return array_merge(
+		array(
+			'<a href="' . admin_url( '/admin.php?page=urlslab-dashboard' ) . '" title="' . __( 'URLsLab Settings', 'urlslab' ) . '">' . __( 'Settings', 'urlslab' ) . '</a>',
+		), $links
+	);
+}
+
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'urlslab_plugin_action_links' );
+
+add_filter( 'plugins_api', 'urlslab_plugin_info', 20, 3 );
+
+function urlslab_plugin_info( $res, $action, $args ) {
+	if ( 'plugin_information' !== $action || plugin_basename( __DIR__ ) !== $args->slug ) {
+		return $res;
+	}
+
+	$slug = plugin_basename( __DIR__ );
+
+	$remote = wp_remote_get(
+		URLSLAB_INFO_URL,
+		array(
+			'timeout' => 5,
+			'headers' => array(
+				'Accept' => 'application/json',
+			),
+		)
+	);
+
+	if ( is_wp_error( $remote ) || 200 !== wp_remote_retrieve_response_code( $remote ) || empty( wp_remote_retrieve_body( $remote ) ) ) {
+		return $res;
+	}
+
+	$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+
+	$res                 = new stdClass();
+	$res->name           = $remote->name;
+	$res->slug           = $remote->slug;
+	$res->author         = $remote->author;
+	$res->author_profile = $remote->author_profile;
+	$res->version        = $remote->version;
+	$res->tested         = $remote->tested;
+	$res->requires       = $remote->requires;
+	$res->requires_php   = $remote->requires_php;
+	$res->download_link  = $remote->download_url;
+	$res->trunk          = $remote->download_url;
+	$res->last_updated   = $remote->last_updated;
+	$res->sections       = array(
+		'description'  => $remote->sections->description,
+		'installation' => $remote->sections->installation,
+		'changelog'    => $remote->sections->changelog,
+	);
+	if ( ! empty( $remote->sections->screenshots ) ) {
+		$res->sections['screenshots'] = $remote->sections->screenshots;
+	}
+
+	$res->banners = array(
+		'low'  => $remote->banners->low,
+		'high' => $remote->banners->high,
+	);
+
+	return $res;
+}
+
+add_filter( 'site_transient_update_plugins', 'urlslab_push_update' );
+function urlslab_push_update( $transient ) {
+	if ( empty( $transient->checked ) ) {
+		return $transient;
+	}
+
+	$remote = wp_remote_get(
+		URLSLAB_INFO_URL,
+		array(
+			'timeout' => 5,
+			'headers' => array(
+				'Accept' => 'application/json',
+			),
+		)
+	);
+
+	if ( is_wp_error( $remote ) || 200 !== wp_remote_retrieve_response_code( $remote ) || empty( wp_remote_retrieve_body( $remote ) ) ) {
+		return $transient;
+	}
+
+	$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+
+	// your installed plugin version should be on the line below! You can obtain it dynamically of course
+	if (
+		$remote
+		&& version_compare( URLSLAB_VERSION, $remote->version, '<' )
+		&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
+		&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
+	) {
+
+		$res                                 = new stdClass();
+		$res->slug                           = $remote->slug;
+		$res->plugin                         = plugin_basename( __FILE__ ); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
+		$res->new_version                    = $remote->version;
+		$res->tested                         = $remote->tested;
+		$res->package                        = $remote->download_url;
+		$transient->response[ $res->plugin ] = $res;
+
+		if ( property_exists( $remote, 'version' ) ) {
+			$transient->checked[ $res->plugin ] = $remote->version;
+		}
+	}
+
+	return $transient;
+}
+
 register_activation_hook( __FILE__, 'activate_urlslab' );
 register_deactivation_hook( __FILE__, 'deactivate_urlslab' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
 require plugin_dir_path( __FILE__ ) . 'includes/class-urlslab.php';
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
 function run_urlslab() {
 	$plugin = new Urlslab();
 	$plugin->run();
