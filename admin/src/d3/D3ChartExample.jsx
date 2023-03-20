9666092f247e775a05987be143ea4a7ff5415151
vzeman <vzeman@qualityunit.com>
2023-03-20 08:52:30 +0100

    chore: fixed size of font

--- admin/src/d3/D3ChartExample.jsx
+++ admin/src/d3/D3ChartExample.jsx
@@ -11,25 +11,28 @@ const D3ChartExample = ({slug, children}) => {
     const chartRef = useRef(null);
 
     const {data} = useQuery({
-        queryKey: [slug, 'testChart'],
-        queryFn: () => fetchData(`keyword/`).then((chartData) => {
+        queryKey: [slug, 'keyword-cloud'],
+        queryFn: () => fetchData(`keyword?filter_kw_usage_count=%7B%22op%22%3A%22%3E%22%2C%22val%22%3A0%7D&sort_column=link_usage_count&sort_direction=DESC&rows_per_page=1000`).then((chartData) => {
             return chartData;
         }),
         refetchOnWindowFocus: false,
     });
     useEffect(() => {
-        if (!data) return;
+        if (!data || !Array.isArray(data)) return;
+
+        console.log(data);
         // D3 word cloud code goes here
         const layout = cloud()
-            .size([400, 400])
+            .size([800, 800])
             .words(data.map(d => ({text: d.keyword, size: d.link_usage_count})))
             .padding(5)
             .rotate(() => ~~(Math.random() * 2) * 90)
             .font("Impact")
-            .fontSize(d => d.size)
+            .fontSize(d => (d.link_usage_count * 10) + "px")
             .on("end", draw);
 
         layout.start();
+        console.log(layout.words());
 
         function draw(words) {
             d3.select(chartRef.current)
@@ -42,9 +45,9 @@ const D3ChartExample = ({slug, children}) => {
                 .data(words)
                 .enter()
                 .append("text")
-                .style("font-size", d => d.size + "px")
+                .style("font-size", "20px")
                 .style("font-family", "Impact")
-                .style("fill", "#333")
+                .style("fill", "#000000")
                 .attr("text-anchor", "middle")
                 .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
                 .text(d => d.text);
