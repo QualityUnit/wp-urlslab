99b1826208a5b94877f4257f222b65d596cb73d0
Martin Stepanek <martin@stepanek.sk>
2023-03-20 07:17:01 +0100

    chore: fix paths in release pipeline

--- .github/scripts/build-zip.sh
+++ .github/scripts/build-zip.sh
@@ -9,6 +9,8 @@ fi
 
 PLUGIN_ZIP_FILENAME="urlslab-${PACKAGE_VERSION}.zip"
 rm -rf *.zip .git .github docker yarn.lock webpack.config.js postcss.config.js phpcs.xml package.json Makefile composer.lock composer.json commitlint.config.js .stylelintrc .gitignore .eslintrc .eslintignore admin/.eslintignore admin/.eslintrc admin/.stylelintrc admin/index.html admin/package.json admin/vite.config.js admin/yarn.lock
-zip -r $PLUGIN_ZIP_FILENAME . -x "*.zip"
+mkdir urlslab
+mv ./* ./urlslab/
+zip -r $PLUGIN_ZIP_FILENAME ./urlslab/ -x "*.zip"
 echo "PLUGIN_ZIP_FILENAME=${PLUGIN_ZIP_FILENAME}" >> $GITHUB_ENV
 echo "PLUGIN_ZIP_PATH=./**/*" >> $GITHUB_ENV
