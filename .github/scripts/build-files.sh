544c2ca897f3069d5c223934cb4e90858aab0119
Martin Stepanek <martin@stepanek.sk>
2023-03-21 10:56:54 +0100

    fix: build and release pipeline

--- .github/scripts/build-files.sh
+++ .github/scripts/build-files.sh
@@ -2,8 +2,10 @@
 # Inspired by Elementor Releasing Pipeline https://github.com/elementor/elementor
 set -eo pipefail
 
+echo "Remove Old Dist - Start"
 rm -rf ./admin/dist
 rm -rf ./public/build
+echo "Remove Old Dist - End"
 
 echo "Composer Install"
 composer install
