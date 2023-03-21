8661f8982660f7f66a671b14e4c331662509097a
Jozef Remeň <jozef.remen@gmail.com>
2023-03-21 14:19:34 +0100

    feat: editing filters

--- admin/src/components/TableFilter.jsx
+++ admin/src/components/TableFilter.jsx
@@ -12,7 +12,9 @@ export default function TableFilter( { slug, header, onFilter } ) {
 	const { __ } = useI18n();
 	const { filters, currentFilters, addFilter, removeFilters } = useFilter( { slug } );
 	const ref = useRef( null );
+	const currentFilter = useRef( );
 	const [ panelActive, activatePanel ] = useState( false );
+	// const [ possibleFilters, setPossibleFilters ] = useState( header );
 	const activeFilters = Object.keys( currentFilters );
 
 	useEffect( () => {
@@ -25,21 +27,60 @@ export default function TableFilter( { slug, header, onFilter } ) {
 		document.addEventListener( 'click', handleClickOutside, false );
 	}, [ panelActive ] );
 
-	if ( onFilter && filters ) {
-		onFilter( { filters, currentFilters } );
-	}
-
-	const possibleFilters = useMemo( () => {
-		if ( header ) {
-			return header;
-		}
-		return undefined;
+	const filtering = useMemo( () => {
+		return {
+			usedFilters: [], possibleFilters: { ...header } };
 	}, [ header ] );
-	// addFilter('kwType', 'M')
+
+	const handleSaveFilter = () => {
+		let key = currentFilter.current;
+		if ( ! key ) {
+			key = Object.keys( filtering.possibleFilters )[ 0 ];
+		}
+		filtering.usedFilters.push( key );
+		delete filtering.possibleFilters[ key ];
+		activatePanel( false );
+
+		addFilter( key, 'M' );
+
+		// if (onFilter && filters) {
+		// 	onFilter({ filters, currentFilters });
+		// }
+	};
+
+	const handleEditFilter = () => {
+		activatePanel( true );
+	};
+
+	const handleRemoveFilter = ( keysArray ) => {
+		if ( keysArray.length === 1 ) {
+			const key = keysArray[ 0 ];
+			const val = header[ key ];
+			filtering.usedFilters = filtering.usedFilters.filter( ( k ) => k !== key );
+			filtering.possibleFilters = { [ key ]: `${ val }`, ...filtering.possibleFilters };
+		}
+		if ( keysArray.length > 1 ) {
+			filtering.usedFilters = [];
+			filtering.possibleFilters = { ...header };
+		}
+		removeFilters( keysArray );
+	};
+
+	console.log( filtering.usedFilters );
+	console.log( filtering.possibleFilters );
+	// console.log( header );
+
 	return (
-		<div className="flex flex-align-center">
-			{ possibleFilters && activeFilters.map( ( key ) => {
-				return ( <Button className="outline ml-s" key={ key } onClick={ () => removeFilters( [ key ] ) }>{ possibleFilters[ key ] }<CloseIcon className="close" /></Button> );
+		<div className="flex flex-align-center flex-wrap">
+			{ header && activeFilters.map( ( key ) => {
+				return <div className="urlslab-button outline ml-s" key={ key }>
+					<Button
+						className="simple"
+						onClick={ () => handleEditFilter() }>
+						{ header[ key ] }
+					</Button>
+					<CloseIcon className="close" onClick={ () => handleRemoveFilter( [ key ] ) } />
+				</div>;
 			} ) }
 
 			<div ref={ ref } className="pos-relative">
@@ -49,22 +90,23 @@ export default function TableFilter( { slug, header, onFilter } ) {
 				{ panelActive &&
 					<div className="urlslab-panel urslab-TableFilter-panel pos-absolute">
 						<SortMenu
-							items={ possibleFilters }
+							items={ filtering.possibleFilters }
 							name="filters"
-							checkedId={ Object.keys( possibleFilters )[ 0 ] }
-							onChange={ ( val ) => val }
+							ref={ currentFilter }
+							checkedId={ Object.keys( filtering.possibleFilters )[ 0 ] }
+							onChange={ ( val ) => currentFilter.current = val }
 						/>
 
 						<div className="Buttons flex flex-align-center">
 							<Button className="simple" onClick={ () => activatePanel( false ) }>{ __( 'Cancel' ) }</Button>
-							<Button active>{ __( 'Save' ) }</Button>
+							<Button active onClick={ handleSaveFilter }>{ __( 'Save' ) }</Button>
 						</div>
 					</div>
 				}
 			</div>
 
 			{ activeFilters?.length > 0 &&
-			<Button className="simple underline" onClick={ () => removeFilters( activeFilters ) }>{ __( 'Clear filters' ) }</Button>
+				<Button className="simple underline" onClick={ () => handleRemoveFilter( activeFilters ) }>{ __( 'Clear filters' ) }</Button>
 			}
 
 		</div>
