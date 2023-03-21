3924ae84bc472a5c9e77ab2799ca3ff14e387231
Martin Stepanek <martin@stepanek.sk>
2023-03-21 11:59:57 +0100

    fix: build and release pipeline

--- .github/scripts/build-files.sh
+++ .github/scripts/build-files.sh
@@ -2,9 +2,15 @@
 # Inspired by Elementor Releasing Pipeline https://github.com/elementor/elementor
 set -eo pipefail
 
+echo "Remove Old Dist"
+rm -rf ./admin/dist
+rm -rf ./public/build
+
 echo "Composer Install"
 composer install
+
 echo "Yarn Install"
 yarn
+
 echo "Build"
 yarn build
