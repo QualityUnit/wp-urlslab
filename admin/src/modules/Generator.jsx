f8f65f27648bd4b4aded468ea50bc9b9984e4294
vzeman <vzeman@qualityunit.com>
2023-03-23 12:33:09 +0100

    feat: Generators table

--- admin/src/modules/Generator.jsx
+++ admin/src/modules/Generator.jsx
@@ -3,20 +3,32 @@ import { useState, Suspense, lazy } from 'react';
 import Overview from '../components/OverviewTemplate';
 import GeneratorOverview from '../overview/Generator';
 import ModuleViewHeader from '../components/ModuleViewHeader';
+import { useI18n } from '@wordpress/react-i18n';
 
 export default function Generator( { moduleId } ) {
 	const [ activeSection, setActiveSection ] = useState( 'overview' );
-
+	const { __ } = useI18n();
 	const SettingsModule = lazy( () => import( `../modules/Settings.jsx` ) );
+	const GeneratorTable = lazy( () => import( `../tables/GeneratorTable.jsx` ) );
+	const slug = 'content-generator';
+	const tableMenu = new Map( [
+		[ slug, __( 'Generators' ) ],
+	] );
 
 	return (
 		<div className="urlslab-tableView">
-			<ModuleViewHeader activeMenu={ ( activemenu ) => setActiveSection( activemenu ) } />
+			<ModuleViewHeader moduleMenu={ tableMenu } activeMenu={ ( activemenu ) => setActiveSection( activemenu ) } />
 			{
 				activeSection === 'overview' &&
 				<Overview moduleId={ moduleId }>
 					<GeneratorOverview />
 				</Overview>
+			}
+			{
+				activeSection === slug &&
+				<Suspense>
+					<GeneratorTable slug={ slug } />
+				</Suspense>
 			}
 			{
 				activeSection === 'settings' &&
