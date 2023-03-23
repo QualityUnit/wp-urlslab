b6553f669f3ce43934e6aa79af5bcdf04a9f979e
vzeman <vzeman@qualityunit.com>
2023-03-23 21:02:20 +0100

    fix: code formatting

--- includes/widgets/class-urlslab-related-resources-widget.php
+++ includes/widgets/class-urlslab-related-resources-widget.php
@@ -72,8 +72,8 @@ class Urlslab_Related_Resources_Widget extends Urlslab_Widget {
 		);
 
 		try {
-			$current_url = new Urlslab_Url( $urlslab_atts['url'] );
-			$current_url_obj = Urlslab_Url_Data_Fetcher::get_instance()->fetch_schedule_url($current_url);
+			$current_url     = new Urlslab_Url( $urlslab_atts['url'] );
+			$current_url_obj = Urlslab_Url_Data_Fetcher::get_instance()->fetch_schedule_url( $current_url );
 			$current_url_obj->request_rel_schedule();
 
 			if ( Urlslab_Url_Row::REL_AVAILABLE !== $current_url_obj->get_rel_schedule() ) {
