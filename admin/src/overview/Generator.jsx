857a9e3bee91cac8bc7e0845799deed8e247f84f
vzeman <vzeman@qualityunit.com>
2023-03-23 14:27:30 +0100

    feat: generator default-value

--- admin/src/overview/Generator.jsx
+++ admin/src/overview/Generator.jsx
@@ -5,7 +5,7 @@ export default function GeneratorOverview() {
 			<p>Generate with AI content in your website with simple shortcode. AI takes into consideration the content of your website related to the query.</p>
 			<p>
 				Shortcode
-				<code>[urlslab-generator template="templates/summary.php" query="Summarize in langueage |$lang|" context="https://www.liveagent.com,https://www.postaffiliatepro"]</code>
+				<code>[urlslab-generator template="templates/summary.php" query="Summarize text from context and return output in language |$lang|" context="https://www.liveagent.com,https://www.postaffiliatepro" default-value="This is my default summarization ..."]</code>
 			</p>
 		</>
 	);
