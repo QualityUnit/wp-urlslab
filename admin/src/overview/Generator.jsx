e5103a1b6f984b3ab885e66bc4c9d8a7df798259
Martin Stepanek <martin@stepanek.sk>
2023-03-27 15:22:42 +0200

    fix: small fixes and improvements (#555)

--- admin/src/overview/Generator.jsx
+++ admin/src/overview/Generator.jsx
@@ -1,12 +1,11 @@
-
 export default function GeneratorOverview() {
 	return (
 		<>
-			<p>Generate with AI content in your website with simple shortcode. AI takes into consideration the content of your website related to the query.</p>
-			<p>
-				Shortcode
-				<code>[urlslab-generator template="templates/summary.php" query="Summarize text from context and return output in language |$lang|" context="https://www.liveagent.com,https://www.postaffiliatepro" default-value="This is my default summarization ..."]</code>
-			</p>
+			<p>Discover the ultimate solution to elevating your website's content with our state-of-the-art AI-powered module. Designed to simplify and streamline content creation, our module generates unique and engaging text tailored to your specific needs. With the intuitive shortcode or Gutenberg block integration, crafting captivating content has never been easier.</p>
+			<p>Harnessing the power of the advanced GPT model version 4 or older 3.5, our plugin ensures top-notch quality and seamless adaptability for your site. Say goodbye to time-consuming content generation and writer's block, as our AI module takes care of all your content needs with ease.</p>
+			<p>Upgrade your website today with our AI-driven module and experience the unparalleled benefits of dynamic, high-quality content that not only captivates your audience but also enhances your site's SEO performance. Don't settle for subpar content – step into the future of web copywriting with our groundbreaking module.</p>
+			<h4>Shortcode</h4>
+			<code>[urlslab-generator template="templates/summary.php" query="Summarize text from context and return output in language |$lang|" context="https://www.liveagent.com,https://www.postaffiliatepro" default-value="This is my default summarization ..."]</code>
 		</>
 	);
 }
