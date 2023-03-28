490ff26032962162b5f28b168a2a91fddcb557e4
Jozef Remeň <jozef.remen@gmail.com>
2023-03-28 17:42:33 +0200

    fix: returning back overwritten properties

--- admin/src/components/TableComponent.jsx
+++ admin/src/components/TableComponent.jsx
@@ -77,7 +77,7 @@ export default function Table( { resizable, children, className, columns, data, 
 					return (
 						<td key={ cell.id } className={ cell.column.columnDef.className }
 							style={ {
-							position: resizable ? 'absolute' : 'relative',
+							position: resizable ? 'absolute' : 'static',
 							left: resizable ? cell.column.getStart() : '0',
 							width: cell.column.getSize() !== 0 && resizable
 								? cell.column.getSize()
