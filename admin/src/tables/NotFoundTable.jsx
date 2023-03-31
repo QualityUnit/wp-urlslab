e49e63922bbf5e46c6398a5d65c71794d7487bf0
vzeman <vzeman@qualityunit.com>
2023-03-31 09:08:53 +0200

    feat: default redirects of 404, redirect logging

---
+++ admin/src/tables/NotFoundTable.jsx
@@ -0,0 +1,106 @@
+import { useMemo } from 'react';
+import {
+	useInfiniteFetch, Tooltip, Checkbox, Trash, Loader, Table, ModuleViewHeaderBottom,
+} from '../constants/tableImports';
+
+import useTableUpdater from '../hooks/useTableUpdater';
+import useChangeRow from '../hooks/useChangeRow';
+
+export default function NotFoundTable( { slug } ) {
+	const pageId = 'url_id';
+
+	const { table, setTable, filters, setFilters, sortingColumn, sortBy } = useTableUpdater( { slug } );
+
+	const url = useMemo( () => `${ filters }${ sortingColumn }`, [ filters, sortingColumn ] );
+
+	const {
+		__,
+		columnHelper,
+		data,
+		status,
+		isSuccess,
+		isFetchingNextPage,
+		hasNextPage,
+		ref,
+	} = useInfiniteFetch( { key: slug, url, pageId } );
+
+	const { row, selectRow, deleteRow } = useChangeRow( { data, url, slug, pageId } );
+
+	const header = {
+		url: __( 'URL' ),
+		cnt: __( 'Visits' ),
+		created: __( 'First Visit' ),
+		updated: 'Last Visit',
+	};
+
+	const columns = [
+		columnHelper.accessor( 'check', {
+			className: 'nolimit checkbox',
+			cell: ( cell ) => <Checkbox checked={ cell.row.getIsSelected() } onChange={ ( val ) => {
+				selectRow( val, cell );
+			} } />,
+			header: null,
+		} ),
+		columnHelper.accessor( 'url', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			header: header.url,
+			minSize: 300,
+		} ),
+		columnHelper.accessor( 'cnt', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			header: header.cnt,
+			minSize: 50,
+		} ),
+		columnHelper.accessor( 'created', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			header: header.created,
+			minSize: 100,
+		} ),
+		columnHelper.accessor( 'updated', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			header: header.updated,
+			minSize: 100,
+		} ),
+		columnHelper.accessor( 'delete', {
+			className: 'deleteRow',
+			cell: ( cell ) => <Trash onClick={ () => deleteRow( { cell } ) } />,
+			header: null,
+		} ),
+	];
+
+	if ( status === 'loading' ) {
+		return <Loader />;
+	}
+
+	return (
+		<>
+			<ModuleViewHeaderBottom
+				slug={ slug }
+				header={ header }
+				table={ table }
+				onSort={ ( val ) => sortBy( val ) }
+				onFilter={ ( filter ) => setFilters( filter ) }
+				onClearRow={ ( clear ) => clear }
+				exportOptions={ {
+					url: slug,
+					filters,
+					fromId: `from_${ pageId }`,
+					pageId,
+					deleteCSVCols: [ pageId, 'dest_url_id' ],
+				} }
+			/>
+			<Table className="fadeInto"
+				slug={ slug }
+				returnTable={ ( returnTable ) => setTable( returnTable ) }
+				columns={ columns }
+				data={ isSuccess && data?.pages?.flatMap( ( page ) => page ?? [] ) }
+			>
+				{ row
+					? <Tooltip center>{ `${ header.url } “${ row.url }”` } { __( 'has been deleted.' ) }</Tooltip>
+					: null
+				}
+				<button ref={ ref }>{ isFetchingNextPage ? 'Loading more...' : hasNextPage }</button>
+			</Table>
+		</>
+	);
+}
---
+++ admin/src/tables/RedirectsTable.jsx
@@ -0,0 +1,123 @@
+import { useMemo } from 'react';
+import {
+	useInfiniteFetch, Tooltip, SortMenu, InputField, Checkbox, Trash, Loader, Table, ModuleViewHeaderBottom,
+} from '../constants/tableImports';
+
+import useTableUpdater from '../hooks/useTableUpdater';
+import useChangeRow from '../hooks/useChangeRow';
+
+export default function RedirectsTable( { slug } ) {
+	const pageId = 'id';
+
+	const { table, setTable, rowToInsert, setInsertRow, filters, setFilters, sortingColumn, sortBy } = useTableUpdater( { slug } );
+
+	const url = useMemo( () => `${ filters }${ sortingColumn }`, [ filters, sortingColumn ] );
+
+	const {
+		__,
+		columnHelper,
+		data,
+		status,
+		isSuccess,
+		isFetchingNextPage,
+		hasNextPage,
+		ref,
+	} = useInfiniteFetch( { key: slug, url, pageId } );
+
+	const { row, selectRow, deleteRow, updateRow } = useChangeRow( { data, url, slug, pageId } );
+
+	const searchTypes = {
+		T: __( 'Plain Text' ),
+		R: __( 'Regular Expr.' ),
+	};
+
+	const header = {
+		str_search: __( 'Search string' ),
+		str_replace: __( 'Replace string' ),
+		search_type: __( 'Search Type' ),
+		url_filter: 'URL Filter',
+	};
+
+	const inserterCells = {
+		str_search: <InputField liveUpdate type="url" defaultValue="" label={ header.str_search } onChange={ ( val ) => setInsertRow( { ...rowToInsert, str_search: val } ) } required />,
+		str_replace: <InputField liveUpdate type="url" defaultValue="" label={ header.str_replace } onChange={ ( val ) => setInsertRow( { ...rowToInsert, str_replace: val } ) } required />,
+		search_type: <SortMenu autoClose items={ searchTypes } name="search_type" checkedId="T" onChange={ ( val ) => setInsertRow( { ...rowToInsert, search_type: val } ) }>{ header.search_type }</SortMenu>,
+		url_filter: <InputField liveUpdate defaultValue="" label={ header.url_filter } onChange={ ( val ) => setInsertRow( { ...rowToInsert, url_filter: val } ) } />,
+	};
+
+	const columns = [
+		columnHelper.accessor( 'check', {
+			className: 'nolimit checkbox',
+			cell: ( cell ) => <Checkbox checked={ cell.row.getIsSelected() } onChange={ ( val ) => {
+				selectRow( val, cell );
+			} } />,
+			header: null,
+		} ),
+		columnHelper.accessor( 'str_search', {
+			className: 'nolimit',
+			cell: ( cell ) => <InputField type="text" defaultValue={ cell.getValue() } onChange={ ( newVal ) => updateRow( { newVal, cell } ) } />,
+			header: header.str_search,
+			size: 300,
+		} ),
+		columnHelper.accessor( 'str_replace', {
+			className: 'nolimit',
+			cell: ( cell ) => <InputField type="text" defaultValue={ cell.getValue() } onChange={ ( newVal ) => updateRow( { newVal, cell } ) } />,
+			header: header.str_replace,
+			size: 300,
+		} ),
+		columnHelper.accessor( 'search_type', {
+			className: 'nolimit',
+			cell: ( cell ) => <SortMenu items={ searchTypes } name={ cell.column.id } checkedId={ cell.getValue() } onChange={ ( newVal ) => updateRow( { newVal, cell } ) } />,
+			header: header.search_type,
+			size: 100,
+		} ),
+		columnHelper.accessor( 'url_filter', {
+			className: 'nolimit',
+			cell: ( cell ) => <InputField type="text" defaultValue={ cell.getValue() } onChange={ ( newVal ) => updateRow( { newVal, cell } ) } />,
+			header: header.url_filter,
+			size: 100,
+		} ),
+		columnHelper.accessor( 'delete', {
+			className: 'deleteRow',
+			cell: ( cell ) => <Trash onClick={ () => deleteRow( { cell } ) } />,
+			header: () => null,
+		} ),
+	];
+
+	if ( status === 'loading' ) {
+		return <Loader />;
+	}
+
+	return (
+		<>
+			<ModuleViewHeaderBottom
+				slug={ slug }
+				header={ header }
+				table={ table }
+				onSort={ ( val ) => sortBy( val ) }
+				onFilter={ ( filter ) => setFilters( filter ) }
+				onClearRow={ ( clear ) => clear && setInsertRow() }
+				insertOptions={ { inserterCells, title: 'Add replacement', data, slug, url, pageId, rowToInsert } }
+				exportOptions={ {
+					url: slug,
+					filters,
+					fromId: `from_${ pageId }`,
+					pageId,
+					deleteCSVCols: [ pageId, 'dest_url_id' ],
+				} }
+			/>
+			<Table className="fadeInto"
+				slug={ slug }
+				returnTable={ ( returnTable ) => setTable( returnTable ) }
+				columns={ columns }
+				data={ isSuccess && data?.pages?.flatMap( ( page ) => page ?? [] ) }
+			>
+				{ row
+					? <Tooltip center>{ `${ header.str_search } “${ row.str_search }”` } { __( 'has been deleted.' ) }</Tooltip>
+					: null
+				}
+				<button ref={ ref }>{ isFetchingNextPage ? 'Loading more...' : hasNextPage }</button>
+			</Table>
+		</>
+	);
+}
