e1fbf985f8db57237e129a56fba6400ceb176119
mvstepanek <martin@stepanek.sk>
2023-04-03 12:26:10 +0000

    chore: bump version to 2.5.5

--- urlslab.php
+++ urlslab.php
@@ -16,7 +16,7 @@
  * Plugin Name:       URLsLab
  * Plugin URI:        https://github.com/QualityUnit/wp-urlslab
  * Description:       URLsLab WordPress Plugin to optimize your website for search engines and enhance automatically content
- * Version: 2.5.4
+ * Version: 2.5.5
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            URLsLab
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'URLSLAB_VERSION', '2.5.4' );
+define( 'URLSLAB_VERSION', '2.5.5' );
 define( 'URLSLAB_VERSION_SETTING', 'urlslab_ver' );
 define( 'URLSLAB_PLUGIN', __FILE__ );
 
