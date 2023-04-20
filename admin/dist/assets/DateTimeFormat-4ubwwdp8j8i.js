877ab937669ad2329ff4d15b08554ea283af5d57
mvstepanek <martin@stepanek.sk>
2023-04-20 07:52:16 +0000

    chore: bump version to 2.13.4

---
+++ admin/dist/assets/DateTimeFormat-4ubwwdp8j8i.js
@@ -0,0 +1,8 @@
+import { R as React } from "../main-4ubwwdp8j8i.js";
+function DateTimeFormat({ datetime }) {
+  const date = new Date(datetime);
+  return /* @__PURE__ */ React.createElement(React.Fragment, null, date.toLocaleDateString(window.navigator.language), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("span", { className: "c-grey-darker" }, date.toLocaleTimeString(window.navigator.language)));
+}
+export {
+  DateTimeFormat as D
+};
