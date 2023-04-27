e53433f7f5e981e2b1d2427c04be17d8bdb58dee
vzeman <vzeman@qualityunit.com>
2023-04-27 12:49:13 +0200

    feat: roles api

--- includes/api/class-urlslab-api-redirects.php
+++ includes/api/class-urlslab-api-redirects.php
@@ -163,37 +163,6 @@ class Urlslab_Api_Redirects extends Urlslab_Api_Table {
 			)
 		);
 
-		register_rest_route(
-			self::NAMESPACE,
-			$base . '/capabilities',
-			array(
-				array(
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'get_capabilities' ),
-					'permission_callback' => array(
-						$this,
-						'get_items_permissions_check',
-					),
-					'args'                => array(),
-				),
-			)
-		);
-
-		register_rest_route(
-			self::NAMESPACE,
-			$base . '/roles',
-			array(
-				array(
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'get_roles' ),
-					'permission_callback' => array(
-						$this,
-						'get_items_permissions_check',
-					),
-					'args'                => array(),
-				),
-			)
-		);
 	}
 
 	/**
@@ -312,34 +281,6 @@ class Urlslab_Api_Redirects extends Urlslab_Api_Table {
 		return new WP_REST_Response( $rows, 200 );
 	}
 
-	public function get_capabilities( $request ) {
-		global $wp_roles;
-		$all_capabilities = array();
-
-		// Loop through all roles
-		foreach ( $wp_roles->roles as $role_key => $role ) {
-			foreach ( $role['capabilities'] as $capability => $value ) {
-				$all_capabilities[ $capability ] = (object) array( 'capability' => $capability );
-			}
-		}
-
-		return new WP_REST_Response( array_values( $all_capabilities ), 200 );
-	}
-
-	public function get_roles( $request ) {
-		global $wp_roles;
-		$all_roles = array();
-
-		// Loop through all roles
-		foreach ( $wp_roles->roles as $role_key => $role ) {
-			$all_roles[] = (object) array(
-				'role_key' => $role_key,
-				'role'     => $role,
-			);
-		}
-
-		return new WP_REST_Response( $all_roles, 200 );
-	}
 
 	public function get_row_object( $params = array() ): Urlslab_Data {
 		return new Urlslab_Redirect_Row( $params );
