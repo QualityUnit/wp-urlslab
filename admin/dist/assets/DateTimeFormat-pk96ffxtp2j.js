421d0fe7a04a33546cc6a192fda62b52c2e934cc
mvstepanek <martin@stepanek.sk>
2023-04-27 11:58:48 +0000

    chore: bump version to 2.16.2

--- admin/dist/assets/DateTimeFormat-pk96ffxtp2j.js
+++
@@ -1,8 +0,0 @@
-import { R as React } from "../main-pk96ffxtp2j.js";
-function DateTimeFormat({ datetime }) {
-  const date = new Date(datetime);
-  return /* @__PURE__ */ React.createElement(React.Fragment, null, date.toLocaleDateString(window.navigator.language), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("span", { className: "c-grey-darker" }, date.toLocaleTimeString(window.navigator.language)));
-}
-export {
-  DateTimeFormat as D
-};
