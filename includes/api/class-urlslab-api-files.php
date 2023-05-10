09fc9f3252c90be43ecc0abf69d620850c25b4f1
vzeman <vzeman@qualityunit.com>
2023-05-10 15:12:33 +0200

    chore: phpcs reformatting code

--- includes/api/class-urlslab-api-files.php
+++ includes/api/class-urlslab-api-files.php
@@ -205,7 +205,9 @@ class Urlslab_Api_Files extends Urlslab_Api_Table {
 	}
 
 	public function validate_s3( WP_REST_Request $request ) {
-		if  ( Urlslab_Driver::get_driver(Urlslab_Driver::DRIVER_S3)->is_connected() ) {
+		/** @var Urlslab_Driver_S3 $driver */
+		$driver = Urlslab_Driver::get_driver( Urlslab_Driver::DRIVER_S3 );
+		if  ( $driver->is_connected() ) {
 			return new WP_REST_Response( __( 'S3 connected' ), 200 );
 		} else {
 			return new WP_REST_Response( __( 'S3 not connected' ), 500 );
