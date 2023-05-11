9336948f570a58f86e56e1ae082f038e08f4115f
Jozef Remeň <jozef.remen@gmail.com>
2023-05-11 09:14:28 +0200

    feat: wysiwig editor

---
+++ admin/src/elements/TinyMCE.jsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+export default function TinyMCE( { defaultValue } ) {
+	const editorRef = useRef( null );
+	return (
+		<>
+			<Editor
+				onInit={ ( evt, editor ) => editorRef.current = editor }
+				initialValue={ defaultValue }
+				init={ {
+					height: 500,
+					menubar: false,
+					plugins: [
+						'advlist autolink lists link image charmap print preview anchor',
+						'searchreplace visualblocks code fullscreen',
+						'insertdatetime media table paste code help wordcount',
+					],
+					toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+				} }
+			/>
+		</>
+	);
+}
