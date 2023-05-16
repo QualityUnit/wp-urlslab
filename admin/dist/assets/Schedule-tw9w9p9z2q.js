432661db707b960e48b5b4f27cea5dbcad294ed3
mvstepanek <martin@stepanek.sk>
2023-05-16 08:58:38 +0000

    chore: bump version to 2.21.14

--- admin/dist/assets/Schedule-tw9w9p9z2q.js
+++
@@ -1,9 +0,0 @@
-import { r as reactExports, _ as __vitePreload, R as React } from "../main-tw9w9p9z2q.js";
-function Schedule() {
-  const slug = "schedule";
-  const SchedulesTable = reactExports.lazy(() => __vitePreload(() => import("./SchedulesTable-tw9w9p9z2q.js"), true ? ["./SchedulesTable-tw9w9p9z2q.js","../main-tw9w9p9z2q.js","./main.css","./useTableUpdater-tw9w9p9z2q.js","./_ColumnsMenu-tw9w9p9z2q.js","./_ColumnsMenu-tw9w9p9z2q.css","./InputField-tw9w9p9z2q.js","./datepicker-tw9w9p9z2q.css","./useMutation-tw9w9p9z2q.js","./Tooltip_SortingFiltering-tw9w9p9z2q.js"] : void 0, import.meta.url));
-  return /* @__PURE__ */ React.createElement("div", { className: "urlslab-tableView" }, /* @__PURE__ */ React.createElement(reactExports.Suspense, null, /* @__PURE__ */ React.createElement(SchedulesTable, { slug })));
-}
-export {
-  Schedule as default
-};
---
+++ admin/dist/assets/URLRelationTable-f4qgngjv6p.js
@@ -0,0 +1,121 @@
+import { R as React, T as Tooltip, L as Loader } from "../main-f4qgngjv6p.js";
+import { u as useTableUpdater, a as useInfiniteFetch, b as useChangeRow, S as SvgIconTrash, M as ModuleViewHeaderBottom, T as Table, P as ProgressBar } from "./useTableUpdater-f4qgngjv6p.js";
+import { S as SortBy } from "./_ColumnsMenu-f4qgngjv6p.js";
+import { T as TooltipSortingFiltering } from "./Tooltip_SortingFiltering-f4qgngjv6p.js";
+/* empty css                       */import { C as Checkbox, I as InputField } from "./InputField-f4qgngjv6p.js";
+import "./useMutation-f4qgngjv6p.js";
+function URLRelationTable({ slug }) {
+  var _a;
+  const paginationId = "src_url_id";
+  const { table, setTable, filters, setFilters, sorting, sortBy } = useTableUpdater({ slug });
+  const url = { filters, sorting };
+  const {
+    __,
+    columnHelper,
+    data,
+    status,
+    isSuccess,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    ref
+  } = useInfiniteFetch({ key: slug, filters, sorting, paginationId });
+  const { row, selectedRows, selectRow, rowToEdit, setEditorRow, deleteRow, deleteSelectedRows, updateRow } = useChangeRow({ data, url, slug, paginationId });
+  const header = {
+    src_url_name: __("Source URL"),
+    dest_url_name: __("Destination URL"),
+    pos: __("Position")
+  };
+  const rowEditorCells = {
+    src_url_name: /* @__PURE__ */ React.createElement(InputField, { liveUpdate: true, type: "url", defaultValue: "", label: header.src_url_name, onChange: (val) => setEditorRow({ ...rowToEdit, src_url_name: val }), required: true }),
+    dest_url_name: /* @__PURE__ */ React.createElement(InputField, { liveUpdate: true, type: "url", defaultValue: "", label: header.dest_url_name, onChange: (val) => setEditorRow({ ...rowToEdit, dest_url_name: val }), required: true }),
+    pos: /* @__PURE__ */ React.createElement(InputField, { liveUpdate: true, type: "number", defaultValue: "0", min: "0", max: "255", label: header.pos, onChange: (val) => setEditorRow({ ...rowToEdit, pos: val }), required: true })
+  };
+  const columns = [
+    columnHelper.accessor("check", {
+      className: "checkbox",
+      cell: (cell) => /* @__PURE__ */ React.createElement(Checkbox, { checked: cell.row.getIsSelected(), onChange: (val) => {
+        selectRow(val, cell);
+      } }),
+      header: null
+    }),
+    columnHelper.accessor("src_url_name", {
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, null, cell.getValue()),
+      cell: (cell) => /* @__PURE__ */ React.createElement("a", { href: cell.getValue(), target: "_blank", rel: "noreferrer" }, cell.getValue()),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.src_url_name),
+      size: 400
+    }),
+    columnHelper.accessor("dest_url_name", {
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, null, cell.getValue()),
+      cell: (cell) => /* @__PURE__ */ React.createElement("a", { href: cell.getValue(), target: "_blank", rel: "noreferrer" }, cell.getValue()),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.dest_url_name),
+      size: 400
+    }),
+    columnHelper.accessor("pos", {
+      className: "nolimit",
+      cell: (cell) => /* @__PURE__ */ React.createElement(
+        InputField,
+        {
+          type: "number",
+          defaultValue: cell.getValue(),
+          onChange: (newVal) => updateRow({ newVal, cell, optionalSelector: "dest_url_id" })
+        }
+      ),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.pos),
+      size: 80
+    }),
+    columnHelper.accessor("editRow", {
+      className: "editRow",
+      tooltip: () => /* @__PURE__ */ React.createElement(Tooltip, { className: "align-left xxxl" }, __("Delete item")),
+      cell: (cell) => /* @__PURE__ */ React.createElement(SvgIconTrash, { onClick: () => deleteRow({ cell, optionalSelector: "dest_url_id" }) }),
+      header: null
+    })
+  ];
+  if (status === "loading") {
+    return /* @__PURE__ */ React.createElement(Loader, null);
+  }
+  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(
+    ModuleViewHeaderBottom,
+    {
+      slug,
+      header,
+      table,
+      selectedRows,
+      onDeleteSelected: () => deleteSelectedRows("dest_url_id"),
+      onFilter: (filter) => setFilters(filter),
+      onUpdateRow: (val) => {
+        setEditorRow();
+        if (val === "rowInserted" || val === "rowChanged") {
+          setEditorRow(val);
+          setTimeout(() => {
+            setEditorRow();
+          }, 3e3);
+        }
+      },
+      rowEditorOptions: { rowEditorCells, title: "Add related article", data, slug, url, paginationId, rowToEdit },
+      exportOptions: {
+        slug,
+        url,
+        paginationId,
+        deleteCSVCols: [paginationId, "dest_url_id"]
+      }
+    }
+  ), /* @__PURE__ */ React.createElement(
+    Table,
+    {
+      className: "fadeInto",
+      slug,
+      returnTable: (returnTable) => setTable(returnTable),
+      columns,
+      data: isSuccess && ((_a = data == null ? void 0 : data.pages) == null ? void 0 : _a.flatMap((page) => page ?? []))
+    },
+    row ? /* @__PURE__ */ React.createElement(Tooltip, { center: true }, __("Item has been deleted.")) : null,
+    rowToEdit === "rowChanged" ? /* @__PURE__ */ React.createElement(Tooltip, { center: true }, __("Item has been changed.")) : null,
+    rowToEdit === "rowInserted" ? /* @__PURE__ */ React.createElement(Tooltip, { center: true }, __("Item has been added.")) : null,
+    /* @__PURE__ */ React.createElement(TooltipSortingFiltering, { props: { isFetching, filters, sorting } }),
+    /* @__PURE__ */ React.createElement("div", { ref }, isFetchingNextPage ? "" : hasNextPage, /* @__PURE__ */ React.createElement(ProgressBar, { className: "infiniteScroll", value: !isFetchingNextPage ? 0 : 100 }))
+  ));
+}
+export {
+  URLRelationTable as default
+};
