72beda75a07d4949d3c33578b55e41ae56c32f3a
vzeman <vzeman@qualityunit.com>
2023-05-18 15:32:21 +0200

    feat: schedules menu extended for overview and credits

--- admin/src/overview/Schedules.jsx
+++ admin/src/overview/Schedules.jsx
@@ -4,6 +4,7 @@ export default function SchedulesOverview() {
 			<h1>Urlslab service</h1>
 			<h2>How can paid service help my Wordpress?</h2>
 			<p>Even you can use our plugin for free and laverage from multiple cool features, integration with our paid service will boost your web in the eyes of search engines thanks to following features:</p>
+			<p>Here should be explanation how credits work, what type of actions we charge and why sometime we return the credit to user (e.g. when we discover, that url doesn't need to be crawled</p>
 		</>
 	);
 }
