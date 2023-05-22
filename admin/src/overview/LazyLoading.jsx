e32cd235e3fd096e347c8f5f7dc03059c70deda4
vzeman <vzeman@qualityunit.com>
2023-05-22 06:16:06 +0200

    chore: added description of video attribute shortcode

--- admin/src/overview/LazyLoading.jsx
+++ admin/src/overview/LazyLoading.jsx
@@ -14,6 +14,9 @@ export default function LazyLoadingOverview( { moduleId } ) {
 				<section>
 					<p>Lazy loading is an essential technique for optimizing page performance and reducing page load time. By deferring loading of images, videos, iframes, and large content chunks, you can drastically improve the speed at which pages load. This is especially important for those with slower internet connections, who are more likely to be affected by page loading times.</p>
 					<p>The main idea behind lazy loading is to delay loading assets until they are actually needed. This means that instead of loading all assets simultaneously, the browser will only load them when they are visible on the user’s screen. It decreases the amount of data that needs to be transferred and therefore increases page loading speed. It also reduces server load, as the server does not have to process every asset simultaneously.</p>
+					<h3>Video data shortcode</h3>
+					<pre>[urlslab-video videoid="YT-video-ID" attribute="atr_name"]</pre>
+					<p>Supported video attribute names: title, description, thumbnail_url, published_at, duration, captions, captions_text, channel_title</p>
 				</section>
 			}
 		</Overview>
