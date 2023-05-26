20f89178a00967751129ebcb30e46b929bfc1bfa
vzeman <vzeman@qualityunit.com>
2023-05-26 12:06:16 +0200

    fix: lx loading

--- includes/class-urlslab.php
+++ includes/class-urlslab.php
@@ -509,6 +509,7 @@ class Urlslab {
 		if ( ! defined( 'DOING_AJAX' ) && ! wp_installing() ) {
 			$plugin_public = new Urlslab_Public( $this->get_urlslab(), $this->get_version() );
 			Urlslab_Loader::get_instance()->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+			Urlslab_Loader::get_instance()->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 			Urlslab_Loader::get_instance()->add_action( 'template_redirect', $plugin_public, 'download_offloaded_file', PHP_INT_MIN );
 			if ( ! defined( 'WP_ADMIN' ) ) {
 				Urlslab_Loader::get_instance()->add_action( 'wp_loaded', $this, 'buffer_start', PHP_INT_MAX );
