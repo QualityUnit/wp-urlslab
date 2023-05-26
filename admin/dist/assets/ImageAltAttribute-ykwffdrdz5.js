c3ba110ae59c572ff7837d64efa96c414150b70f
QualityUnitWebCI <dev-ci-web@qualityunit.com>
2023-05-26 09:15:59 +0000

    chore: bump version to 2.25.9

---
+++ admin/dist/assets/ImageAltAttribute-ykwffdrdz5.js
@@ -0,0 +1,20 @@
+import { r as reactExports, R as React, _ as __vitePreload } from "../main-ykwffdrdz5.js";
+import { O as Overview, M as ModuleViewHeader } from "./OverviewTemplate-ykwffdrdz5.js";
+/* empty css                              */function ImageAltAttributeOverview({ moduleId }) {
+  const [section, setSection] = reactExports.useState("about");
+  return /* @__PURE__ */ React.createElement(Overview, { moduleId, noCheckbox: true, section: (val) => setSection(val), noIntegrate: true }, section === "about" && /* @__PURE__ */ React.createElement("section", null, /* @__PURE__ */ React.createElement("h4", null, "About the module"), /* @__PURE__ */ React.createElement("p", null, "The Image SEO module is an essential tool for any website owner looking to maximize visibility and ensure that their images are properly optimised for SEO. This module can instantly improve image SEO by automatically adding descriptive alt texts to images."), /* @__PURE__ */ React.createElement("p", null, "Images are an important part of any website, as they can help to draw in visitors and make the website more visually appealing. However, if images are not properly optimised for SEO, they may not show up in search engine results and can be detrimental to the website's overall visibility."), /* @__PURE__ */ React.createElement("p", null, "The module also helps to improve user experience by making it easier for visitors to find and understand the images on the website. Visitors can more easily identify the images and understand their context by automatically adding descriptive alt texts to images. It can make the website more user-friendly, which can help to improve the overall experience for visitors.")), section === "faq" && /* @__PURE__ */ React.createElement("section", null, /* @__PURE__ */ React.createElement("h4", null, "FAQ"), /* @__PURE__ */ React.createElement("p", null, "Available soon.")));
+}
+function ImageAltAttribute({ moduleId }) {
+  const [activeSection, setActiveSection] = reactExports.useState("overview");
+  const SettingsModule = reactExports.lazy(() => __vitePreload(() => import("./Settings-ykwffdrdz5.js"), true ? ["./Settings-ykwffdrdz5.js","../main-ykwffdrdz5.js","./main-ykwffdrdz5.css","./datepicker-ykwffdrdz5.js","./MultiSelectMenu-ykwffdrdz5.js","./MultiSelectMenu-ykwffdrdz5.css","./datepicker-ykwffdrdz5.css","./Textarea-ykwffdrdz5.js","./Switch-ykwffdrdz5.js","./Switch-ykwffdrdz5.css","./Settings-ykwffdrdz5.css"] : void 0, import.meta.url));
+  return /* @__PURE__ */ React.createElement("div", { className: "urlslab-tableView" }, /* @__PURE__ */ React.createElement(
+    ModuleViewHeader,
+    {
+      moduleId,
+      activeMenu: (activemenu) => setActiveSection(activemenu)
+    }
+  ), activeSection === "overview" && /* @__PURE__ */ React.createElement(ImageAltAttributeOverview, { moduleId }), activeSection === "settings" && /* @__PURE__ */ React.createElement(reactExports.Suspense, null, /* @__PURE__ */ React.createElement(SettingsModule, { className: "fadeInto", settingId: moduleId })));
+}
+export {
+  ImageAltAttribute as default
+};
---
+++ admin/dist/assets/ScreenshotTable-ykwffdrdz5.js
@@ -0,0 +1,149 @@
+import { r as reactExports, R as React, i as Tooltip, L as Loader } from "../main-ykwffdrdz5.js";
+import { a as useTableUpdater, b as useInfiniteFetch, c as useChangeRow, d as SortBy, D as DateTimeFormat, S as SvgIconTrash, M as ModuleViewHeaderBottom, T as Table, P as ProgressBar } from "./ModuleViewHeaderBottom-ykwffdrdz5.js";
+import { T as TagsMenu } from "./TagsMenu-ykwffdrdz5.js";
+import { T as TooltipSortingFiltering } from "./Tooltip_SortingFiltering-ykwffdrdz5.js";
+import { C as Checkbox } from "./MultiSelectMenu-ykwffdrdz5.js";
+import { S as SvgIconLink } from "./icon-link-ykwffdrdz5.js";
+import "./datepicker-ykwffdrdz5.js";
+function ScreenshotTable({ slug }) {
+  var _a;
+  const paginationId = "url_id";
+  const { table, setTable, filters, setFilters, sorting, sortBy } = useTableUpdater({ slug });
+  const url = { filters, sorting };
+  const [detailsOptions, setDetailsOptions] = reactExports.useState(null);
+  const [tooltipUrl, setTooltipUrl] = reactExports.useState();
+  const {
+    __,
+    columnHelper,
+    data,
+    status,
+    isSuccess,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    ref
+  } = useInfiniteFetch({ key: slug, filters, sorting, paginationId });
+  const { row, selectedRows, selectRow, deleteRow, deleteSelectedRows, updateRow } = useChangeRow({ data, url, slug, paginationId });
+  const scrStatusTypes = {
+    N: __("Waiting"),
+    A: __("Available"),
+    P: __("Pending"),
+    U: __("Updating"),
+    E: __("Disabled")
+  };
+  const header = {
+    screenshot_url_thumbnail: __("Screenshot URL"),
+    url_name: __("Destination URL"),
+    url_title: __("Title"),
+    scr_status: __("Status"),
+    screenshot_usage_count: __("Usage"),
+    update_scr_date: __("Last change"),
+    labels: __("Tags")
+  };
+  const columns = [
+    columnHelper.accessor("check", {
+      className: "checkbox",
+      cell: (cell) => /* @__PURE__ */ React.createElement(Checkbox, { defaultValue: cell.row.getIsSelected(), onChange: (val) => {
+        selectRow(val, cell);
+      } }),
+      header: null
+    }),
+    columnHelper == null ? void 0 : columnHelper.accessor("screenshot_url_thumbnail", {
+      tooltip: (cell) => {
+        if (tooltipUrl === cell.getValue()) {
+          return /* @__PURE__ */ React.createElement(Tooltip, null, /* @__PURE__ */ React.createElement("img", { src: cell.getValue(), alt: "url" }));
+        }
+        return false;
+      },
+      // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
+      cell: (cell) => /* @__PURE__ */ React.createElement("a", { onMouseOver: () => setTooltipUrl(cell.getValue()), onMouseLeave: () => setTooltipUrl(), href: cell.getValue(), title: cell.getValue(), target: "_blank", rel: "noreferrer" }, cell.getValue()),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.screenshot_url_thumbnail),
+      size: 150
+    }),
+    columnHelper.accessor("url_name", {
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, null, cell.getValue()),
+      cell: (cell) => /* @__PURE__ */ React.createElement("a", { href: cell.getValue(), title: cell.getValue(), target: "_blank", rel: "noreferrer" }, cell.getValue()),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.url_name),
+      size: 200
+    }),
+    columnHelper.accessor("url_title", {
+      className: "nolimit",
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, { className: "xxl" }, cell.getValue()),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.url_title),
+      size: 150
+    }),
+    columnHelper == null ? void 0 : columnHelper.accessor("scr_status", {
+      filterValMenu: scrStatusTypes,
+      cell: (cell) => scrStatusTypes[cell.getValue()],
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.scr_status),
+      size: 80
+    }),
+    columnHelper.accessor("update_scr_date", {
+      cell: (val) => /* @__PURE__ */ React.createElement(DateTimeFormat, { datetime: val.getValue() }),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.update_scr_date),
+      size: 115
+    }),
+    columnHelper == null ? void 0 : columnHelper.accessor("screenshot_usage_count", {
+      cell: (cell) => /* @__PURE__ */ React.createElement("div", { className: "flex flex-align-center" }, cell == null ? void 0 : cell.getValue(), (cell == null ? void 0 : cell.getValue()) > 0 && /* @__PURE__ */ React.createElement("button", { className: "ml-s", onClick: () => setDetailsOptions({
+        title: `Screenshot used on these URLs`,
+        slug,
+        url: `${cell.row.original.url_id}/linked-from`,
+        showKeys: ["src_url_name"],
+        listId: "src_url_id"
+      }) }, /* @__PURE__ */ React.createElement(SvgIconLink, null), /* @__PURE__ */ React.createElement(Tooltip, null, __("Show URLs where used")))),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.screenshot_usage_count),
+      size: 60
+    }),
+    columnHelper.accessor("labels", {
+      className: "nolimit",
+      cell: (cell) => /* @__PURE__ */ React.createElement(TagsMenu, { defaultValue: cell.getValue(), slug, onChange: (newVal) => updateRow({ newVal, cell }) }),
+      header: header.labels,
+      size: 150
+    }),
+    columnHelper.accessor("editRow", {
+      className: "editRow",
+      tooltip: () => /* @__PURE__ */ React.createElement(Tooltip, { className: "align-left xxxl" }, __("Delete item")),
+      cell: (cell) => /* @__PURE__ */ React.createElement(SvgIconTrash, { onClick: () => deleteRow({ cell }) }),
+      header: null
+    })
+  ];
+  if (status === "loading") {
+    return /* @__PURE__ */ React.createElement(Loader, null);
+  }
+  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(
+    ModuleViewHeaderBottom,
+    {
+      slug,
+      header,
+      table,
+      selectedRows,
+      noImport: true,
+      onDeleteSelected: deleteSelectedRows,
+      onFilter: (filter) => setFilters(filter),
+      onUpdate: () => setDetailsOptions(),
+      detailsOptions,
+      exportOptions: {
+        slug,
+        url,
+        paginationId,
+        deleteCSVCols: ["urlslab_url_id", "url_id", "urlslab_domain_id"],
+        perPage: 1e3
+      }
+    }
+  ), /* @__PURE__ */ React.createElement(
+    Table,
+    {
+      className: "fadeInto",
+      slug,
+      returnTable: (returnTable) => setTable(returnTable),
+      columns,
+      data: isSuccess && ((_a = data == null ? void 0 : data.pages) == null ? void 0 : _a.flatMap((page) => page ?? []))
+    },
+    row ? /* @__PURE__ */ React.createElement(Tooltip, { center: true }, `${header.url_name} “${row.url_name}”`, " has been deleted.") : null,
+    /* @__PURE__ */ React.createElement(TooltipSortingFiltering, { props: { isFetching, filters, sorting } }),
+    /* @__PURE__ */ React.createElement("div", { ref }, isFetchingNextPage ? "" : hasNextPage, /* @__PURE__ */ React.createElement(ProgressBar, { className: "infiniteScroll", value: !isFetchingNextPage ? 0 : 100 }))
+  ));
+}
+export {
+  ScreenshotTable as default
+};
