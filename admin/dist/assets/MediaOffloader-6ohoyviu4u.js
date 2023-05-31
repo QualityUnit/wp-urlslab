747b96688a428db55d5ea66e15c20d98a076a622
ihdk <ihudak@qualityunit.com>
2023-05-31 07:49:52 +0200

    custom vite build & watch

---
+++ admin/dist/assets/MediaOffloader-6ohoyviu4u.js
@@ -0,0 +1,48 @@
+<<<<<<< HEAD:admin/dist/assets/MediaOffloader-myg4akepfo.js
+<<<<<<<< HEAD:admin/dist/assets/MediaOffloader-txs3jaim6w.js
+import { r as reactExports, R as React, u as useI18n, _ as __vitePreload } from "../main-txs3jaim6w.js";
+import { O as Overview, M as ModuleViewHeader } from "./OverviewTemplate-txs3jaim6w.js";
+========
+import { u as useI18n, _ as __vitePreload } from "../main-myg4akepfo.js";
+import { r as reactExports, R as React } from "./index-myg4akepfo.js";
+import { O as Overview, M as ModuleViewHeader } from "./OverviewTemplate-myg4akepfo.js";
+>>>>>>>> 9abc1ecf (initial build):admin/dist/assets/MediaOffloader-myg4akepfo.js
+=======
+import { r as reactExports, R as React, u as useI18n, _ as __vitePreload } from "../main-6ohoyviu4u.js";
+import { O as Overview, M as ModuleViewHeader } from "./OverviewTemplate-6ohoyviu4u.js";
+>>>>>>> 18bd270e (custom vite build & watch):admin/dist/assets/MediaOffloader-6ohoyviu4u.js
+/* empty css                              */function MediaOffloaderOverview({ moduleId }) {
+  const [section, setSection] = reactExports.useState("about");
+  return /* @__PURE__ */ React.createElement(Overview, { moduleId, section: (val) => setSection(val), noIntegrate: true }, section === "about" && /* @__PURE__ */ React.createElement("section", null, /* @__PURE__ */ React.createElement("h4", null, "About the module"), /* @__PURE__ */ React.createElement("p", null, "The Media Manager module can be a great tool for improving the performance of any website. With its automatic image enhancement, it can make images smaller while also offloading them to the cloud or a database. This will help reduce the load time when a user accesses a website. In addition to this, it can also generate modern image formats such as WebP and Avif, which are more efficient and provide better compression ratios."), /* @__PURE__ */ React.createElement("p", null, "Moreover, it can also help with SEO by automatically removing broken images from the content, thus improving the overall experience for the users. It can be a great way to ensure your website is optimised and running at its best.")), section === "faq" && /* @__PURE__ */ React.createElement("section", null, /* @__PURE__ */ React.createElement("h4", null, "FAQ"), /* @__PURE__ */ React.createElement("p", null, "Available soon.")));
+}
+function MediaOffloader({ moduleId }) {
+  const { __ } = useI18n();
+  const [activeSection, setActiveSection] = reactExports.useState("overview");
+  const slug = "file";
+  const tableMenu = /* @__PURE__ */ new Map([
+    [slug, __("Media Files")]
+  ]);
+<<<<<<< HEAD:admin/dist/assets/MediaOffloader-myg4akepfo.js
+<<<<<<<< HEAD:admin/dist/assets/MediaOffloader-txs3jaim6w.js
+  const SettingsModule = reactExports.lazy(() => __vitePreload(() => import("./Settings-txs3jaim6w.js"), true ? ["./Settings-txs3jaim6w.js","../main-txs3jaim6w.js","./main-txs3jaim6w.css","./datepicker-txs3jaim6w.js","./MultiSelectMenu-txs3jaim6w.js","./MultiSelectMenu-txs3jaim6w.css","./datepicker-txs3jaim6w.css","./Textarea-txs3jaim6w.js","./Switch-txs3jaim6w.js","./Switch-txs3jaim6w.css","./Settings-txs3jaim6w.css"] : void 0, import.meta.url));
+  const MediaFilesTable = reactExports.lazy(() => __vitePreload(() => import("./MediaFilesTable-txs3jaim6w.js"), true ? ["./MediaFilesTable-txs3jaim6w.js","../main-txs3jaim6w.js","./main-txs3jaim6w.css","./ModuleViewHeaderBottom-txs3jaim6w.js","./datepicker-txs3jaim6w.js","./MultiSelectMenu-txs3jaim6w.js","./MultiSelectMenu-txs3jaim6w.css","./datepicker-txs3jaim6w.css","./ModuleViewHeaderBottom-txs3jaim6w.css","./TagsMenu-txs3jaim6w.js","./Tooltip_SortingFiltering-txs3jaim6w.js","./icon-link-txs3jaim6w.js"] : void 0, import.meta.url));
+========
+  const SettingsModule = reactExports.lazy(() => __vitePreload(() => import("./Settings-myg4akepfo.js"), true ? ["./Settings-myg4akepfo.js","./index-myg4akepfo.js","..\\main-myg4akepfo.js","./main-myg4akepfo.css","./datepicker-myg4akepfo.js","./MultiSelectMenu-myg4akepfo.js","./MultiSelectMenu-myg4akepfo.css","./datepicker-myg4akepfo.css","./Textarea-myg4akepfo.js","./Switch-myg4akepfo.js","./Switch-myg4akepfo.css","./Settings-myg4akepfo.css"] : void 0, import.meta.url));
+  const MediaFilesTable = reactExports.lazy(() => __vitePreload(() => import("./MediaFilesTable-myg4akepfo.js"), true ? ["./MediaFilesTable-myg4akepfo.js","./index-myg4akepfo.js","./ModuleViewHeaderBottom-myg4akepfo.js","..\\main-myg4akepfo.js","./main-myg4akepfo.css","./datepicker-myg4akepfo.js","./MultiSelectMenu-myg4akepfo.js","./MultiSelectMenu-myg4akepfo.css","./datepicker-myg4akepfo.css","./ModuleViewHeaderBottom-myg4akepfo.css","./TagsMenu-myg4akepfo.js","./Tooltip_SortingFiltering-myg4akepfo.js","./icon-link-myg4akepfo.js"] : void 0, import.meta.url));
+>>>>>>>> 9abc1ecf (initial build):admin/dist/assets/MediaOffloader-myg4akepfo.js
+=======
+  const SettingsModule = reactExports.lazy(() => __vitePreload(() => import("./Settings-6ohoyviu4u.js"), true ? ["./Settings-6ohoyviu4u.js","..\\main-6ohoyviu4u.js","./main-6ohoyviu4u.css","./datepicker-6ohoyviu4u.js","./MultiSelectMenu-6ohoyviu4u.js","./MultiSelectMenu-6ohoyviu4u.css","./datepicker-6ohoyviu4u.css","./Textarea-6ohoyviu4u.js","./Switch-6ohoyviu4u.js","./Switch-6ohoyviu4u.css","./Settings-6ohoyviu4u.css"] : void 0, import.meta.url));
+  const MediaFilesTable = reactExports.lazy(() => __vitePreload(() => import("./MediaFilesTable-6ohoyviu4u.js"), true ? ["./MediaFilesTable-6ohoyviu4u.js","..\\main-6ohoyviu4u.js","./main-6ohoyviu4u.css","./ModuleViewHeaderBottom-6ohoyviu4u.js","./datepicker-6ohoyviu4u.js","./MultiSelectMenu-6ohoyviu4u.js","./MultiSelectMenu-6ohoyviu4u.css","./datepicker-6ohoyviu4u.css","./ModuleViewHeaderBottom-6ohoyviu4u.css","./TagsMenu-6ohoyviu4u.js","./Tooltip_SortingFiltering-6ohoyviu4u.js","./icon-link-6ohoyviu4u.js"] : void 0, import.meta.url));
+>>>>>>> 18bd270e (custom vite build & watch):admin/dist/assets/MediaOffloader-6ohoyviu4u.js
+  return /* @__PURE__ */ React.createElement("div", { className: "urlslab-tableView" }, /* @__PURE__ */ React.createElement(
+    ModuleViewHeader,
+    {
+      moduleId,
+      moduleMenu: tableMenu,
+      activeMenu: (activemenu) => setActiveSection(activemenu)
+    }
+  ), activeSection === "overview" && /* @__PURE__ */ React.createElement(MediaOffloaderOverview, { moduleId }), activeSection === slug && /* @__PURE__ */ React.createElement(reactExports.Suspense, null, /* @__PURE__ */ React.createElement(MediaFilesTable, { slug })), activeSection === "settings" && /* @__PURE__ */ React.createElement(reactExports.Suspense, null, /* @__PURE__ */ React.createElement(SettingsModule, { className: "fadeInto", settingId: moduleId })));
+}
+export {
+  MediaOffloader as default
+};
---
+++ admin/modules/ai_content_assistant/App.jsx
@@ -0,0 +1,10 @@
+
+import './assets/styles/style.scss';
+
+export default function App() {
+	return (
+		<div className="">
+			divko
+		</div>
+	);
+}
