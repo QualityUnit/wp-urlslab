fd687eee45c616013a0f215321b5a2da02e11ede
QualityUnitWebCI <dev-ci-web@qualityunit.com>
2023-06-01 11:54:12 +0000

    chore: bump version to 2.25.23

---
+++ admin/dist/assets/MetaTagsTable-sxzakuekjr.js
@@ -0,0 +1,167 @@
+import { R as React, l as Tooltip, L as Loader } from "../main-sxzakuekjr.js";
+import { a as useTableUpdater, b as useInfiniteFetch, c as useChangeRow, e as SortBy, D as DateTimeFormat, S as SvgIconTrash, M as ModuleViewHeaderBottom, T as Table, P as ProgressBar } from "./ModuleViewHeaderBottom-sxzakuekjr.js";
+import { T as TagsMenu } from "./TagsMenu-sxzakuekjr.js";
+import { T as TooltipSortingFiltering } from "./Tooltip_SortingFiltering-sxzakuekjr.js";
+import { C as Checkbox } from "./MultiSelectMenu-sxzakuekjr.js";
+import { I as InputField } from "./datepicker-sxzakuekjr.js";
+function LinkManagerTable({ slug }) {
+  var _a;
+  const paginationId = "url_id";
+  const { table, setTable, filters, setFilters, sorting, sortBy } = useTableUpdater({ slug });
+  const url = { filters, sorting };
+  const {
+    __,
+    columnHelper,
+    data,
+    status,
+    isSuccess,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    ref
+  } = useInfiniteFetch({ key: slug, filters, sorting, paginationId });
+  const { selectedRows, selectRow, deleteRow, deleteSelectedRows, updateRow } = useChangeRow({ data, url, slug, paginationId });
+  const httpStatusTypes = {
+    "-2": __("Processing"),
+    "-1": __("Waiting"),
+    200: __("Valid"),
+    400: __("Client Error"),
+    404: __("Not Found"),
+    500: __("Server Error"),
+    503: __("Server Error")
+  };
+  const relScheduleTypes = {
+    N: "New",
+    M: "Manual",
+    S: "Scheduled",
+    E: "Error"
+  };
+  const header = {
+    url_name: __("URL"),
+    url_title: __("Title"),
+    url_meta_description: __("Description"),
+    url_summary: __("Summary"),
+    http_status: __("Status"),
+    labels: __("Tags"),
+    rel_schedule: __("Schedule"),
+    update_http_date: __("Last change")
+  };
+  const columns = [
+    columnHelper.accessor("check", {
+      className: "checkbox",
+      cell: (cell) => /* @__PURE__ */ React.createElement(Checkbox, { defaultValue: cell.row.getIsSelected(), onChange: (val) => {
+        selectRow(val, cell);
+      } }),
+      header: null
+    }),
+    columnHelper.accessor("url_name", {
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, null, cell.getValue()),
+      cell: (cell) => /* @__PURE__ */ React.createElement("a", { href: cell.getValue(), title: cell.getValue(), target: "_blank", rel: "noreferrer" }, cell.getValue()),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.url_name),
+      size: 200
+    }),
+    columnHelper.accessor("url_title", {
+      className: "nolimit",
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, { className: "xxl" }, cell.getValue()),
+      cell: (cell) => /* @__PURE__ */ React.createElement(
+        InputField,
+        {
+          defaultValue: cell.getValue(),
+          onChange: (newVal) => updateRow({ newVal, cell })
+        }
+      ),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.url_title),
+      size: 150
+    }),
+    columnHelper == null ? void 0 : columnHelper.accessor("url_meta_description", {
+      className: "nolimit",
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, { className: "xxl" }, cell.getValue()),
+      cell: (cell) => /* @__PURE__ */ React.createElement(
+        InputField,
+        {
+          defaultValue: cell.getValue(),
+          onChange: (newVal) => updateRow({ newVal, cell })
+        }
+      ),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.url_meta_description),
+      size: 150
+    }),
+    columnHelper.accessor("url_summary", {
+      className: "nolimit",
+      tooltip: (cell) => /* @__PURE__ */ React.createElement(Tooltip, { className: "xxl" }, cell.getValue()),
+      cell: (cell) => /* @__PURE__ */ React.createElement(
+        InputField,
+        {
+          defaultValue: cell.getValue(),
+          onChange: (newVal) => updateRow({ newVal, cell })
+        }
+      ),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.url_summary),
+      size: 200
+    }),
+    columnHelper == null ? void 0 : columnHelper.accessor("http_status", {
+      filterValMenu: httpStatusTypes,
+      cell: (cell) => httpStatusTypes[cell.getValue()],
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.http_status),
+      size: 80
+    }),
+    columnHelper.accessor("rel_schedule", {
+      filterValMenu: relScheduleTypes,
+      cell: (cell) => relScheduleTypes[cell.getValue()],
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.rel_schedule),
+      size: 80
+    }),
+    columnHelper.accessor("update_http_date", {
+      cell: (val) => /* @__PURE__ */ React.createElement(DateTimeFormat, { datetime: val.getValue() }),
+      header: (th) => /* @__PURE__ */ React.createElement(SortBy, { props: { header, sorting, th, onClick: () => sortBy(th) } }, header.update_http_date),
+      size: 115
+    }),
+    columnHelper.accessor("labels", {
+      className: "nolimit",
+      cell: (cell) => /* @__PURE__ */ React.createElement(TagsMenu, { defaultValue: cell.getValue(), slug, onChange: (newVal) => updateRow({ newVal, cell }) }),
+      header: header.labels,
+      size: 150
+    }),
+    columnHelper.accessor("editRow", {
+      className: "editRow",
+      cell: (cell) => /* @__PURE__ */ React.createElement(SvgIconTrash, { onClick: () => deleteRow({ cell, id: "url_name" }) }),
+      header: null
+    })
+  ];
+  if (status === "loading") {
+    return /* @__PURE__ */ React.createElement(Loader, null);
+  }
+  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(
+    ModuleViewHeaderBottom,
+    {
+      table,
+      selectedRows,
+      onDeleteSelected: () => deleteSelectedRows({ id: "url_name" }),
+      onFilter: (filter) => setFilters(filter),
+      noImport: true,
+      options: {
+        header,
+        slug,
+        data,
+        url,
+        paginationId,
+        deleteCSVCols: ["urlslab_url_id", "url_id", "urlslab_domain_id"],
+        perPage: 1e3
+      }
+    }
+  ), /* @__PURE__ */ React.createElement(
+    Table,
+    {
+      className: "fadeInto",
+      slug,
+      returnTable: (returnTable) => setTable(returnTable),
+      columns,
+      data: isSuccess && ((_a = data == null ? void 0 : data.pages) == null ? void 0 : _a.flatMap((page) => page ?? []))
+    },
+    /* @__PURE__ */ React.createElement(TooltipSortingFiltering, { props: { isFetching, filters, sorting } }),
+    /* @__PURE__ */ React.createElement("div", { ref }, isFetchingNextPage ? "" : hasNextPage, /* @__PURE__ */ React.createElement(ProgressBar, { className: "infiniteScroll", value: !isFetchingNextPage ? 0 : 100 }))
+  ));
+}
+export {
+  LinkManagerTable as default
+};
---
+++ admin/dist/assets/Redirects-sxzakuekjr.js
@@ -0,0 +1,28 @@
+import { r as reactExports, R as React, u as useI18n, _ as __vitePreload } from "../main-sxzakuekjr.js";
+import { O as Overview, M as ModuleViewHeader } from "./OverviewTemplate-sxzakuekjr.js";
+/* empty css                              */function RedirectsOverview({ moduleId }) {
+  const [section, setSection] = reactExports.useState("about");
+  return /* @__PURE__ */ React.createElement(Overview, { moduleId, section: (val) => setSection(val), noIntegrate: true }, section === "about" && /* @__PURE__ */ React.createElement("section", null, /* @__PURE__ */ React.createElement("h4", null, "About the module"), /* @__PURE__ */ React.createElement("p", null, "Enhance your website's user experience and search engine optimisation with the essential Redirects and 404 Monitor module. Effortlessly identify and resolve 404 errors, maintain your site's integrity, and keep broken links at bay."), /* @__PURE__ */ React.createElement("p", null, "Featuring a user-friendly interface, this module simplifies the process of creating and managing redirects. Set up permanent or temporary redirects with ease, ensuring your visitors always land on the correct page. Stay informed of any 404 errors with the advanced monitoring system, allowing you to promptly address issues."), /* @__PURE__ */ React.createElement("p", null, "Don't let broken links and 404 errors hinder your website's performance. Equip your site with the Redirects and 404 Monitor module, and experience the benefits of a seamless browsing experience for users and improved SEO for your website.")), section === "faq" && /* @__PURE__ */ React.createElement("section", null, /* @__PURE__ */ React.createElement("h4", null, "FAQ"), /* @__PURE__ */ React.createElement("p", null, "Available soon.")));
+}
+function Redirects({ moduleId }) {
+  const { __ } = useI18n();
+  const [activeSection, setActiveSection] = reactExports.useState("overview");
+  const tableMenu = /* @__PURE__ */ new Map([
+    ["redirects", __("Redirects")],
+    ["notfound", __("404 Monitor")]
+  ]);
+  const SettingsModule = reactExports.lazy(() => __vitePreload(() => import("./Settings-sxzakuekjr.js"), true ? ["./Settings-sxzakuekjr.js","../main-sxzakuekjr.js","./main-sxzakuekjr.css","./datepicker-sxzakuekjr.js","./MultiSelectMenu-sxzakuekjr.js","./MultiSelectMenu-sxzakuekjr.css","./datepicker-sxzakuekjr.css","./Textarea-sxzakuekjr.js","./Switch-sxzakuekjr.js","./Switch-sxzakuekjr.css","./Settings-sxzakuekjr.css"] : void 0, import.meta.url));
+  const NotFoundTable = reactExports.lazy(() => __vitePreload(() => import("./NotFoundTable-sxzakuekjr.js"), true ? ["./NotFoundTable-sxzakuekjr.js","../main-sxzakuekjr.js","./main-sxzakuekjr.css","./ModuleViewHeaderBottom-sxzakuekjr.js","./datepicker-sxzakuekjr.js","./MultiSelectMenu-sxzakuekjr.js","./MultiSelectMenu-sxzakuekjr.css","./datepicker-sxzakuekjr.css","./ModuleViewHeaderBottom-sxzakuekjr.css","./TagsMenu-sxzakuekjr.js","./Tooltip_SortingFiltering-sxzakuekjr.js","./SuggestInputField-sxzakuekjr.js","./useRedirectTableMenus-sxzakuekjr.js"] : void 0, import.meta.url));
+  const RedirectsTable = reactExports.lazy(() => __vitePreload(() => import("./RedirectsTable-sxzakuekjr.js"), true ? ["./RedirectsTable-sxzakuekjr.js","../main-sxzakuekjr.js","./main-sxzakuekjr.css","./ModuleViewHeaderBottom-sxzakuekjr.js","./datepicker-sxzakuekjr.js","./MultiSelectMenu-sxzakuekjr.js","./MultiSelectMenu-sxzakuekjr.css","./datepicker-sxzakuekjr.css","./ModuleViewHeaderBottom-sxzakuekjr.css","./TagsMenu-sxzakuekjr.js","./Tooltip_SortingFiltering-sxzakuekjr.js","./SuggestInputField-sxzakuekjr.js","./icon-edit-sxzakuekjr.js","./useRedirectTableMenus-sxzakuekjr.js"] : void 0, import.meta.url));
+  return /* @__PURE__ */ React.createElement("div", { className: "urlslab-tableView" }, /* @__PURE__ */ React.createElement(
+    ModuleViewHeader,
+    {
+      moduleId,
+      moduleMenu: tableMenu,
+      activeMenu: (activemenu) => setActiveSection(activemenu)
+    }
+  ), activeSection === "overview" && /* @__PURE__ */ React.createElement(RedirectsOverview, { moduleId }), activeSection === "redirects" && /* @__PURE__ */ React.createElement(reactExports.Suspense, null, /* @__PURE__ */ React.createElement(RedirectsTable, { slug: "redirects" })), activeSection === "notfound" && /* @__PURE__ */ React.createElement(reactExports.Suspense, null, /* @__PURE__ */ React.createElement(NotFoundTable, { slug: "not-found-log" })), activeSection === "settings" && /* @__PURE__ */ React.createElement(reactExports.Suspense, null, /* @__PURE__ */ React.createElement(SettingsModule, { className: "fadeInto", settingId: moduleId })));
+}
+export {
+  Redirects as default
+};
