4cdf47b5d62d641280aff81fd4204dbfce5f6349
yasha-dev1 <yasha1boroumand@gmail.com>
2023-06-19 15:26:07 +0200

    fix: vendor change ignoring vendor and fix for ci cd

--- .github/scripts/build-files.sh
+++ .github/scripts/build-files.sh
@@ -6,8 +6,36 @@ echo "Remove Old Dist"
 rm -rf ./admin/dist
 rm -rf ./public/build
 
-echo "Composer Install"
+echo "Composer Installing in dev mode"
 composer install
+
+# setting up prefixed vendor
+echo "Setting up prefixed vendor"
+composer run-script prefix-dependencies
+
+echo "Composer Installing"
+composer install --no-dev
+
+# Removing redundant libs
+echo "Remove Redundant libs"
+cd vendor/aws/aws-sdk-php/src
+
+# removing redundant aws libs
+find . -mindepth 1 -maxdepth 1 -type d ! -name 'CloudFront' ! -name 'S3' ! -name 'Exception' -exec rm -r {} \;
+cd ../../../
+
+# removing */docs/*
+find . -type d -name 'docs' -exec rm -r {} \; || true
+
+# removing codesniffer
+rm -rf squizlabs automattic bin dealerdirect fidry humbug nikic sirbrillig webmozart wp-coding-standards urlslab
+cd composer
+rm -rf installers autoload_classmap.php autoload_files.php autoload_namespaces.php autoload_psr4.php installed.json installed.php InstalledVersions.php
+cd ../../
+
+echo "Composer Installing Prefixed Vendor"
+cd vendor_prefixed
+composer install --no-dev
 
 echo "Yarn Install"
 yarn
--- vendor_prefixed/urlslab-client/guzzlehttp/guzzle/src/Exception/TransferException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Urlslab_Vendor\GuzzleHttp\Exception;
-
-class TransferException extends \RuntimeException implements GuzzleException
-{
-}
