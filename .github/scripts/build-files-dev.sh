d74d4e5ce642339be1f6a494082e40b0d11ce981
yasha-dev1 <yasha1boroumand@gmail.com>
2023-06-30 11:21:54 +0200

    feat: updated Readme

---
+++ .github/scripts/build-files-dev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Inspired by Elementor Releasing Pipeline https://github.com/elementor/elementor
+set -eo pipefail
+
+echo "Remove Old Dist"
+rm -rf ./admin/dist
+rm -rf ./public/build
+
+echo "Composer Installing in dev mode"
+composer install
+
+# setting up prefixed vendor
+echo "Setting up prefixed vendor"
+composer run-script prefix-dependencies
+
+echo "Composer Installing"
+composer install
+
+echo "Composer Installing Prefixed Vendor"
+cd vendor_prefixed
+composer install
+
+echo "Yarn Install"
+yarn
+
+echo "Build"
+yarn build
+
+echo "Build AI Content Assistant"
+cd ../admin/src/app-ai-content-assistant
+yarn
+yarn build
