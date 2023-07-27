40d1fa5d974224f92dc5481bb0738854eea919d1
vzeman <vzeman@qualityunit.com>
2023-07-27 09:31:29 +0200

    feat: top urls view

---
+++ admin/src/tables/SerpUrlsTable.jsx
@@ -0,0 +1,157 @@
+/* eslint-disable indent */
+import {
+	useInfiniteFetch,
+	ProgressBar,
+	SortBy,
+	Loader,
+	Tooltip,
+	Table,
+	ModuleViewHeaderBottom,
+	TooltipSortingFiltering,
+	SingleSelectMenu, InputField, Editor, LangMenu, TagsMenu,
+} from '../lib/tableImports';
+
+import useTableUpdater from '../hooks/useTableUpdater';
+import useChangeRow from '../hooks/useChangeRow';
+import useTablePanels from '../hooks/useTablePanels';
+import React, { useCallback } from 'react';
+import TextArea from "../elements/Textarea";
+
+export default function SerpUrlsTable( { slug } ) {
+	const paginationId = 'url_id';
+	const { table, setTable, filters, setFilters, sorting, sortBy } = useTableUpdater( { slug } );
+	const defaultSorting = sorting.length ? sorting : [ { key: 'top10_queries_cnt', dir: 'DESC', op: '<' } ];
+	const url = { filters, sorting: defaultSorting };
+
+	const {
+		__,
+		columnHelper,
+		data,
+		status,
+		isSuccess,
+		isFetching,
+		isFetchingNextPage,
+		hasNextPage,
+		ref,
+	} = useInfiniteFetch( { key: slug, filters, sorting: defaultSorting, paginationId } );
+
+	const { selectRows, deleteRow, deleteSelectedRows, updateRow } = useChangeRow( { data, url, slug, paginationId } );
+
+	const { activatePanel, setRowToEdit, setOptions } = useTablePanels();
+	const rowToEdit = useTablePanels( ( state ) => state.rowToEdit );
+
+	const setUnifiedPanel = useCallback( ( cell ) => {
+		const origCell = cell?.row.original;
+		setOptions( [] );
+		setRowToEdit( {} );
+		updateRow( { cell, id: 'url_name' } );
+	}, [ setOptions, setRowToEdit, slug, updateRow ] );
+
+	const domainTypes = {
+		X: __('Other'),
+		M: __('My Domain'),
+		C: __('Competitor')
+	};
+
+	const header = {
+		url_name: __( 'URL' ),
+		url_title: __( 'Title' ),
+		url_description: __( 'Description' ),
+		domain_type: __( 'Domain Type' ),
+		best_position: __( 'Best Position' ),
+		top10_queries: __( 'Top 10 Queries' ),
+		top10_queries_cnt: __( 'Top 10' ),
+		queries_cnt: __( 'Top 100' ),
+		queries: __( 'Queries' ),
+	};
+
+
+	const columns = [
+		columnHelper.accessor( 'url_name', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => <a href={ cell.getValue() } target="_blank"><strong>{ cell.getValue() }</strong></a>,
+			header: ( th ) => <SortBy props={ { header, sorting, th, onClick: () => sortBy( th ) } }>{ header.url_name }</SortBy>,
+			minSize: 200,
+		} ),
+		columnHelper.accessor( 'url_title', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => <strong>{ cell.getValue() }</strong>,
+			header: ( th ) => <SortBy props={ { header, sorting, th, onClick: () => sortBy( th ) } }>{ header.url_title }</SortBy>,
+			minSize: 100,
+		} ),
+		columnHelper.accessor( 'url_description', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => cell.getValue(),
+			header: ( th ) => <SortBy props={ { header, sorting, th, onClick: () => sortBy( th ) } }>{ header.url_description }</SortBy>,
+			minSize: 100,
+		} ),
+		columnHelper.accessor( 'domain_type', {
+			filterValMenu: domainTypes,
+			className: 'nolimit',
+			cell: ( cell ) => domainTypes[ cell.getValue() ],
+			header: ( th ) => <SortBy props={ { header, sorting, th, onClick: () => sortBy( th ) } }>{ header.domain_type }</SortBy>,
+			size: 80,
+		} ),
+
+		columnHelper.accessor( 'best_position', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => <strong>{ cell.getValue() }</strong>,
+			header: ( th ) => <SortBy props={ { header, sorting: defaultSorting, th, onClick: () => sortBy( th ) } }>{ header.best_position }</SortBy>,
+			minSize: 50,
+		} ),
+		columnHelper.accessor( 'top10_queries_cnt', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => cell.getValue(),
+			header: ( th ) => <SortBy props={ { header, sorting: defaultSorting, th, onClick: () => sortBy( th ) } }>{ header.top10_queries_cnt }</SortBy>,
+			minSize: 50,
+		} ),
+		columnHelper.accessor( 'top10_queries', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => cell.getValue(),
+			header: ( th ) => <SortBy props={ { header, sorting: defaultSorting, th, onClick: () => sortBy( th ) } }>{ header.top10_queries }</SortBy>,
+			minSize: 200,
+		} ),
+		columnHelper.accessor( 'queries_cnt', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => cell.getValue(),
+			header: ( th ) => <SortBy props={ { header, sorting: defaultSorting, th, onClick: () => sortBy( th ) } }>{ header.queries_cnt }</SortBy>,
+			minSize: 50,
+		} ),
+		columnHelper.accessor( 'queries', {
+			tooltip: ( cell ) => <Tooltip>{ cell.getValue() }</Tooltip>,
+			cell: ( cell ) => cell.getValue(),
+			header: ( th ) => <SortBy props={ { header, sorting: defaultSorting, th, onClick: () => sortBy( th ) } }>{ header.queries }</SortBy>,
+			minSize: 200,
+		} ),
+	];
+
+	if ( status === 'loading' ) {
+		return <Loader />;
+	}
+
+	return (
+		<>
+			<ModuleViewHeaderBottom
+				table={ table }
+				onFilter={ ( filter ) => setFilters( filter ) }
+				noDelete
+				noInsert
+				noImport
+				options={ { header, data, slug, paginationId, url, id: 'url_name', title: '', rowToEdit,
+					deleteCSVCols: [ paginationId, 'url_id' ] }
+				}
+			/>
+			<Table className="fadeInto"
+				slug={ slug }
+				returnTable={ ( returnTable ) => setTable( returnTable ) }
+				columns={ columns }
+				data={ isSuccess && data?.pages?.flatMap( ( page ) => page ?? [] ) }>
+				<TooltipSortingFiltering props={ { isFetching, filters, sorting } } />
+				<div ref={ ref }>
+					{ isFetchingNextPage ? '' : hasNextPage }
+					<ProgressBar className="infiniteScroll" value={ ! isFetchingNextPage ? 0 : 100 } />
+				</div>
+			</Table>
+		</>
+	);
+}
---
+++ includes/api/class-urlslab-api-serp-urls.php
@@ -0,0 +1,96 @@
+<?php
+
+class Urlslab_Api_Serp_Urls extends Urlslab_Api_Table {
+	const SLUG = 'serp-urls';
+
+	public function register_routes() {
+		$base = '/' . self::SLUG;
+		register_rest_route( self::NAMESPACE, $base . '/', $this->get_route_get_items() );
+		register_rest_route( self::NAMESPACE, $base . '/count', $this->get_count_route( array( $this->get_route_get_items() ) ) );
+	}
+
+
+	/**
+	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function get_items( $request ) {
+		$rows = $this->get_items_sql( $request )->get_results();
+
+		if ( is_wp_error( $rows ) ) {
+			return new WP_Error( 'error', __( 'Failed to get items', 'urlslab' ), array( 'status' => 400 ) );
+		}
+
+		foreach ( $rows as $row ) {
+			$row->url_id            = (int) $row->url_id;
+			$row->domain_id         = (int) $row->domain_id;
+			$row->queries_cnt       = (int) $row->queries_cnt;
+			$row->top10_queries_cnt = (int) $row->top10_queries_cnt;
+			$row->best_position     = (int) $row->best_position;
+		}
+
+		return new WP_REST_Response( $rows, 200 );
+	}
+
+
+	public function get_row_object( $params = array(), $loaded_from_db = true ): Urlslab_Data {
+		return new Urlslab_Serp_Url_Row( $params, $loaded_from_db );
+	}
+
+	public function get_editable_columns(): array {
+		return array();
+	}
+
+	protected function get_items_sql( WP_REST_Request $request ): Urlslab_Api_Table_Sql {
+		$sql = new Urlslab_Api_Table_Sql( $request );
+		foreach ( array_keys( $this->get_row_object()->get_columns() ) as $column ) {
+			$sql->add_select_column( $column, 'u' );
+		}
+		$sql->add_select_column( 'MIN(position)', false, 'best_position' );
+		$sql->add_select_column( 'COUNT(*)', false, 'queries_cnt' );
+		$sql->add_select_column( 'GROUP_CONCAT((CASE WHEN position <= 10 THEN query ELSE null END)  order by position DESC)', false, 'top10_queries' );
+		$sql->add_select_column( 'SUM(CASE WHEN position <= 10 THEN 1 ELSE 0 END)', false, 'top10_queries_cnt' );
+		$sql->add_select_column( 'GROUP_CONCAT(query order by position DESC)', false, 'queries' );
+		$sql->add_select_column( 'domain_type', 'd' );
+
+		$sql->add_from( $this->get_row_object()->get_table_name() . ' u' );
+		$sql->add_from( 'INNER JOIN ' . URLSLAB_SERP_POSITIONS_TABLE . ' p ON u.url_id = p.url_id' );
+		$sql->add_from( 'INNER JOIN ' . URLSLAB_SERP_QUERIES_TABLE . ' q ON q.query_id = p.query_id' );
+		$sql->add_from( 'INNER JOIN ' . URLSLAB_SERP_DOMAINS_TABLE . ' d ON u.domain_id = d.domain_id' );
+
+		$columns = $this->prepare_columns( $this->get_row_object()->get_columns(), 'u' );
+		$columns = array_merge(
+			$columns,
+			$this->prepare_columns(
+				array(
+					'best_position'     => '%d',
+					'queries_cnt'       => '%d',
+					'top10_queries_cnt' => '%d',
+					'top10_queries'     => '%s',
+					'queries'           => '%s',
+					'domain_type'       => '%s',
+				)
+			)
+		);
+
+		$sql->add_group_by( 'url_id', 'u' );
+		$sql->add_having_filters( $columns, $request );
+		$sql->add_sorting( $columns, $request );
+
+		return $sql;
+	}
+
+	private function get_route_get_items(): array {
+		return array(
+			'methods'             => WP_REST_Server::CREATABLE,
+			'callback'            => array( $this, 'get_items' ),
+			'args'                => $this->get_table_arguments(),
+			'permission_callback' => array(
+				$this,
+				'get_items_permissions_check',
+			),
+		);
+	}
+
+}
