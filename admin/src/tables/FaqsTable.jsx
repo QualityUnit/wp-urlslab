b27328566aac97af218330a57ba1be6c1b22d51d
Jozef Remeň <jozef.remen@gmail.com>
2023-07-27 15:09:57 +0200

    fix: fixing various js bugs in selection and delete

--- admin/src/tables/FaqsTable.jsx
+++ admin/src/tables/FaqsTable.jsx
@@ -40,7 +40,7 @@ export default function FaqsTable( { slug } ) {
 		ref,
 	} = useInfiniteFetch( { key: slug, filters, sorting, paginationId } );
 
-	const { selectedRows, selectRow, deleteRow, deleteSelectedRows, updateRow } = useChangeRow( { data, url, slug, paginationId } );
+	const { selectRows, deleteRow, deleteSelectedRows, updateRow } = useChangeRow( { data, url, slug, paginationId } );
 
 	const { activatePanel, setRowToEdit } = useTablePanels();
 	const rowToEdit = useTablePanels( ( state ) => state.rowToEdit );
@@ -83,10 +83,14 @@ export default function FaqsTable( { slug } ) {
 	const columns = [
 		columnHelper.accessor( 'check', {
 			className: 'nolimit checkbox',
-			cell: ( cell ) => <Checkbox defaultValue={ cell.row.getIsSelected() } onChange={ ( val ) => {
-				selectRow( val, cell );
+			cell: ( cell ) => <Checkbox defaultValue={ cell.row.getIsSelected() } onChange={ () => {
+				cell.row.toggleSelected();
+				selectRows( cell );
 			} } />,
-			header: null,
+			header: ( head ) => <Checkbox defaultValue={ head.table.getIsAllPageRowsSelected() } onChange={ ( val ) => {
+				head.table.toggleAllPageRowsSelected( val );
+				selectRows( val ? head : undefined );
+			} } />,
 		} ),
 		columnHelper.accessor( 'faq_id', {
 			className: 'nolimit',
@@ -156,10 +160,14 @@ export default function FaqsTable( { slug } ) {
 		<>
 			<ModuleViewHeaderBottom
 				table={ table }
-				selectedRows={ selectedRows }
-				onDeleteSelected={ () => deleteSelectedRows( { id: 'faq_id' } ) }
+				onDeleteSelected={ deleteSelectedRows }
 				onFilter={ ( filter ) => setFilters( filter ) }
-				options={ { header, rowEditorCells, title: 'Add New FAQ', data, slug, url, paginationId, rowToEdit, id: 'faq_id' } }
+				options={ { header, data, slug, url, paginationId,
+					title: 'Add New FAQ',
+					rowEditorCells,
+					rowToEdit,
+					id: 'faq_id',
+				} }
 			/>
 			<Table className="fadeInto"
 				slug={ slug }
