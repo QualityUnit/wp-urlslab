ef98f4f930c0f5ba98bd303a433bae4ba9654df6
Jozef Remeň <jozef.remen@gmail.com>
2023-08-28 11:59:29 +0200

    fix: media files table

--- admin/src/tables/MediaFilesTable.jsx
+++ admin/src/tables/MediaFilesTable.jsx
@@ -26,7 +26,7 @@ export default function MediaFilesTable( { slug } ) {
 
 	const { selectRows, deleteRow, updateRow } = useChangeRow();
 
-	const { resetTableStore } = useTablePanels();
+	const { resetTableStore } = useTableStore();
 	const { activatePanel, setOptions, setRowToEdit, resetPanelsStore } = useTablePanels();
 
 	const setUnifiedPanel = ( cell ) => {
