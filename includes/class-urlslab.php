494d9edd4881c4050c3e64d78d11c9481c424088
Martin Stepanek <martin@stepanek.sk>
2023-08-29 08:45:13 +0200

    feat: remove updater

--- includes/class-urlslab.php
+++ includes/class-urlslab.php
@@ -22,7 +22,6 @@
  * @since      1.0.0
  */
 class Urlslab {
-	public const URLSLAB_INFO_URL = 'https://raw.githubusercontent.com/QualityUnit/wp-urlslab/main/info.json';
 	private static $buffer_started = false;
 
 	/**
@@ -288,105 +287,6 @@ class Urlslab {
 		return $this->version;
 	}
 
-	public function get_info() {
-		if ( empty( $this->plugin_info ) ) {
-			$remote = get_transient( 'urlslab_update_info' );
-			if ( ! $remote ) {
-				$remote = wp_remote_get(
-					self::URLSLAB_INFO_URL,
-					array(
-						'timeout' => 5,    // phpcs:ignore
-						'headers' => array(
-							'Accept' => 'application/json',
-						),
-					)
-				);
-				set_transient( 'urlslab_update_info', $remote, 3600 * 3 );
-			}
-			$this->plugin_info = $remote;
-		}
-
-		return $this->plugin_info;
-	}
-
-	public function plugin_info( $res, $action, $args ) {
-		if ( 'plugin_information' !== $action || URLSLAB_PLUGIN_SLUG !== $args->slug ) {
-			return $res;
-		}
-
-		$remote = $this->get_info();
-
-		if ( is_wp_error( $remote ) || 200 !== wp_remote_retrieve_response_code( $remote ) || empty( wp_remote_retrieve_body( $remote ) ) ) {
-			return $res;
-		}
-
-		$remote = json_decode( wp_remote_retrieve_body( $remote ) );
-
-		$res                 = new stdClass();
-		$res->name           = $remote->name;
-		$res->slug           = $remote->slug;
-		$res->author         = $remote->author;
-		$res->author_profile = $remote->author_profile;
-		$res->version        = $remote->version;
-		$res->tested         = $remote->tested;
-		$res->requires       = $remote->requires;
-		$res->requires_php   = $remote->requires_php;
-		$res->download_link  = $remote->download_url;
-		$res->trunk          = $remote->download_url;
-		$res->last_updated   = $remote->last_updated;
-		$res->sections       = array(
-			'description'  => $remote->sections->description,
-			'installation' => $remote->sections->installation,
-			'changelog'    => $remote->sections->changelog,
-		);
-		if ( ! empty( $remote->sections->screenshots ) ) {
-			$res->sections['screenshots'] = $remote->sections->screenshots;
-		}
-
-		$res->banners = array(
-			'low'  => $remote->banners->low,
-			'high' => $remote->banners->high,
-		);
-
-		return $res;
-	}
-
-	public function push_update( $transient ) {
-		if ( empty( $transient->checked ) ) {
-			return $transient;
-		}
-
-		$remote = $this->get_info();
-
-		if ( is_wp_error( $remote ) || 200 !== wp_remote_retrieve_response_code( $remote ) || empty( wp_remote_retrieve_body( $remote ) ) ) {
-			return $transient;
-		}
-
-		$remote = json_decode( wp_remote_retrieve_body( $remote ) );
-
-		// your installed plugin version should be on the line below! You can obtain it dynamically of course
-		if (
-			$remote
-			&& version_compare( URLSLAB_VERSION, $remote->version, '<' )
-			&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
-			&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
-		) {
-			$res                                 = new stdClass();
-			$res->slug                           = $remote->slug;
-			$res->plugin                         = URLSLAB_PLUGIN_BASENAME;
-			$res->new_version                    = $remote->version;
-			$res->tested                         = $remote->tested;
-			$res->package                        = $remote->download_url;
-			$transient->response[ $res->plugin ] = $res;
-
-			if ( property_exists( $remote, 'version' ) ) {
-				$transient->checked[ $res->plugin ] = $remote->version;
-			}
-		}
-
-		return $transient;
-	}
-
 	public function plugin_action_links( array $links ) {
 		return array_merge(
 			array(
@@ -552,7 +452,6 @@ class Urlslab {
 			0
 		);
 		Urlslab_Loader::get_instance()->add_filter( 'plugins_api', $this, 'plugin_info', 20, 3 );
-		Urlslab_Loader::get_instance()->add_filter( 'site_transient_update_plugins', $this, 'push_update' );
 		Urlslab_Loader::get_instance()->add_filter( 'plugin_action_links_' . URLSLAB_PLUGIN_BASENAME, $this, 'plugin_action_links' );
 	}
 
