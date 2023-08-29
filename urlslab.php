f9203ae3cacfa77a845d265005081dccb46a16dc
QualityUnitWebCI <dev-ci-web@qualityunit.com>
2023-08-29 05:21:12 +0000

    chore: bump version to 2.47.0-beta.5

--- urlslab.php
+++ urlslab.php
@@ -16,7 +16,7 @@
  * Plugin Name:       URLsLab (developer beta)
  * Plugin URI:        https://github.com/QualityUnit/wp-urlslab
  * Description:       URLsLab WordPress Plugin to optimize your website for search engines and enhance automatically content
- * Version: 2.47.0-beta.4
+ * Version: 2.47.0-beta.5
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            URLsLab
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'URLSLAB_VERSION', '2.47.0-beta.4' );
+define( 'URLSLAB_VERSION', '2.47.0-beta.5' );
 define( 'URLSLAB_VERSION_SETTING', 'urlslab_ver' );
 define( 'URLSLAB_PLUGIN', __FILE__ );
 
