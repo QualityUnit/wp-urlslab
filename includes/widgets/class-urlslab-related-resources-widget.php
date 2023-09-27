b55e9be93d1eca88b118dd719aad21b99d7a332a
vzeman <vzeman@qualityunit.com>
2023-09-27 16:29:58 +0200

    fix: fatal error

--- includes/widgets/class-urlslab-related-resources-widget.php
+++ includes/widgets/class-urlslab-related-resources-widget.php
@@ -133,7 +133,7 @@ class Urlslab_Related_Resources_Widget extends Urlslab_Widget {
 				$current_url_obj->request_rel_schedule();
 				if ( ! empty( $result ) && is_array( $result ) ) {
 					$content  .= $this->render_shortcode_header( $urlslab_atts );
-					$strategy = Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Link_Enhancer::SLUG )->get_option( Urlslab_Link_Enhancer::SETTING_NAME_DESC_REPLACEMENT_STRATEGY );
+					$strategy = $this->get_strategy();
 					foreach ( $urls as $url ) {
 						if ( $current_url_obj->get_url_id() != $url->get_url_id() ) {
 							$url_obj = Urlslab_Url_Data_Fetcher::get_instance()->load_and_schedule_url( $url );
@@ -477,5 +477,12 @@ class Urlslab_Related_Resources_Widget extends Urlslab_Widget {
 			null,
 			'autoinclude'
 		);
+	}
+
+	private function get_strategy() {
+		if (Urlslab_User_Widget::get_instance()->is_widget_activated(Urlslab_Link_Enhancer::SLUG)) {
+			return Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Link_Enhancer::SLUG )->get_option( Urlslab_Link_Enhancer::SETTING_NAME_DESC_REPLACEMENT_STRATEGY );
+		}
+		return Urlslab_Link_Enhancer::DESC_TEXT_SUMMARY;
 	}
 }
