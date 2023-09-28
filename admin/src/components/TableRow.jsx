33721502fb1b1d3cd588e6971b6af4ae27f3b8d3
Jozef Remen <jozef.remen@gmail.com>
2023-09-28 13:05:10 +0200

    feat: moving individual cells to component (#1172)

--- admin/src/components/TableRow.jsx
+++ admin/src/components/TableRow.jsx
@@ -7,46 +7,51 @@ import { flexRender } from '@tanstack/react-table';
 import useTableStore from '../hooks/useTableStore';
 import { TableContext } from './TableComponent';
 
-function TableRow( { row } ) {
+const TableCell = memo( function TableCell( { cell, isEditCell } ) {
 	const { resizable, userCustomSettings, closeableRowActions } = useContext( TableContext );
-
-	const visibleCells = row.getVisibleCells();
 	const sorting = useTableStore( ( state ) => state.sorting );
+	const isTooltip = cell.column.columnDef.tooltip && cell.getValue();
+
+	return (
+		cell.column.getIsVisible() &&
+		<td
+			key={ cell.id }
+			className={ classNames( [
+				cell.column.columnDef.className,
+				sorting.length && sorting[ 0 ].key === cell.column.columnDef.accessorKey ? 'highlight' : null,
+				closeableRowActions && isEditCell && ! userCustomSettings.openedRowActions ? 'closed' : null,
+			] ) }
+			style={ {
+				width: cell.column.getSize() !== 0 && resizable
+					? cell.column.getSize()
+					: undefined,
+			} }
+		>
+			{ /** its safe to use always Tooltip component, nullish values doesn't render tooltip */ }
+			<Tooltip
+				placement="bottom-start"
+				title={
+					isTooltip
+						? <Box sx={ { maxWidth: '45rem' } }>{ flexRender( cell.column.columnDef.tooltip, cell.getContext() ) }</Box>
+						: null
+				}
+			>
+				<div className="limit">
+					{ flexRender( cell.column.columnDef.cell, cell.getContext() ) }
+				</div>
+			</Tooltip>
+		</td>
+	);
+} );
+
+function TableRow( { row } ) {
+	const visibleCells = row.getVisibleCells();
 
 	return <tr className={ row.getIsSelected() ? 'selected' : '' }>
 		{ visibleCells.map( ( cell, index ) => {
-			const isTooltip = cell.column.columnDef.tooltip && cell.getValue();
 			const isEditCell = index === visibleCells.length - 1 && cell.column.id === 'editRow';
-			return (
-				cell.column.getIsVisible() &&
-				<td
-					key={ cell.id }
-					className={ classNames( [
-						cell.column.columnDef.className,
-						sorting.length && sorting[ 0 ].key === cell.column.columnDef.accessorKey ? 'highlight' : null,
-						closeableRowActions && isEditCell && ! userCustomSettings.openedRowActions ? 'closed' : null,
-					] ) }
-					style={ {
-						width: cell.column.getSize() !== 0 && resizable
-							? cell.column.getSize()
-							: undefined,
-					} }
-				>
-					{ /** its safe to use always Tooltip component, nullish values doesn't render tooltip */ }
-					<Tooltip
-						placement="bottom-start"
-						title={
-							isTooltip
-								? <Box sx={ { maxWidth: '45rem' } }>{ flexRender( cell.column.columnDef.tooltip, cell.getContext() ) }</Box>
-								: null
-						}
-					>
-						<div className="limit">
-							{ flexRender( cell.column.columnDef.cell, cell.getContext() ) }
-						</div>
-					</Tooltip>
-				</td>
-			);
+
+			return <TableCell cell={ cell } key={ index } isEditCell={ isEditCell } />;
 		} ) }
 	</tr>;
 }
