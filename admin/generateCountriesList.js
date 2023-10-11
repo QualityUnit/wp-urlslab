7a762bdac8e5b3262e4d0c1357e6f86b6425eba3
ihdk <ivan.hdk@gmail.com>
2023-10-11 14:02:44 +0200

    feat: node js to generate countries list in build process

---
+++ admin/generateCountriesList.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const iso3311a2 = require( 'iso-3166-1-alpha-2' );
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+const fileListPath = path.join( __dirname, 'src/app/generatedData', 'countriesList.json' );
+
+const countriesList = {};
+for ( const [ key, value ] of Object.entries( iso3311a2.getData() ) ) {
+	countriesList[ key.toLowerCase() ] = value;
+}
+
+fs.writeFileSync( fileListPath, JSON.stringify( countriesList ), 'utf8' );
+
+console.info( 'JSON with Countries generated successfully!' );
