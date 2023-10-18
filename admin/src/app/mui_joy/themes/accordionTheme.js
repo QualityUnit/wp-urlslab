7be57115c9505266b6432137411c91ce5043ea72
ihdk <ivan.hdk@gmail.com>
2023-10-18 09:19:37 +0200

    fix: corrected side spacings

--- admin/src/app/mui_joy/themes/accordionTheme.js
+++ admin/src/app/mui_joy/themes/accordionTheme.js
@@ -6,15 +6,24 @@ const accordionTheme = {
 					...( ( ownerState.variant === 'plain' || ownerState.variant === 'outlined' ) && {
 						backgroundColor: theme.palette.common.white,
 					} ),
+
+					'--LeftArrowWidth': theme.spacing( 2.25 ),
+					'--LeftArrowSpace': `calc( var( --LeftArrowWidth ) + var( --ListItem-paddingLeft ) )`, // arrow width + gap between arrow and title
+					'--ContentWrapper--paddingLeft': theme.spacing( 2 ),
+					'--ContentWrapper--paddingRight': theme.spacing( 2 ),
+
 					// adds separator for main module description above table
 					...( ownerState.isMainTableDescription && {
 						'--Accordion-borderBottom': `1px solid ${ theme.vars.palette.divider }`,
+						'--ContentWrapper--paddingLeft': theme.spacing( 3 ),
+						'--ContentWrapper--paddingRight': theme.spacing( 3 ),
 					} ),
 
 				} ),
 			} ),
 		},
 	},
+
 	JoyAccordionSummary: {
 		styleOverrides: {
 			button: ( { ownerState, theme } ) => ( {
@@ -23,11 +32,13 @@ const accordionTheme = {
 					flexDirection: 'row-reverse',
 					justifyContent: 'left',
 
+					paddingLeft: 'var( --ContentWrapper--paddingLeft )',
+					paddingRight: 'var( --ContentWrapper--paddingRight )',
+
 					// adds extra padding to follow module header left paddings
 					...( ownerState.isMainTableDescription && {
 						paddingTop: theme.spacing( 1 ),
 						paddingBottom: theme.spacing( 1 ),
-						paddingLeft: theme.spacing( 3 ),
 					} ),
 
 				} ),
@@ -37,15 +48,16 @@ const accordionTheme = {
 	},
 	JoyAccordionDetails: {
 		styleOverrides: {
-			root: ( { ownerState, theme } ) => ( {
+			root: ( { ownerState } ) => ( {
 				...( ownerState.isDescriptionBox && {
-					paddingLeft: theme.spacing( 1 ),
-					paddingRight: theme.spacing( 1 ),
 
-					// adds extra padding to follow module header left paddings
-					...( ownerState.isMainTableDescription && {
-						paddingLeft: theme.spacing( 1.5 ),
-					} ),
+					paddingLeft: 'calc( var(--ContentWrapper--paddingLeft ) + var( --LeftArrowSpace ) + var( --ListItem-paddingLeft ) / 2 )',
+					paddingRight: 'var( --ContentWrapper--paddingRight )',
+
+					'.MuiAccordionDetails-content': {
+						paddingLeft: 0,
+						paddingRight: 0,
+					},
 				} ),
 			} ),
 		},
