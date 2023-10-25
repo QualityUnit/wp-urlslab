028861d443331742344ed19f946b3ee88c3e10e6
vzeman <vzeman@qualityunit.com>
2023-10-25 15:15:40 +0200

    fix: url not visible

--- includes/api/class-urlslab-api-web-vitals.php
+++ includes/api/class-urlslab-api-web-vitals.php
@@ -124,9 +124,10 @@ class Urlslab_Api_Web_Vitals extends Urlslab_Api_Table {
 							'nav_type'    => $metric['navigationType'],
 							'rating'      => $metric['rating'],
 							'url_id'      => $url->get_url_id(),
+							'url_name'    => $body['url'],
 							'value'       => $metric['value'],
 							'attribution' => $store_attribution ? json_encode( $metric['attribution'] ) : '',
-							'element'     => $metric['attribution']['element'] ?? $metric['attribution']['largestShiftTarget'] ?? '',
+							'element'     => $metric['attribution']['element'] ?? $metric['attribution']['largestShiftTarget'] ?? $metric['attribution']['eventTarget'] ?? '',
 							'entries'     => json_encode( $metric['entries'] ),
 						),
 						false
