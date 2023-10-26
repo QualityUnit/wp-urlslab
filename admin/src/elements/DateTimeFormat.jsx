c809da75588f85c5f9def4676aad4fb89d4d7bf5
vzeman <vzeman@qualityunit.com>
2023-10-26 17:29:38 +0200

    fix: show `-` if no date entered

--- admin/src/elements/DateTimeFormat.jsx
+++ admin/src/elements/DateTimeFormat.jsx
@@ -2,7 +2,9 @@ export default function DateTimeFormat( { datetime, noTime, oneLine } ) {
 	const { date, getSettings } = window.wp.date;
 	const dateFormatted = date( getSettings().formats.date, datetime );
 	const time = date( getSettings().formats.time, datetime );
-
+	if (!datetime) {
+		return <>—</>;
+	}
 	return (
 		<>
 			{ dateFormatted }
