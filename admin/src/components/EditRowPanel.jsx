b764b1a23bc4db6082e0a5bed78586a0379a0398
Jozef Remeň <jozef.remen@gmail.com>
2023-11-13 11:19:05 +0100

    fix: autocomplete capabilities menu

--- admin/src/components/EditRowPanel.jsx
+++ admin/src/components/EditRowPanel.jsx
@@ -25,6 +25,8 @@ function EditRowPanel( { editorMode, noScrollbar, notWide, text } ) {
 
 	const { insertRow, saveEditedRow } = useChangeRow( );
 
+	console.log( rowToEdit );
+
 	const requiredFields = rowEditorCells && Object.keys( rowEditorCells ).filter( ( cell ) => rowEditorCells[ cell ]?.props.required === true );
 
 	const cellsFinal = useMemo( () => {
--- admin/src/elements/CapabilitiesMenu.jsx
+++ admin/src/elements/CapabilitiesMenu.jsx
@@ -1,20 +1,64 @@
+import { memo, useCallback, useMemo, useRef } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { __ } from '@wordpress/i18n';
-import MultiSelectMenu from './MultiSelectMenu';
 
-export default function CapabilitiesMenu( { role, defaultValue, description, onChange } ) {
+import Box from '@mui/joy/Box';
+import Autocomplete, { createFilterOptions } from '@mui/joy/Autocomplete';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import AutocompleteOption from '@mui/joy/AutocompleteOption';
+import Checkbox from './Checkbox';
+
+const Input = ( { defaultValue, role, onChange, inputStyles } ) => {
 	const queryClient = useQueryClient();
 	const roles = queryClient.getQueryData( [ 'roles' ] );
 	const capabilities = roles[ role || Object.keys( roles )[ 0 ] ].capabilities;
 
-	return <MultiSelectMenu className="wide"
-		items={ capabilities }
-		key={ role }
-		defaultValue={ defaultValue || Object.keys( capabilities ) }
-		id={ `capabilities-${ role }` }
-		description={ description }
-		onChange={ ( selectedItems ) => onChange( selectedItems ) }
-	>
-		{ __( 'Capabilities' ) }
-	</MultiSelectMenu>;
-}
+	const capabilitiesOptions = useMemo( () => {
+		return Object.keys( capabilities ).reduce( ( opts, key ) => ( { ...opts, [ key ]: { label: capabilities[ key ], id: key } } ), {} );
+	}, [ capabilities ] );
+	const array = useRef( [] );
+
+	const handleOnChange = useCallback( ( arr ) => {
+		array.current = [ ...array.current, ...arr.map( ( val ) => val.id ) ];
+		onChange( [ ...new Set( array.current.flat() ) ] );
+	}, [ ] );
+
+	return <Autocomplete
+		multiple
+		id="capabilities"
+		options={ Object.values( capabilitiesOptions ) }
+		disableCloseOnSelect
+		limitTags={ 2 }
+		value={ ( defaultValue?.length && Object.values( capabilitiesOptions ).filter( ( cap ) => defaultValue.includes( cap.id ) ) ) || Object.values( capabilitiesOptions ) }
+		onChange={ ( event, val ) => handleOnChange( val ) }
+		getOptionLabel={ ( option ) => option.label }
+		slotProps={ { listbox: { sx: { padding: 0 } } } }
+		sx={ { ...inputStyles } }
+		renderOption={ ( props, option, { selected } ) => (
+			<li { ...props } className="pl-m pr-m">
+				<Checkbox
+					defaultValue={ selected }
+				>
+					{ option.label }
+				</Checkbox>
+			</li>
+		) }
+		disableClearable
+	/>;
+};
+
+const CapabilitiesMenu = ( { defaultValue, role, className, onChange, inputStyles } ) => (
+
+	<div className={ className || '' }>
+		{
+			<FormControl>
+				<FormLabel sx={ ( theme ) => ( { fontSize: theme.fontSize.labelSize } ) }>{ __( 'Capabilities' ) }</FormLabel>
+				<Input defaultValue={ defaultValue } role={ role } onChange={ onChange } inputStyles={ inputStyles } />
+			</FormControl>
+
+		}
+	</div>
+);
+
+export default memo( CapabilitiesMenu );
