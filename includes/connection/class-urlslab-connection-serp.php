a77364474c3ecc7c2a016335798faf440162fcca
vzeman <vzeman@qualityunit.com>
2023-11-15 18:37:26 +0100

    chore: refactore storing serp results

--- includes/connection/class-urlslab-connection-serp.php
+++ includes/connection/class-urlslab-connection-serp.php
@@ -8,6 +8,7 @@ use Urlslab_Vendor\OpenAPI\Client\Model\DomainDataRetrievalSerpApiSearchResponse
 use Urlslab_Vendor\OpenAPI\Client\Model\DomainDataRetrievalSerpApiSearchRequest;
 
 class Urlslab_Connection_Serp {
+	private $serp_queries_count = - 1;
 
 	private static Urlslab_Connection_Serp $instance;
 	private static SerpApi $serp_client;
@@ -278,4 +279,158 @@ class Urlslab_Connection_Serp {
 
 		return $ret;
 	}
+
+	public function save_serp_response( \OpenAPI\Client\Model\DomainDataRetrievalSerpApiBulkSearchResponse $serp_response, array $queries ): void {
+		global $wpdb;
+		foreach ( $serp_response->getSerpData() as $idx => $rsp ) {
+
+			switch ( $rsp->getSerpQueryStatus() ) {
+				case \Urlslab_Vendor\OpenAPI\Client\Model\DomainDataRetrievalSerpApiSearchResponse::SERP_QUERY_STATUS_AVAILABLE:
+					$serp_data = $this->extract_serp_data( $queries[ $idx ], $rsp, Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Serp::SLUG )->get_option( Urlslab_Widget_Serp::SETTING_NAME_IMPORT_RELATED_QUERIES_POSITION ) );
+
+					if ( is_bool( $serp_data ) && ! $serp_data ) {
+						$queries[ $idx ]->set_status( Urlslab_Data_Serp_Query::STATUS_ERROR );
+					} else {
+						// valid data
+						$has_monitored_domain = $serp_data['has_monitored_domain'];
+						$urls                 = $serp_data['urls'];
+						$domains              = $serp_data['domains'];
+						$positions            = $serp_data['positions'];
+						$positions_history    = $serp_data['positions_history'];
+
+						if (
+							$has_monitored_domain >= Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Serp::SLUG )->get_option( Urlslab_Widget_Serp::SETTING_NAME_IRRELEVANT_QUERY_LIMIT ) ||
+							Urlslab_Data_Serp_Query::TYPE_USER === $queries[ $idx ]->get_type() ||
+							count( Urlslab_Data_Serp_Domain::get_monitored_domains() ) < Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Serp::SLUG )->get_option( Urlslab_Widget_Serp::SETTING_NAME_IRRELEVANT_QUERY_LIMIT )
+						) {
+							$wpdb->delete(
+								URLSLAB_SERP_POSITIONS_TABLE,
+								array(
+									'query_id' => $queries[ $idx ]->get_query_id(),
+									'country'  => $queries[ $idx ]->get_country(),
+								),
+								array( '%d', '%s' )
+							);
+
+							if ( ! empty( $urls ) ) {
+								$urls[0]->insert_all( $urls, true );
+							}
+							if ( ! empty( $positions ) ) {
+								$positions[0]->insert_all(
+									$positions,
+									false,
+									array(
+										'position',
+										'updated',
+									)
+								);
+							}
+							if ( ! empty( $positions_history ) ) {
+								$positions_history[0]->insert_all( $positions_history, true );
+							}
+							if ( ! empty( $domains ) ) {
+								$domains[0]->insert_all( $domains, true );
+							}
+
+
+							$this->discoverNewQueries( $rsp, $queries[ $idx ] );
+
+							$queries[ $idx ]->set_status( Urlslab_Data_Serp_Query::STATUS_PROCESSED );
+
+							if ( Urlslab_Data_Serp_Query::TYPE_SERP_FAQ === $queries[ $idx ]->get_type() && Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Serp::SLUG )->get_option( Urlslab_Widget_Serp::SETTING_NAME_IMPORT_FAQS ) ) {
+								//if the question is relevant FAQ, add it to the FAQ table
+								$faq_row = new Urlslab_Data_Faq( array( 'question' => ucfirst( $queries[ $idx ]->get_query() ) ), false );
+								if ( ! $faq_row->load( array( 'question' ) ) ) {
+									$faq_row->set_status( Urlslab_Data_Faq::STATUS_EMPTY );
+									$faq_row->insert();
+								}
+							}
+						} else {
+							$queries[ $idx ]->set_status( Urlslab_Data_Serp_Query::STATUS_SKIPPED ); //irrelevant query
+						}
+					}
+
+					break;
+				case \Urlslab_Vendor\OpenAPI\Client\Model\DomainDataRetrievalSerpApiSearchResponse::SERP_QUERY_STATUS_PENDING:
+				case \Urlslab_Vendor\OpenAPI\Client\Model\DomainDataRetrievalSerpApiSearchResponse::SERP_QUERY_STATUS_UPDATING:
+					$queries[ $idx ]->set_status( Urlslab_Data_Serp_Query::STATUS_PROCESSING );
+					break;
+				default:
+					break;
+			}
+			$queries[ $idx ]->update();
+
+		}
+	}
+
+
+	/**
+	 * @param $serp_response
+	 *
+	 * @return void
+	 */
+	private function discoverNewQueries( $serp_response, Urlslab_Data_Serp_Query $query ): void {
+
+		if ( $this->get_serp_queries_count() >= Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Serp::SLUG )->get_option( Urlslab_Widget_Serp::SETTING_NAME_SERP_IMPORT_LIMIT ) ) {
+			return;
+		}
+
+		//Discover new queries
+		$fqs     = $serp_response->getFaqs();
+		$queries = array();
+		if ( ! empty( $fqs ) && Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Serp::SLUG )->get_option( Urlslab_Widget_Serp::SETTING_NAME_IMPORT_FAQS_AS_QUERY ) ) {
+			foreach ( $fqs as $faq ) {
+				$f_query = new Urlslab_Data_Serp_Query(
+					array(
+						'query'           => strtolower( trim( $faq->question ) ),
+						'parent_query_id' => $query->get_query_id(),
+						'country'         => $query->get_country(),
+						'status'          => Urlslab_Data_Serp_Query::STATUS_NOT_PROCESSED,
+						'type'            => Urlslab_Data_Serp_Query::TYPE_SERP_FAQ,
+					)
+				);
+				if ( $f_query->is_valid() ) {
+					$queries[] = $f_query;
+				}
+			}
+		}
+
+		$related = $serp_response->getRelatedSearches();
+		if ( ! empty( $related ) && Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Serp::SLUG )->get_option( Urlslab_Widget_Serp::SETTING_NAME_IMPORT_RELATED_QUERIES ) ) {
+			foreach ( $related as $related_search ) {
+				$new_query = new Urlslab_Data_Serp_Query(
+					array(
+						'query'           => strtolower( trim( $related_search->query ) ),
+						'country'         => $query->get_country(),
+						'parent_query_id' => $query->get_query_id(),
+						'status'          => Urlslab_Data_Serp_Query::STATUS_NOT_PROCESSED,
+						'type'            => Urlslab_Data_Serp_Query::TYPE_SERP_RELATED,
+					)
+				);
+				if ( $new_query->is_valid() ) {
+					$queries[] = $new_query;
+				}
+			}
+		}
+		if ( ! empty( $queries ) ) {
+			$queries[0]->insert_all( $queries, true );
+		}
+	}
+
+	private function get_serp_queries_count(): int {
+		if ( 0 <= $this->serp_queries_count ) {
+			return $this->serp_queries_count;
+		}
+
+		$this->serp_queries_count = get_transient( 'urlslab_serp_queries_count' );
+		if ( false === $this->serp_queries_count ) {
+			global $wpdb;
+			$this->serp_queries_count = $wpdb->get_var( $wpdb->prepare( 'SELECT COUNT(*) FROM ' . URLSLAB_SERP_QUERIES_TABLE . ' WHERE status NOT IN (%s, %s)', Urlslab_Data_Serp_Query::STATUS_ERROR, Urlslab_Data_Serp_Query::STATUS_SKIPPED ) ); // phpcs:ignore
+
+			set_transient( 'urlslab_serp_queries_count', $this->serp_queries_count, 60 );
+		}
+
+		return (int) $this->serp_queries_count;
+	}
+
 }
