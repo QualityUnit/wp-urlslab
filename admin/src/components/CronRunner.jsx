6e2aac20acb7416c9371b720eac2db41da04f23d
Jozef Remeň <jozef.remen@gmail.com>
2023-11-21 15:26:54 +0100

    fix: removing commented out code

--- admin/src/components/CronRunner.jsx
+++ admin/src/components/CronRunner.jsx
@@ -6,7 +6,6 @@ import { useI18n } from '@wordpress/react-i18n';
 
 import cronReducer from '../lib/cronReducer';
 
-// import NotificationsPanel from './NotificationsPanel';
 import Tooltip from '../elements/Tooltip';
 import SvgIcon from '../elements/SvgIcon';
 
@@ -90,17 +89,6 @@ export default function CronRunner() {
 		}
 	}, [ state.cronRunning, cancelCron, handleCronRunner ] );
 
-	// useEffect( () => {
-	// 	if ( state.cronTasksResult?.length ) {
-	// 		dispatch( { type: 'setCronPanelError', cronPanelError: false } );
-	// 		dispatch( { type: 'setCronPanelActive', cronPanelActive: true } );
-	// 		setTimeout( () => {
-	// 			dispatch( { type: 'setCronPanelActive', cronPanelActive: false } );
-	// 			dispatch( { type: 'setCronTasks', cronTasksResult: [] } );
-	// 		}, 4000 );
-	// 	}
-	// }, [ state.cronPanelActive, state.cronTasksResult ] );
-
 	return (
 		<button className="urlslab-cronrunner pos-relative small" onClick={ () => handleCronRunner( true ) }>
 			{ ! state.cronRunning
@@ -113,13 +101,6 @@ export default function CronRunner() {
 					<Tooltip className="showOnHover align-left xxxl">{ __( 'Stop Cron Tasks Execution' ) }</Tooltip>
 				</span>
 			}
-			{ /*
-			<NotificationsPanel className={ `${ state.cronPanelError ? 'error' : 'dark' } wide` } active={ ( state.cronTasksResult?.length > 0 && state.cronPanelActive ) || state.cronPanelError }>
-				{ ! state.cronPanelError
-					? state.cronTasksResult?.map( ( task ) => <div className="message" key={ task.task }>{ task.description }</div> )
-					: <div className="message" key="cronError">{ __( 'Error has occured. Will run again in 1 min.' ) }</div>
-				}
-			</NotificationsPanel> */ }
 		</button>
 	);
 }
