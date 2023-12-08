8d610d45f1709dc46ebd5a4148e75ca81ccfb939
ihdk <ivan.hdk@gmail.com>
2023-12-08 10:15:42 +0100

    fix: memoize useFreeModules hooks return value for deps

--- admin/src/hooks/useFreeModules.jsx
+++ admin/src/hooks/useFreeModules.jsx
@@ -2,21 +2,22 @@
  * Hook to get list of free modules from module query
  */
 
+import { useMemo } from 'react';
 import useModulesQuery from '../queries/useModulesQuery';
 
 const useFreeModules = () => {
 	const { data: modules } = useModulesQuery();
-	const freeModules = [];
-
-	if ( modules && Object.keys( modules ).length ) {
-		for ( const moduleId in modules ) {
-			if ( moduleId !== 'general' && modules[ moduleId ].apikey === false ) {
-				freeModules.push( moduleId );
+	return useMemo( () => {
+		const data = [];
+		if ( modules && Object.keys( modules ).length ) {
+			for ( const moduleId in modules ) {
+				if ( moduleId !== 'general' && modules[ moduleId ].apikey === false ) {
+					data.push( moduleId );
+				}
 			}
 		}
-	}
-
-	return freeModules;
+		return data;
+	}, [ modules ] );
 };
 
 export default useFreeModules;
