0cbb495d1dcb038adc61314799cd7ecd4f6072c3
Jozef Remeň <jozef.remen@gmail.com>
2023-12-12 15:41:16 +0100

    fix: overview pages translatetable

--- admin/src/overview/LazyLoading.jsx
+++ admin/src/overview/LazyLoading.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { __ } from '@wordpress/i18n/';
 import Overview from '../components/OverviewTemplate';
 
 export default function LazyLoadingOverview( { moduleId } ) {
@@ -9,26 +10,26 @@ export default function LazyLoadingOverview( { moduleId } ) {
 			{
 				section === 'about' &&
 				<section>
-					<p>Lazy loading is an essential technique for optimizing page performance and reducing page load time. Traditionally, all the images and videos are loaded upon accessing a website. By using lazy loading, this content is loaded only when it enters a user’s viewport.</p>
-					<p>URLsLab’s Lazy Loading module takes care of lazy loading throughout your website. It ensures that the browser loads all the necessary assets only when they are actually needed. The Lazy Loading module allows you to do this with various pieces of content, including YouTube videos. You can set up and employ the Lazy Loading module from one convenient interface without the need to be skilled in coding.</p>
-					<p>So how can the Lazy Loading module help your website? By decreasing the amount of data that needs to be transferred, lazy loading boosts page loading speed. This comes with a plethora of benefits, including higher SEO rankings, improved visitor experience and engagement, and enhanced organic traffic. Moreover, using the Lazy Loading module saves resources, as it reduces server load.</p>
+					<p>{ __( 'Lazy loading is an essential technique for optimizing page performance and reducing page load time. Traditionally, all the images and videos are loaded upon accessing a website. By using lazy loading, this content is loaded only when it enters a user’s viewport.' ) }</p>
+					<p>{ __( 'URLsLab’s Lazy Loading module takes care of lazy loading throughout your website. It ensures that the browser loads all the necessary assets only when they are actually needed. The Lazy Loading module allows you to do this with various pieces of content, including YouTube videos. You can set up and employ the Lazy Loading module from one convenient interface without the need to be skilled in coding.' ) }</p>
+					<p>{ __( 'So how can the Lazy Loading module help your website? By decreasing the amount of data that needs to be transferred, lazy loading boosts page loading speed. This comes with a plethora of benefits, including higher SEO rankings, improved visitor experience and engagement, and enhanced organic traffic. Moreover, using the Lazy Loading module saves resources, as it reduces server load.' ) }</p>
 				</section>
 			}
 			{
 				section === 'integrate' &&
 					<section>
-						<h4>Video data shortcode</h4>
+						<h4>{ __( 'Video data shortcode' ) }</h4>
 						<code>[urlslab-video videoid="YT-video-ID" attribute="atr_name"]</code>
-						<p>Supported video attribute names: title, description, thumbnail_url, published_at, duration, captions, captions_text, channel_title</p>
+						<p>{ __( 'Supported video attribute names: title, description, thumbnail_url, published_at, duration, captions, captions_text, channel_title' ) }</p>
 					</section>
 			}
 			{
 				section === 'faq' &&
 					<section>
-						<h4>What are the SEO benefits of using the Lazy Loading module?</h4>
-						<p>Websites with peak performance are usually favored by search engines. When you use lazy loading, you can decrease page loading times, and improve your overall website performance.</p>
-						<h4>Does the Lazy Loading allow me to specify which parts of my web page to lazy load?</h4>
-						<p>Using URLsLab's Lazy Loading module, you can select which web page parts to lazy load. For controlling content lazy loading, extra parameters can be defined in the content settings.</p>
+						<h4>{ __( 'What are the SEO benefits of using the Lazy Loading module?' ) }</h4>
+						<p>{ __( 'Websites with peak performance are usually favored by search engines. When you use lazy loading, you can decrease page loading times, and improve your overall website performance.' ) }</p>
+						<h4>{ __( 'Does the Lazy Loading allow me to specify which parts of my web page to lazy load?' ) }</h4>
+						<p>{ __( 'Using URLsLab’s Lazy Loading module, you can select which web page parts to lazy load. For controlling content lazy loading, extra parameters can be defined in the content settings.' ) }</p>
 					</section>
 			}
 		</Overview>
--- admin/src/overview/SearchAndReplace.jsx
+++ admin/src/overview/SearchAndReplace.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { __ } from '@wordpress/i18n/';
 import Overview from '../components/OverviewTemplate';
 
 export default function SearchAndReplaceOverview( { moduleId } ) {
@@ -9,18 +10,18 @@ export default function SearchAndReplaceOverview( { moduleId } ) {
 			{
 				section === 'about' &&
 				<section>
-					<p>Managing a website with a large content volume can be a hassle. Luckily, URLsLab’s Search and Replace module can save you time and frustration by automatically mass-replacing URLs and content on the fly.</p>
-					<p>Using the Search and Replace module is a great way to resolve any issues with your content quickly and efficiently. It also provides a reliable fallback method, in case you want to roll back any actions. All changes are reversible at any time, so you don’t have to worry about making permanent changes to the database.</p>
-					<p>Overall, the Search and Replace module is also incredibly versatile and can be used for various purposes. From replacing incorrect URLs in the content to correcting typos and other mistakes, the module can help you streamline many manual tasks. With such a wide range of capabilities, the Search and Replace module is an invaluable tool for anyone who needs to quickly and effectively make changes in the content.</p>
+					<p>{ __( 'Managing a website with a large content volume can be a hassle. Luckily, URLsLab’s Search and Replace module can save you time and frustration by automatically mass-replacing URLs and content on the fly.' ) }</p>
+					<p>{ __( 'Using the Search and Replace module is a great way to resolve any issues with your content quickly and efficiently. It also provides a reliable fallback method, in case you want to roll back any actions. All changes are reversible at any time, so you don’t have to worry about making permanent changes to the database.' ) }</p>
+					<p>{ __( 'Overall, the Search and Replace module is also incredibly versatile and can be used for various purposes. From replacing incorrect URLs in the content to correcting typos and other mistakes, the module can help you streamline many manual tasks. With such a wide range of capabilities, the Search and Replace module is an invaluable tool for anyone who needs to quickly and effectively make changes in the content.' ) }</p>
 				</section>
 			}
 			{
 				section === 'faq' &&
 					<section>
-						<h4>How does the Search and Replace module handle incorrect URLs?</h4>
-						<p>Incorrect or outdated URLs are in the Search and Replace module’s realm of capabilities. You can replace incorrect URLs in bulk, or one one by. This is especially useful when rebranding your website.</p>
-						<h4>Can I undo changes made by the Search and Replace module?</h4>
-						<p>Yes, you can. With a single click, you can revert any changes. This way you don’t have to worry about making permanent changes in the database and not having a reliable fallback method.</p>
+						<h4>{ __( 'How does the Search and Replace module handle incorrect URLs?' ) }</h4>
+						<p>{ __( 'Incorrect or outdated URLs are in the Search and Replace module’s realm of capabilities. You can replace incorrect URLs in bulk, or one one by. This is especially useful when rebranding your website.' ) }</p>
+						<h4>{ __( 'Can I undo changes made by the Search and Replace module?' ) }</h4>
+						<p>{ __( 'Yes, you can. With a single click, you can revert any changes. This way you don’t have to worry about making permanent changes in the database and not having a reliable fallback method.' ) }</p>
 					</section>
 			}
 		</Overview>
