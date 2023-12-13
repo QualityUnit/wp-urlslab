8aab6392d150f40b603711ce3163a9c9129c7ec0
Jozef Remen <jozef.remen@gmail.com>
2023-12-13 07:28:35 +0100

    fix: overview pages translatetable (#1514)

--- admin/src/overview/CustomHtml.jsx
+++ admin/src/overview/CustomHtml.jsx
@@ -1,52 +1,33 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { __ } from '@wordpress/i18n/';
 import Overview from '../components/OverviewTemplate';
 
-export default function CustomHtmlOverview({moduleId}) {
-    const [section, setSection] = useState('about');
+export default function CustomHtmlOverview( { moduleId } ) {
+	const [ section, setSection ] = useState( 'about' );
 
-    return (
-        <Overview moduleId={moduleId} section={(val) => setSection(val)} noIntegrate>
-            {
-                section === 'about' &&
-                <section>
-                    <p>Modifying your website’s content or behavior can be a time-consuming process. The Code Injection
-                        module is a versatile and powerful solution designed to inject custom HTML code into any section
-                        of your web page. This module allows you to seamlessly integrate various third-party
-                        applications, such as <a href="https://tagmanager.google.com/" target="_blank">Google Tag
-                            Manager</a>, <a href="https://analytics.google.com/" target="_blank">Google
-                            Analytics</a>, <a href="https://www.liveagent.com" target="_blank">live chat</a>, <a
-                            href="https://www.postaffiliatepro.com" target="_blank">affiliate tracking</a> or any other
-                        external service.</p>
-                    <p>With this module, you can define specific rules for loading your custom HTML code, giving you
-                        complete control over where your code will be executed. As a result, you can ensure your code
-                        has the desired impact on your site's user experience while avoiding any potential
-                        conflicts.</p>
-                    <p>Using URLsLab’s Code Injection module adds a level of versatility to your website. The module
-                        allows you to improve user experience while streamlining website operations and saving
-                        resources.</p>
-                </section>
-            }
-            {
-                section === 'faq' &&
-                <section>
-                    <h4>When would I need this module?</h4>
-                    <p>Example could be adding Google Analytics tracking code or <a href="https://www.liveagent.com">Live
-                        Chat</a> button to your website. Thanks to this module you will just copy and paste the given
-                        HTML/javascript to this module and on each page of your website will be included the code
-                        automatically. No
-                        need to edit Wordpress templates anymore.</p>
-                    <h4>Can I control where my custom HTML code will be injected?</h4>
-                    <p>Yes, you can. In the module’s Settings tab, you can define each parameter in intricate detail
-                        when adding a new rule, ensuring that the custom HTML is executed according to your
-                        specifications. Your custom HTML code will be applied to all your pages if you add a default
-                        rule.</p>
-                    <h4>How does the Code Injection module avoid potential conflicts with other codes on my
-                        website?</h4>
-                    <p>With the Code Injection module, you can specify where your custom HTML code will be executed,
-                        eliminating conflicts with other elements. Thanks to flexible rules you can cherry-pick pages
-                        where will be custom HTML included while other pages will be intact.</p>
-                </section>
-            }
-        </Overview>
-    );
+	return (
+		<Overview moduleId={ moduleId } section={ ( val ) => setSection( val ) } noIntegrate>
+			{
+				section === 'about' &&
+				<section>
+					<p>{ __( 'Modifying your website’s content or behavior can be a time-consuming process. The Code Injection module is a versatile and powerful solution designed to inject custom HTML code into any sectioncof your web page. This module allows you to seamlessly integrate various third-party applications, such as' ) } <a href="https://tagmanager.google.com/" target="_blank" rel="noreferrer">Google Tag Manager</a>, <a href="https://analytics.google.com/" target="_blank" rel="noreferrer">Google Analytics</a>, <a href="https://www.liveagent.com" target="_blank" rel="noreferrer">{ __( 'live chat' ) }</a>, <a
+						href="https://www.postaffiliatepro.com" target="_blank" rel="noreferrer">{ __( 'affiliate tracking' ) }</a> or any other external service.</p>
+					<p>{ __( 'With this module, you can define specific rules for loading your custom HTML code, giving you complete control over where your code will be executed. As a result, you can ensure your code has the desired impact on your site’s user experience while avoiding any potential conflicts.' ) }</p>
+					<p>{ __( 'Using URLsLab’s Code Injection module adds a level of versatility to your website. The module allows you to improve user experience while streamlining website operations and saving resources.' ) }</p>
+				</section>
+			}
+			{
+				section === 'faq' &&
+				<section>
+					<h4>{ __( 'When would I need this module?' ) }</h4>
+					<p>{ __( 'Example could be adding Google Analytics tracking code or ' ) }<a href="https://www.liveagent.com">Live
+						Chat</a> { __( 'button to your website. Thanks to this module you will just copy and paste the given HTML/javascript to this module and on each page of your website will be included the code automatically. No need to edit Wordpress templates anymore.' ) }</p>
+					<h4>{ __( 'Can I control where my custom HTML code will be injected?' ) }</h4>
+					<p>{ __( 'Yes, you can. In the module’s Settings tab, you can define each parameter in intricate detail when adding a new rule, ensuring that the custom HTML is executed according to your specifications. Your custom HTML code will be applied to all your pages if you add a default rule.' ) }</p>
+					<h4>{ __( 'How does the Code Injection module avoid potential conflicts with other codes on my website?' ) }</h4>
+					<p>{ __( 'With the Code Injection module, you can specify where your custom HTML code will be executed, eliminating conflicts with other elements. Thanks to flexible rules you can cherry-pick pages where will be custom HTML included while other pages will be intact.' ) }</p>
+				</section>
+			}
+		</Overview>
+	);
 }
--- admin/src/overview/HtmlOptimizer.jsx
+++ admin/src/overview/HtmlOptimizer.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { __ } from '@wordpress/i18n/';
 import Overview from '../components/OverviewTemplate';
 
 export default function HtmlOptimizerOverview( { moduleId } ) {
@@ -9,18 +10,18 @@ export default function HtmlOptimizerOverview( { moduleId } ) {
 			{
 				section === 'about' &&
 				<section>
-					<p>The use of inline CSS instead of external CSS files is a great way to improve page performance and reduce content-blocker requests - requests that block other requests for some time during page load, and they are rapidly slowing down page performance and the grade in Core Web Vitals.</p>
-					<p>Inline CSS is a way to add styling directly to an HTML element, eliminating the need to make additional requests to external CSS files, thus improving page speed. The Optimisations module allows you to overview and process CSS and JavaScript files from one convenient interface.</p>
-					<p>Using URLsLab’s Optimisations allows you to optimize such files even without extensive coding skills, making it an accessible solution for website owners and SEO professionals across the board. Using this module results in improved website performance, enhanced SEO rankings, and ultimately better user experience.</p>
+					<p>{ __( 'The use of inline CSS instead of external CSS files is a great way to improve page performance and reduce content-blocker requests - requests that block other requests for some time during page load, and they are rapidly slowing down page performance and the grade in Core Web Vitals.' ) }</p>
+					<p>{ __( 'Inline CSS is a way to add styling directly to an HTML element, eliminating the need to make additional requests to external CSS files, thus improving page speed. The Optimisations module allows you to overview and process CSS and JavaScript files from one convenient interface.' ) }</p>
+					<p>{ __( 'Using URLsLab’s Optimisations allows you to optimize such files even without extensive coding skills, making it an accessible solution for website owners and SEO professionals across the board. Using this module results in improved website performance, enhanced SEO rankings, and ultimately better user experience.' ) }</p>
 				</section>
 			}
 			{
 				section === 'faq' &&
 					<section>
-						<h4>How does the Optimisations module improve website performance?</h4>
-						<p>By removing content-blocker requests, the Optimisations module assists in boosting your website performance.</p>
-						<h4>Can I use the Optimisations module on a large website?</h4>
-						<p>Yes, all URLsLab modules are designed to integrate with websites of all sizes, including those with high volumes of content and pages.</p>
+						<h4>{ __( 'How does the Optimisations module improve website performance?' ) }</h4>
+						<p>{ __( 'By removing content-blocker requests, the Optimisations module assists in boosting your website performance.' ) }</p>
+						<h4>{ __( 'Can I use the Optimisations module on a large website?' ) }</h4>
+						<p>{ __( 'Yes, all URLsLab modules are designed to integrate with websites of all sizes, including those with high volumes of content and pages.' ) }</p>
 					</section>
 			}
 		</Overview>
