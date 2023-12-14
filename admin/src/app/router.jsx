88f3383a2c7483054178526d0e878f37b8767dd7
Ivan Hudak <ivan.hdk@gmail.com>
2023-12-14 13:55:00 +0100

    fix: modules groups routes

--- admin/src/app/router.jsx
+++ admin/src/app/router.jsx
@@ -3,12 +3,14 @@ import { createHashRouter } from 'react-router-dom';
 import AppRoot from '../AppRoot';
 
 import modulesList from './modulesList.json';
+import useModulesQuery from '../queries/useModulesQuery';
 
 // manually created static routes
 const Modules = lazy( () => import( '../modules/static/Modules' ) );
 const Settings = lazy( () => import( '../modules/static/Settings' ) );
 const Schedule = lazy( () => import( '../modules/static/Schedule' ) );
 const TagsLabels = lazy( () => import( '../modules/static/TagsLabels' ) );
+const Page404 = lazy( () => import( '../modules/static/Page404' ) );
 
 // routes generated on the base of modules files available in ./modules folder
 const modulesRoutes = modulesList.map( ( ( moduleName ) => {
@@ -55,7 +57,7 @@ const routes = [
 	// 404 route
 	{
 		path: '*',
-		element: <Modules />,
+		element: <Page404 />,
 	},
 
 	// generated modules routes
@@ -69,3 +71,34 @@ export const router = createHashRouter( [
 		children: routes,
 	},
 ] );
+
+const useRouter = () => {
+	const { data: modules = {} } = useModulesQuery();
+	if ( Object.keys( modules ).length ) {
+		const groups = {};
+		for ( const m in modules ) {
+			const module = modules[ m ];
+			if ( module.id !== 'general' && module.group ) {
+				const groupRoute = Object.keys( module.group )[ 0 ];
+				const groupName = module.group[ groupRoute ];
+				groups[ groupRoute ] = groupName;
+			}
+		}
+		const groupsRoutes = Object.keys( groups ).map( ( groupRoute ) => ( {
+			path: groupRoute,
+			element: <Modules />,
+		} ) );
+
+		return createHashRouter( [
+			{
+				path: '/',
+				element: <AppRoot />,
+				children: [ ...routes, ...groupsRoutes ],
+			},
+		] );
+	}
+
+	return null;
+};
+
+export default useRouter;
