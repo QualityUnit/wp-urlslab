452468696f6e9033d32df82d3474b38f66b2d141
vzeman <vzeman@qualityunit.com>
2023-12-14 18:53:51 +0100

    fix: garbage collector for cron

---
+++ includes/cron/class-urlslab-cron-cache-garbage-collector.php
@@ -0,0 +1,24 @@
+<?php
+
+class Urlslab_Cron_Cache_Garbage_Collector extends Urlslab_Cron {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function get_description(): string {
+		return __( 'Cache garbage collection', 'urlslab' );
+	}
+
+	protected function execute(): bool {
+		if ( ! Urlslab_User_Widget::get_instance()->is_widget_activated( Urlslab_Widget_Cache::SLUG ) ) {
+			return false;
+		}
+		/** @var Urlslab_Widget_Cache $widget */
+		$widget = Urlslab_User_Widget::get_instance()->get_widget( Urlslab_Widget_Cache::SLUG );
+		$widget->invalidate_old_cache();
+		$widget->garbage_collection();
+
+		return false;
+	}
+}
