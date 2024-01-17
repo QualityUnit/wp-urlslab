9e932c97a7ea5353b08a35943f3e06010231fa54
Ivan Hudak <ivan.hdk@gmail.com>
2024-01-17 15:49:30 +0100

    feat: date range picker allow submit values with button or closing

--- admin/src/elements/DateRangeButton.jsx
+++ admin/src/elements/DateRangeButton.jsx
@@ -1,4 +1,5 @@
-import { memo, useState } from 'react';
+import { memo, useCallback, useState } from 'react';
+import { __ } from '@wordpress/i18n';
 
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css';
@@ -6,55 +7,83 @@ import { DateRangePicker } from 'react-date-range';
 
 import { getDateFnsFormat } from '../lib/helpers';
 
-import Box from '@mui/joy/Box';
+import Stack from '@mui/joy/Stack';
 import Button from '@mui/joy/Button';
 import Tooltip from '@mui/joy/Tooltip';
 import { ClickAwayListener } from '@mui/base/ClickAwayListener';
+import { useTheme } from '@mui/joy';
 
-function DateRangeButton( { startDate, endDate, className, handleSelect, customButtonText, buttonProps, tooltipProps } ) {
-	const { date, getSettings } = window.wp.date;
-	const dateToString = ( datetime ) => {
-		return date( getSettings().formats.date, datetime );
-	};
+const { date, getSettings } = window.wp.date;
+const dateToString = ( datetime ) => {
+	return date( getSettings().formats.date, datetime );
+};
 
-	const [ range, setRange ] = useState( [ {
+function DateRangeButton( { startDate, endDate, className, handleSelect, customButtonText, submitWithButton, submitOnClose, buttonProps, tooltipProps } ) {
+	const theme = useTheme();
+	const [ range, setRange ] = useState( {
 		startDate: new Date( startDate ),
 		endDate: new Date( endDate ),
 		key: 'selection',
-	} ] );
+	} );
+
 	const [ buttonText, setButtonText ] = useState( dateToString( startDate ) + ' - ' + dateToString( endDate ) );
 	const [ showCalendar, setShowCalendar ] = useState( false );
 
-	const toggleCalendar = () => {
-		setShowCalendar( ! showCalendar );
-	};
-	const onSelect = ( newRange ) => {
-		setRange( [ newRange.selection ] );
-		setButtonText( dateToString( newRange.selection.startDate ) + ' - ' + dateToString( newRange.selection.endDate ) );
-		handleSelect( newRange.selection );
-	};
+	const toggleCalendar = useCallback( () => {
+		setShowCalendar( ( s ) => ! s );
+	}, [] );
 
+	const onSelect = useCallback( ( newRange ) => {
+		setRange( newRange.selection );
+		setButtonText( dateToString( newRange.selection.startDate ) + ' - ' + dateToString( newRange.selection.endDate ) );
+		if ( ! submitWithButton ) {
+			handleSelect( newRange.selection );
+		}
+	}, [ handleSelect, submitWithButton ] );
+
+	const manualSubmit = useCallback( () => {
+		handleSelect( range );
+		toggleCalendar();
+	}, [ handleSelect, range, toggleCalendar ] );
 	return (
 		<Tooltip
 			variant="outlined"
 			open={ showCalendar }
 			title={
-				<ClickAwayListener onClickAway={ toggleCalendar }>
-					<Box>
+				<ClickAwayListener onClickAway={ () => {
+					if ( submitOnClose ) {
+						manualSubmit();
+						return;
+					}
+					toggleCalendar();
+				} }>
+					<Stack>
 						<DateRangePicker
 							showSelectionPreview={ true }
 							moveRangeOnFirstSelection={ false }
 							retainEndDateOnFirstSelection={ true }
 							className={ className }
-							ranges={ range }
+							ranges={ [ range ] }
 							months={ 2 }
 							direction="horizontal"
 							onChange={ onSelect }
 							maxDate={ new Date() }
 							dateDisplayFormat={ getDateFnsFormat().date }
 							weekStartsOn={ getSettings().l10n.startOfWeek }
+							rangeColors={ [ theme.vars.palette.primary[ 500 ], theme.vars.palette.primary[ 400 ], theme.vars.palette.primary[ 300 ] ] }
 						/>
-					</Box>
+						{ submitWithButton &&
+							<Button
+								color="primary"
+								variant="solid"
+								size="md"
+								onClick={ manualSubmit }
+								sx={ { '--Button-radius': 0 } }
+							>
+								{ __( 'Apply selected range' ) }
+							</Button>
+						}
+					</Stack>
 				</ClickAwayListener>
 			}
 			fitContent
