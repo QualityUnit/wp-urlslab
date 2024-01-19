82a2bce64c9654636c68374ce7b1eef664705318
Jozef Remeň <jozef.remen@gmail.com>
2024-01-19 14:28:28 +0100

    fix: table edit panel background color on selected row

--- admin/src/components/TableComponent.jsx
+++ admin/src/components/TableComponent.jsx
@@ -34,7 +34,6 @@ export default function Table( { resizable, defaultSorting, children, className,
 		slug = customSlug;
 	}
 
-	console.log( 'table' );
 	const [ rowSelection, setRowSelection ] = useState( {} );
 
 	const setColumnVisibility = useCallback( ( updater ) => {
