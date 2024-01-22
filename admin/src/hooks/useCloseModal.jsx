ae1396c9b96c65a792d5ad4c4449d439fd51cf4a
Ivan Hudak <ivan.hdk@gmail.com>
2024-01-22 11:22:35 +0100

    fix: modal window keuyp event removal on effect cleanup

--- admin/src/hooks/useCloseModal.jsx
+++ admin/src/hooks/useCloseModal.jsx
@@ -2,7 +2,7 @@ import { useCallback, useEffect } from 'react';
 import { ReactComponent as CloseIcon } from '../assets/images/icons/icon-close.svg';
 import useTablePanels from './useTablePanels';
 
-export default function useCloseModal( ) {
+export default function useCloseModal() {
 	const activePanel = useTablePanels( ( state ) => state.activePanel );
 	const activatePanel = useTablePanels( ( state ) => state.activatePanel );
 	const setRowToEdit = useTablePanels( ( state ) => state.setRowToEdit );
@@ -17,13 +17,18 @@ export default function useCloseModal( ) {
 		return operationVal;
 	}, [ activatePanel, setOptions, setRowToEdit ] );
 
-	useEffect( () => {
-		window.addEventListener( 'keyup', ( event ) => {
-			if ( event.key === 'Escape' ) {
-				handleClose( );
-			}
-		} );
+	const handleCloseByEsc = useCallback( ( event ) => {
+		if ( event.key === 'Escape' ) {
+			handleClose( );
+		}
 	}, [ handleClose ] );
+
+	useEffect( () => {
+		window.addEventListener( 'keyup', handleCloseByEsc );
+		return () => {
+			window.removeEventListener( 'keyup', handleCloseByEsc );
+		};
+	}, [ handleCloseByEsc ] );
 
 	if ( activePanel !== undefined ) {
 		document.querySelector( '#urlslab-root' ).classList.add( 'dark' );
