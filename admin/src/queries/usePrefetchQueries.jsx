13c69fff9bebdefe72dd5fcb9f22ccf2f5ded480
Jozef Remeň <jozef.remen@gmail.com>
2024-01-24 10:27:59 +0100

    fix: created post type endpoint for web vitals table

--- admin/src/queries/usePrefetchQueries.jsx
+++ admin/src/queries/usePrefetchQueries.jsx
@@ -57,7 +57,7 @@ const usePrefetchQueries = () => {
 	queryClient.prefetchQuery( {
 		queryKey: [ 'postTypes' ],
 		queryFn: async () => {
-			const response = await getFetch( 'generator/post' );
+			const response = await getFetch( 'web-vitals/post' );
 			if ( response.ok ) {
 				return response.json();
 			}
