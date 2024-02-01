9a2543c32d17a2e569a76b2127d0912177f36240
Jozef Remeň <jozef.remen@gmail.com>
2024-02-01 11:20:23 +0100

    feat: editable attributes for url and count

--- blocks/includes/blocks/class-urlslab-faqs-elementor.php
+++ blocks/includes/blocks/class-urlslab-faqs-elementor.php
@@ -26,11 +26,50 @@ class Urlslab_FAQs_Elementor extends Widget_Base {
 		return array( 'faq', 'urlslab' );
 	}
 
+	protected function register_controls() {
+		
+		$this->start_controls_section(
+			'content_section',
+			array(
+				'label' => $this->get_title(),
+				'tab'   => Controls_Manager::TAB_CONTENT,
+			)
+		);
+
+		$this->add_control(
+			'url',
+			array(
+				'type'        => Controls_Manager::TEXT,
+				'label'       => __( 'Page url', 'urlslab' ),
+				'placeholder' => __( 'Insert website url', 'urlslab' ),
+				'description' => __( 'Link to the page for which a FAQs should be generated.', 'urlslab' ),
+				'ai'          => array( 'active' => false ),
+			)
+		);
+
+		$this->add_control(
+			'count',
+			array(
+				'type'        => Controls_Manager::NUMBER,
+				'label'       => __( 'Number of FAQs', 'urlslab' ),
+				'description' => __( 'Define how many questions should show.', 'urlslab' ),
+				'default'     => 10,
+				'ai'          => array( 'active' => false ),
+			)
+		);
+
+		$this->end_controls_section();
+	}
+
 	public function render() {
+		$settings = $this->get_settings_for_display();
+
+		$this->add_render_attribute( 'shortcode', 'url', $settings['url'] );
+		$this->add_render_attribute( 'shortcode', 'count', $settings['count'] );
 		ob_start();
 		?> 
 			<div class="urlslab-block urlslab-block-<?= esc_attr( $this->slug ) ?>-elementor">
-				<?php echo do_shortcode( '[urlslab-faq]' ); ?>
+				<?php echo do_shortcode( '[urlslab-faq ' . $this->get_render_attribute_string( 'shortcode' ) . ']' ); ?>
 			</div>
 		<?php
 		echo wp_kses_post( ob_get_clean() );
--- blocks/src/blocks/faqs/edit.js
+++ blocks/src/blocks/faqs/edit.js
@@ -3,21 +3,45 @@ import classNames from 'classnames';
 import { __ } from '@wordpress/i18n';
 import { Icon, postComments } from '@wordpress/icons';
 import { useInstanceId } from '@wordpress/compose';
-import { useBlockProps } from '@wordpress/block-editor';
-import { Button, Spinner } from '@wordpress/components';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, TextControl, Button, Spinner } from '@wordpress/components';
 
 import useModules from '../../hooks/useModules';
 
 const slug = 'faqs';
 const moduleSlug = 'urlslab-faq';
 
-const Edit = ( ) => {
+const Edit = ( { attributes, setAttributes } ) => {
+	const { url, count } = attributes;
 	const instanceId = useInstanceId( Edit );
 	const inputId = `urlslab-${ slug }-input-${ instanceId }`;
 	const { moduleStatus, activateModule } = useModules( { moduleSlug } );
 
 	return (
 		<>
+			{ moduleStatus && moduleStatus?.active &&
+				<InspectorControls key="setting">
+					<PanelBody
+						title={ __( 'Options', 'urlslab' ) }
+						initialOpen={ true }
+					>
+						<TextControl
+							label={ __( 'Page URL', 'urlslab' ) }
+							help={ __( 'Link to the page for which a FAQs should be generated.', 'urlslab' ) }
+							value={ url }
+							onChange={ ( val ) => setAttributes( { url: val } ) }
+						/>
+
+						<TextControl
+							label={ __( 'Number of FAQs', 'urlslab' ) }
+							help={ __( 'Define how many questions should show.', 'urlslab' ) }
+							value={ count }
+							onChange={ ( val ) => setAttributes( { count: val } ) }
+						/>
+
+					</PanelBody>
+				</InspectorControls>
+			}
 			<div {
 				...useBlockProps(
 					{ className: classNames(
@@ -34,7 +58,7 @@ const Edit = ( ) => {
 
 				<div className="urlslab-fullwidth-wrapper">
 					{ moduleStatus && moduleStatus?.active
-						? <strong>{ '[urlslab-faq]' }</strong>
+						? <strong>{ `[urlslab-faq ${ url ? 'url="' + url + '"' : '' } ${ count ? 'count="' + count + '"' : '' }]` }</strong>
 						: moduleStatus
 							? <>
 								<p>{ __( 'This widget requires FAQs module in URLsLab to be active. If you want to use this widget, activate FAQs module please.' ) }</p>
