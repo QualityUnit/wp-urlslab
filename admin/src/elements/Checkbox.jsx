53163a9b65af2606636dc99598bfd9e4f4aecbc3
Ivan Hudak <ivan.hdk@gmail.com>
2024-02-02 17:33:43 +0100

    feat: implemented controlled and uncontrolled checkbox

--- admin/src/elements/Checkbox.jsx
+++ admin/src/elements/Checkbox.jsx
@@ -1,24 +1,40 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import '../assets/styles/elements/_Checkbox.scss';
 
-export default function Checkbox( { defaultValue, hasComponent, smallText, readOnly, radial, name, className, onChange, textBefore, children, disabled } ) {
-	const [ isChecked, setChecked ] = useState( defaultValue ? true : false );
+/*
+* controlled: input value controlled by parent component using 'value' prop, defaultValue is 'value'
+* uncontrolled: input value handled by inner state, default value is provided 'defaultValue' prop.
+*/
+export default function Checkbox( { defaultValue, value, hasComponent, smallText, readOnly, radial, name, className, onChange, textBefore, children, disabled } ) {
+	const [ isChecked, setChecked ] = useState( ( value || defaultValue ) ? true : false );
+	const isControlledInit = useRef( true );
+	const isControlled = value !== undefined;
 
 	const handleOnChange = ( ) => {
 		if ( onChange && ! readOnly ) {
 			onChange( ! isChecked );
 		}
-		if ( ! readOnly ) {
+		if ( ! readOnly && ! isControlled ) {
 			setChecked( ( state ) => ! state );
 		}
 	};
+
+	useEffect( () => {
+		// update value from parent and prevent double render on input mount
+		if ( isControlled && ! isControlledInit.current ) {
+			setChecked( value );
+		}
+		isControlledInit.current = false;
+	}, [ value, isControlled ] );
+
 	return (
 		<label className={ `urlslab-checkbox ${ className || '' } ${ textBefore ? 'textBefore' : '' } ${ radial ? 'radial' : '' }` }>
 			<input
 				className={ `urlslab-checkbox-input ${ isChecked ? 'checked' : '' }` }
 				type={ name ? 'radio' : 'checkbox' }
 				name={ name || '' }
-				defaultChecked={ isChecked }
+				checked={ isChecked }
+				defaultValue={ isControlled ? value : defaultValue }
 				disabled={ disabled ? 'disabled' : '' }
 				onChange={ ( event ) => handleOnChange( event ) }
 			/>
@@ -30,3 +46,4 @@ export default function Checkbox( { defaultValue, hasComponent, smallText, readO
 		</label>
 	);
 }
+
