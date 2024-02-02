560c4180ca93a10c4f8989845a385815431db1ca
Ivan Hudak <ivan.hdk@gmail.com>
2024-02-02 17:35:38 +0100

    feat: custom table row select checkbox to simplify global states management

---
+++ admin/src/elements/TableSelectCheckbox.jsx
@@ -0,0 +1,29 @@
+import { memo } from 'react';
+import useSelectRows from '../hooks/useSelectRows';
+import useChangeRow from '../hooks/useChangeRow';
+import useTableStore from '../hooks/useTableStore';
+
+import { Checkbox } from '../lib/tableImports';
+
+const TableSelectCheckbox = ( { tableElement } ) => {
+	const slug = useTableStore( ( state ) => state.activeTable );
+	const selectedAll = useSelectRows( ( state ) => state.selectedAll[ slug ] );
+	const selectedRows = useSelectRows( ( state ) => state.selectedRows[ slug ] );
+	const { selectRows } = useChangeRow();
+
+	const rowId = tableElement?.row?.id;
+	const isSelectAll = rowId === undefined;
+
+	const selected = isSelectAll
+		? selectedAll
+		: selectedRows?.[ rowId ] !== undefined;
+
+	return (
+		<Checkbox
+			value={ selected }
+			onChange={ ( checked ) => selectRows( tableElement, checked, isSelectAll ) }
+		/>
+	);
+};
+
+export default memo( TableSelectCheckbox );
