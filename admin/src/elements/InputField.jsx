8312131952dff4e3c5da0e4aed9c6119fa640030
Ivan Hudak <ivan.hdk@gmail.com>
2024-02-06 09:47:40 +0100

    fix: input field handle correctly nullish "0" value

--- admin/src/elements/InputField.jsx
+++ admin/src/elements/InputField.jsx
@@ -10,7 +10,8 @@ import Tooltip from './Tooltip';
 */
 function InputField( { defaultValue, value, isLoading, autoFocus, placeholder, message, liveUpdate, className, type, min, max, readonly, disabled, label, title, description, labelInline, onChange, onKeyDown, onBlur, onFocus, onKeyUp, children, required, style } ) {
 	const { __ } = useI18n();
-	const [ val, setVal ] = useState( value || defaultValue || '' );
+	// eslint-disable-next-line no-nested-ternary
+	const [ val, setVal ] = useState( value !== undefined ? value : defaultValue !== undefined ? defaultValue : '' );
 	const isControlledInit = useRef( true );
 	const isControlled = value !== undefined;
 	const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
