94c3777fb16bc9bbd27c494ceaa96c360f854aee
Ivan Hudak <ivan.hdk@gmail.com>
2024-02-11 13:43:01 +0100

    fix: try catch in useInfiniteQuery methods because of not logged possible errors

--- admin/src/hooks/useInfiniteFetch.jsx
+++ admin/src/hooks/useInfiniteFetch.jsx
@@ -21,48 +21,59 @@ export default function useInfiniteFetch( options, maxRows = 50 ) {
 	const query = useInfiniteQuery( {
 		queryKey: [ key, filtersArray( filters ), sorting, fetchOptions ],
 		queryFn: async ( { pageParam = '', signal } ) => {
-			const { lastRowId, sortingFilters, sortingFiltersLastValue } = pageParam;
-			if ( ! wait ) {
-				const response = await postFetch(
-					key,
-					{
-						...fetchOptions,
-						sorting: [ ...sortingArray( key ), { col: paginationId, dir: 'ASC' } ],
-						filters: sortingFilters
-							? [
-								{ cond: 'OR',
-									filters: [
-										{ cond: 'AND', filters: [ ...sortingFiltersLastValue, { col: paginationId, op: '>', val: lastRowId } ] },
-										...sortingFilters,
-									],
-								},
-								...filtersArray( filters ),
-							]
-							: [ ...filtersArray( filters ) ],
-						rows_per_page: maxRows,
-					},
-					{ ...( allowTableFetchAbort ? { signal } : null ) }
-				);
-				if ( response.ok ) {
-					return response.json();
+			try {
+				const { lastRowId, sortingFilters, sortingFiltersLastValue } = pageParam;
+				if ( ! wait ) {
+					const response = await postFetch(
+						key,
+						{
+							...fetchOptions,
+							sorting: [ ...sortingArray( key ), { col: paginationId, dir: 'ASC' } ],
+							filters: sortingFilters
+								? [
+									{ cond: 'OR',
+										filters: [
+											{ cond: 'AND', filters: [ ...sortingFiltersLastValue, { col: paginationId, op: '>', val: lastRowId } ] },
+											...sortingFilters,
+										],
+									},
+									...filtersArray( filters ),
+								]
+								: [ ...filtersArray( filters ) ],
+							rows_per_page: maxRows,
+						},
+						{ ...( allowTableFetchAbort ? { signal } : null ) }
+					);
+					if ( response.ok ) {
+						return response.json();
+					}
 				}
+				return [];
+			} catch ( error ) {
+				// eslint-disable-next-line no-console
+				console.error( error );
 			}
-			return [];
 		},
 		getNextPageParam: ( allRows ) => {
-			if ( ! allRows || allRows?.length < maxRows ) {
-				return undefined;
+			// keep function in try catch because of not logged possible errors in method
+			try {
+				if ( ! allRows || allRows?.length < maxRows ) {
+					return undefined;
+				}
+				const lastRowId = allRows[ allRows?.length - 1 ][ paginationId ] ?? undefined;
+				const sortingFilters = sorting ? sorting.map( ( sortingObj ) => {
+					const { key: keyName, op } = sortingObj;
+					return { col: keyName, op, val: allRows[ allRows?.length - 1 ][ keyName ] };
+				} ) : [];
+				const sortingFiltersLastValue = sorting ? sorting.map( ( sortingObj ) => {
+					const { key: keyName } = sortingObj;
+					return { col: keyName, op: '=', val: allRows[ allRows?.length - 1 ][ keyName ] };
+				} ) : [];
+				return { lastRowId, sortingFilters, sortingFiltersLastValue };
+			} catch ( error ) {
+				// eslint-disable-next-line no-console
+				console.error( error );
 			}
-			const lastRowId = allRows[ allRows?.length - 1 ][ paginationId ] ?? undefined;
-			const sortingFilters = sorting ? sorting.map( ( sortingObj ) => {
-				const { key: keyName, op } = sortingObj;
-				return { col: keyName, op, val: allRows[ allRows?.length - 1 ][ keyName ] };
-			} ) : [];
-			const sortingFiltersLastValue = sorting ? sorting.map( ( sortingObj ) => {
-				const { key: keyName } = sortingObj;
-				return { col: keyName, op: '=', val: allRows[ allRows?.length - 1 ][ keyName ] };
-			} ) : [];
-			return { lastRowId, sortingFilters, sortingFiltersLastValue };
 		},
 		keepPreviousData: true,
 		refetchOnMount: true,
