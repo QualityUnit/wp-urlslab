2152099cffb51125b439de76d5fdc8bfa3a2370d
Ivan Hudak <ivan.hdk@gmail.com>
2024-03-27 14:01:39 +0100

    refactor: modules sections user data moved to store

--- admin/src/hooks/useModuleSectionRoute.jsx
+++ admin/src/hooks/useModuleSectionRoute.jsx
@@ -2,37 +2,21 @@
  * Hook to handle opened module section by route
  */
 
-import { useCallback, useEffect, useState } from 'react';
 import { useOutletContext, useParams } from 'react-router-dom';
-import { get } from 'idb-keyval';
+
+import useUserLocalData from './useUserLocalData';
 
 const useModuleSectionRoute = ( availableRoutes, staticId = null ) => {
+	const params = useParams();
 	const { moduleId } = useOutletContext();
 
-	const [ activeSectionData, setActiveSectionData ] = useState( null );
-	const params = useParams();
 	const isRootRoute = Object.keys( params ).length === 0;
-
-	// if it's root module route, check for last visited section
-	const checkLastVisitedTab = useCallback( async () => {
-		if ( isRootRoute ) {
-			const savedData = await get( staticId !== null ? staticId : moduleId );
-			setActiveSectionData( savedData );
-		}
-	}, [ isRootRoute, moduleId, staticId ] );
-
-	useEffect( () => {
-		checkLastVisitedTab();
-	}, [ checkLastVisitedTab ] );
+	const currentId = staticId !== null ? staticId : moduleId;
+	const activeSectionData = useUserLocalData( ( state ) => state.userData[ currentId ] );
 
 	// accessed directly module route without defined section route
-	if ( isRootRoute ) {
-		if ( activeSectionData === undefined ) {
-			// not saved last visited section, return default
-			return 'overview';
-		}
-		// return null if not fetched yet, prevent showing overview while loading from idb-keyval
-		return activeSectionData?.activeMenu ? activeSectionData.activeMenu : null;
+	if ( isRootRoute && activeSectionData && activeSectionData.activeMenu ) {
+		return activeSectionData.activeMenu;
 	}
 
 	// defined section in route, return if defined section route exists
